6b4cd1c6da64d29a2085b78bfe627f2d990214f5
Arkadiusz Bisaga <ethnar86@gmail.com>
2007-05-25 10:32:17 +0000

    Bug fix

--- modules/Base/Theme/ThemeCommon_0.php
+++ modules/Base/Theme/ThemeCommon_0.php
@@ -45,9 +45,10 @@ class Base_ThemeCommon {
 	}
 
 	public static function get_template_file($modulename,$filename) {
-		$f = self::get_template_dir().$modulename.'__'.$filename;
+		$filename = str_replace("/", "_", $modulename).'__'.str_replace("/", "_", $filename);
+		$f = self::get_template_dir().$filename;
 		if(!is_readable($f)) {
-			$f = 'data/Base/Theme/templates/default/'.$modulename.'__'.$filename;
+			$f = 'data/Base/Theme/templates/default/'.$filename;
 			if(!is_readable($f))
 				return false;
 		}
@@ -58,11 +59,9 @@ class Base_ThemeCommon {
 		if(!isset($module_name)) 
 			trigger_error('Invalid argument for load_css, no module was specified.',E_USER_ERROR);
 		
-		$module_name = str_replace("/", "_", $module_name).'__'.str_replace("/", "_", $css_name);
-
-		$css = self::get_template_file($module_name.'.css');
+		$css = self::get_template_file($module_name,$css_name.'.css');
 		if(!$css) {
-			if($trig_error) trigger_error('Invalid css specified: '.$module_name.'.css',E_USER_ERROR);
+			if($trig_error) trigger_error('Invalid css specified: '.$module_name.'__'.$css_name.'.css',E_USER_ERROR);
 			return false;
 		} else {
 			load_css($css);
