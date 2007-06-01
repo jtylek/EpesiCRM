62816150e6ba3ddb816b37b3036cff2984cad178
Paweł Bukowski <pbukowski@telaxus.com>
2007-06-01 10:17:10 +0000

    bugfix of scrolled table

--- modules/Base/Theme/smarty/plugins/function.html_table_tcms.js
+++ modules/Base/Theme/smarty/plugins/function.html_table_tcms.js
@@ -10,6 +10,35 @@ getWidth = function(someObject){
 	return w;
 }
 
+scrolled_table_fix_cell = function(cell, width, w2) {
+	cell.style.width = width;
+	var div = document.createElement('div');
+	div.innerHTML = cell.innerHTML;
+	div.style.overflow = 'hidden';
+	div.style.textOverflow = 'clip';
+	div.style.width = w2;
+	cell.innerHTML = '';
+	cell.appendChild(div);
+}
+
+scrolled_table_fix_scrollbar = function() {
+	var spans = document.getElementsByTagName('span');
+	for (var i = 0; i < spans.length; i++) {
+		var relAttribute = String(spans[i].getAttribute('rel'));
+		if (relAttribute == 'scrolled_table') {
+			var table = spans[i].getElementsByTagName('table');
+			var tbody = table[1].getElementsByTagName('tbody')[0];
+			
+			var diff_width = getWidth(table[0])-getWidth(table[1]);
+			if(diff_width>0) {
+				var trs = tbody.getElementsByTagName('tr');
+				var firstcols = trs[0].getElementsByTagName('td');
+				firstcols[firstcols.length-1].style.width = (parseInt(firstcols[firstcols.length-1].style.width)-diff_width+2)+'px';
+			}
+		}
+	}
+}
+
 scrolled_table_fix_headers = function() {
 	var spans = document.getElementsByTagName('span');
 	for (var i = 0; i < spans.length; i++) {
@@ -19,15 +48,20 @@ scrolled_table_fix_headers = function() {
 			var theader = table[0].getElementsByTagName('thead')[0];
 			var tbody = table[1].getElementsByTagName('tbody')[0];
 			
+			table[0].style.width = getWidth(table[1]);
+			
 			var headers = theader.getElementsByTagName('th');
 			var trs = tbody.getElementsByTagName('tr');
 			var firstcols = trs[0].getElementsByTagName('td');
 
-			for (var j = 0; j < headers.length-1; j++)
-				headers[j].style.width = getWidth(firstcols[j])+'px';
-			headers[headers.length-1].style.width = 'auto';
+			for (var j = 0; j < headers.length; j++) {
+				var w=getWidth(firstcols[j]);
+				scrolled_table_fix_cell(headers[j],w+'px',(w-2)+'px');
+			}
+//			scrolled_table_fix_cell(headers[headers.length-1],'auto','auto');
 		}
 	}
+	setTimeout("scrolled_table_fix_scrollbar()",10);
 }
 
 scrolled_table_fix_cols = function() {
@@ -41,17 +75,15 @@ scrolled_table_fix_cols = function() {
 			
 			var headers = theader.getElementsByTagName('th');
 			var trs = tbody.getElementsByTagName('tr');
+			var firstcols = trs[0].getElementsByTagName('td');
+
 			var header_size = new Array();
 			for (var j = 0; j < headers.length; j++)
 				header_size[j] = getWidth(headers[j]);
 
-			for(var k=0; k<trs.length; k++) {
-				var tds = trs[k].getElementsByTagName('td');
-				for(var j=0; j<headers.length; j++) {
-					tds[j].style.width = header_size[j]+'px';
-				}
-			}
+			for(var j=0; j<headers.length; j++)
+				firstcols[j].style.width = header_size[j]+'px';
 		}
 	}
-	setTimeout("scrolled_table_fix_headers()",1);
+	setTimeout("scrolled_table_fix_headers()",10);
 }
