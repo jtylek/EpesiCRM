2c6922553cc8ebf339bf3e302c317399ce0a48fe
Paweł Bukowski <pbukowski@telaxus.com>
2007-06-03 21:49:04 +0000

    bug fix of gb

--- modules/Base/Theme/smarty/plugins/function.html_table_tcms.js
+++ modules/Base/Theme/smarty/plugins/function.html_table_tcms.js
@@ -91,6 +91,7 @@ scrolled_table_fix_scrollbar = function() {
 
 			if(diff_width>0) {
 				var trs = tbody.getElementsByTagName('tr');
+				if(typeof trs=='undefined') continue;
 				var firstcols = trs[0].getElementsByTagName('td');
 				firstcols[firstcols.length-1].style.width = (parseInt(firstcols[firstcols.length-1].style.width)-diff_width+2)+'px';
 			}
@@ -111,6 +112,7 @@ scrolled_table_fix_headers = function() {
 			
 			var headers = theader.getElementsByTagName('th');
 			var trs = tbody.getElementsByTagName('tr');
+			if(typeof trs=='undefined') continue;
 			var firstcols = trs[0].getElementsByTagName('td');
 
 			for (var j = 0; j < headers.length; j++) {
@@ -133,6 +135,7 @@ scrolled_table_fix_cols = function() {
 			
 			var headers = theader.getElementsByTagName('th');
 			var trs = tbody.getElementsByTagName('tr');
+			if(typeof trs=='undefined') continue;
 			var firstcols = trs[0].getElementsByTagName('td');
 
 			var header_size = new Array();
