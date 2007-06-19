fcc2766b859dd9a2077293d6f79b3f4bd8ae6cb5
Paweł Bukowski <pbukowski@telaxus.com>
2007-06-19 20:31:22 +0000

    statusbar overlay

--- modules/Base/StatusBar/StatusBar_0.php
+++ modules/Base/StatusBar/StatusBar_0.php
@@ -30,29 +30,44 @@ class Base_StatusBar extends Module {
 	}
 	
 	private function load_js() {
-		eval_js_once('var statusbar_message_t=\'\';
-				statusbar_message=function(text){statusbar_message_t=text;};
-				statusbar_fade=function(){wait_while_null(\'document.getElementById(\\\'statusbar\\\')\',\'Effect.Fade(\\\'statusbar\\\', {duration:1.0})\');};				
-				updateSajaIndicatorFunction=function(){
-					saja.indicator=\'statusbar_text\';
-					document.getElementById(\'sajaStatus\').style.visibility=\'hidden\';
-					document.getElementById(\'statusbar\').style.display=\'none\';
-					saja.updateIndicator=function(){
-						if(saja.procOn){
-							Effect.Appear(\'statusbar\', {duration:0.0});
-						}else{
-							if(statusbar_message_t!=\'\') {
-								t=document.getElementById(\'statusbar_text\');
-								if(t)t.innerHTML=statusbar_message_t;
-								statusbar_message(\'\');
-								setTimeout(\'statusbar_fade()\',3000);
-							}else{
-								statusbar_fade();
-							};
-						};
-					};
-				};
-				wait_while_null(\'Effect\',\'updateSajaIndicatorFunction()\')');
+		eval_js_once('var statusbar_message_t=\'\';' .
+				'statusbar_message=function(text){statusbar_message_t=text;};' .
+				'statusbar_fade=function(){wait_while_null(\'document.getElementById(\\\'statusbar\\\')\',\'Effect.Fade(\\\'statusbar\\\', {duration:1.0});$(\\\'overlay\\\').style.display=\\\'none\\\';\');};' .				
+				'updateSajaIndicatorFunction=function(){' .
+					'saja.indicator=\'statusbar_text\';' .
+					'document.getElementById(\'sajaStatus\').style.visibility=\'hidden\';' .
+					'statbar = document.getElementById(\'statusbar\');' .
+					'statbar.style.display=\'none\';' .
+					'statbar.onclick = statusbar_fade;' .
+					'overlay = document.createElement(\'div\');' .
+					'overlay.style.display = \'none\';' .
+					'overlay.id = \'overlay\';' .
+					'if(navigator.appName.indexOf(\'Explorer\') != -1 ) {' .
+						'overlay.style.position = \'absolute\';' .
+						'overlay.className = \'statusbar_overlay_ie\';' .
+						'overlay.style.height = (document.documentElement.clientHeight < document.body.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight) + \'px\';' .
+					'} else {' .
+						'$(\'overlay\').style.position = \'fixed\';' .
+						'overlay.className = \'statusbar_overlay\';' .
+					'};' .
+					'saja.updateIndicator=function(){' .
+						'if(saja.procOn){' .
+							'Effect.Appear(\'statusbar\', {duration:0.0});' .
+							//'Effect.Appear(\'overlay\', {duration:0.0});' .
+							'$(\'overlay\').style.display=\'block\';' .
+						'}else{' .
+							'if(statusbar_message_t!=\'\') {' .
+								't=document.getElementById(\'statusbar_text\');' .
+								'if(t)t.innerHTML=statusbar_message_t;' .
+								'statusbar_message(\'\');' .
+								'setTimeout(\'statusbar_fade()\',3000);' .
+							'}else{' .
+								'statusbar_fade();' .
+							'};' .
+						'};' .
+					'};' .
+				'};' .
+				'wait_while_null(\'Effect\',\'updateSajaIndicatorFunction()\')');
 	}
 }
 ?>
