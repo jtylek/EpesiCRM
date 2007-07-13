042eebc3d7a2ef782aa7727f469e35770577ad00
Paweł Bukowski <pbukowski@telaxus.com>
2007-07-13 17:42:56 +0000

    comments

--- modules/Apps/Shoutbox/Shoutbox_0.php
+++ modules/Apps/Shoutbox/Shoutbox_0.php
@@ -4,25 +4,40 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 class Apps_Shoutbox extends Module {
 
 	public function body($arg) {
+		//initialize lang module
 		$l = & $this->init_module('Base/Lang');
 
+		//initialize HTML_QuickForm
 		$qf = & $this->init_module('Libs/QuickForm');
+		//create text box
 		$text = & HTML_QuickForm::createElement('text','post',$l->t('Post'),'id="shoutbox_text"');
+		//create submit button
 		$submit = & HTML_QuickForm::createElement('submit','button',$l->t('Submit'));
+		//add it
 		$qf->addGroup(array($text,$submit),'post');
 		$qf->addGroupRule('post',$l->t('Field required'),'required',null,2);
+
+		//if submited
 		if($qf->validate()) {
+			 //get post group
 			$msg = $qf->exportValue('post');
+			//get msg from post group
 			$msg = $msg['post'];
+			//get logged user id
 			$user_id = Base_UserCommon::get_my_user_id();
+			//clear text box and focus it
 			eval_js('document.getElementById(\'shoutbox_text\').value=\'\';focus_by_id(\'shoutbox_text\')');
+			
+			//insert to db
 			if(isset($user_id))
 				DB::Execute('INSERT INTO apps_shoutbox_messages(message,base_user_login_id) VALUES(%s,%d)',array($msg,$user_id));
 			else
 				DB::Execute('INSERT INTO apps_shoutbox_messages(message) VALUES(%s)',array($msg));
 		}
+		//display form
 		$qf->display();
 
+		//get last 50 messages
 		$arr = DB::GetAll('SELECT asm.id, ul.login, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id ORDER BY asm.posted_on DESC LIMIT 50');
 		print('<div id=\'shoutbox\' style="height:200px;overflow:auto;text-align:left;border:1px solid black;">');
 		foreach($arr as $row) {
@@ -32,15 +47,18 @@ class Apps_Shoutbox extends Module {
 		}
 		print('</div>');
 
+		//if i am admin add "clear shoutbox" actionbar button
 		if(Base_AclCommon::i_am_admin())
 			Base_ActionBarCommon::add('delete',$l->ht('Clear shoutbox'),$this->create_callback_href(array($this,'delete_all')));
 
+		//if there is displayed shoutbox, call myFunctions->refresh from refresh.php file every 5s
 		eval_js_once('shoutbox_refresh = function(){if(typeof(document.getElementById(\'shoutbox\'))==\'undefined\') return;'.
 			$GLOBALS['base']->run('refresh(client_id)->shoutbox:innerHTML','modules/Apps/Shoutbox/refresh.php').
 			'};setInterval(\'shoutbox_refresh()\',5000)');
 		
 	}
 	
+	//delete_all callback (on "clear shoutbox" button)
 	public function delete_all() {
 		DB::Execute('DELETE FROM apps_shoutbox_messages');
 	}
--- modules/Apps/Shoutbox/refresh.php
+++ modules/Apps/Shoutbox/refresh.php
@@ -1,8 +1,11 @@
 <?php
 class myFunctions extends Epesi {
 	public function refresh($cl_id) {
+		//initialize Epesi
 		$this->init($cl_id);
-		$arr = DB::GetAll('SELECT asm.id, ul.login, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id ORDER BY asm.posted_on DESC LIMIT 50');
+		//get last 50 messages
+		$arr = DB::GetAll('SELECT ul.login, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id ORDER BY asm.posted_on DESC LIMIT 50');
+		//print it out
 		foreach($arr as $row) {
 			if(!$row['login']) $row['login']='Anonymous';
 			print(Base_LangCommon::ts('Apps_Shoutbox','[%s] %s: %s',array($row['posted_on'], $row['login'], $row['message'])).'<br>');
