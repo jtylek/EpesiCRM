e4029f9990fcd795c3ba7aacc164022016c60970
Arkadiusz Bisaga <ethnar86@gmail.com>
2007-08-20 14:52:11 +0000

    update 0.8.11 to 0.9.0

--- update.php
+++ update.php
@@ -9,15 +9,67 @@
  */
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
-$versions = array('0.8.5','0.8.6','0.8.7','0.8.8','0.8.9','0.8.10','0.8.11');
+include_once('include/misc.php');
 
-function update_from_0_8_6_to_0_8_7() {
+function themeup(){
+	$data_dir = 'data/Base_Theme/templates/default/';
+	$content = scandir($data_dir);
+	foreach ($content as $name){
+		if ($name == '.' || $name == '..') continue;
+		if (!is_dir($data_dir.'/'.$name)){
+			unlink($data_dir.'/'.$name);
+		}
+	}	
+	$ret = DB::Execute('SELECT * FROM modules');
+	while($row = $ret->FetchRow()) {
+		$directory = 'modules/'.str_replace('_','/',$row[0]).'/theme';
+		$mod_name = $row[0];
+		$data_dir = 'data/Base_Theme/templates/default/';
+		if (!is_dir($directory)) continue;
+		$content = scandir($directory);
+		foreach ($content as $name){
+			if($name == '.' || $name == '..' || strpos('.php',$name)!==false) continue;
+			if (!is_dir($directory.'/'.$name)){
+				copy($directory.'/'.$name,$data_dir.$mod_name.'__'.$name);
+			}
+		}
+	}
 }
 
+$versions = array('0.8.5','0.8.6','0.8.7','0.8.8','0.8.9','0.8.10','0.8.11','0.9.0');
+
+/******************* 0.8.11 to 0.9.0 **********************/
+function mod_cmp($a, $b){
+	$la = strlen($a['name']);
+	$lb = strlen($b['name']);
+	if ($la === $lb) return 0;
+	else return ($la > $lb)?-1:1; 
+}
+function update_from_0_8_11_to_0_9_0() {
+	DB::Execute('UPDATE modules SET version=0 WHERE name=\'Base_Admin\'');
+	recursive_copy('data','data_old');
+	rename('data','data_tmp');
+	mkdir('data');
+	copy('data_tmp/config.php','data/config.php');
+	$mod = DB::GetAll('SELECT name FROM modules');
+	usort($mod,'mod_cmp');
+	foreach($mod as $row) {
+		$name = str_replace('_','/',$row['name']);
+		recursive_copy('data_tmp/'.$name,'data/'.$row['name']);
+		recursive_rmdir('data_tmp/'.$name);
+	}
+	recursive_rmdir('data_tmp');
+	themeup();
+}
+/****************** 0.8.6 to 0.8.7 **********************/
+function update_from_0_8_6_to_0_8_7() {
+}
+/****************** 0.8.5 to 0.8.6 **********************/
 function update_from_0_8_5_to_0_8_6() {
 }
 
 //=========================================================================
+
 try {
 $cur_ver = Variable::get('version');
 } catch(Exception $s) {
