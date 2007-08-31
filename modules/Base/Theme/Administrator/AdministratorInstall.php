e50cd3cf518e5140870a7961b01f87253daf1c99
Paweł Bukowski <pbukowski@telaxus.com>
2007-08-31 10:20:22 +0000

    bug fix

--- modules/Base/Theme/Administrator/AdministratorInstall.php
+++ modules/Base/Theme/Administrator/AdministratorInstall.php
@@ -29,6 +29,7 @@ class Base_Theme_AdministratorInstall extends ModuleInstall {
 			array('name'=>'Base/ActionBar','version'=>0),
 			array('name'=>'Utils/FileUpload','version'=>0),
 			array('name'=>'Utils/FileDownload','version'=>0),
+			array('name'=>'Utils/Image','version'=>0),
 			array('name'=>'Libs/QuickForm','version'=>0), 
 			array('name'=>'Base/StatusBar','version'=>0),
 			array('name'=>'Base/Lang','version'=>0));
