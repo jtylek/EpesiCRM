33b06917d644fbf949f6749aa0664f7915fb608a
Paweł Bukowski <pbukowski@telaxus.com>
2007-09-19 08:26:39 +0000

    bug fixes

--- modules/Base/Dashboard/ab.js
+++ modules/Base/Dashboard/ab.js
@@ -2,7 +2,7 @@ dashboard_activate = function() {
 	if(!$('dashboard')) return;
 	for(var id=0; id<3; id++)
 		Sortable.create("dashboard_applets_"+id,{dropOnEmpty:true,tag:'div',containment:["dashboard_applets_0","dashboard_applets_1","dashboard_applets_2"],constraint:false, ghosting: true, handle: 'handle',onUpdate:function(c){
-		        new Ajax.Request("modules/Base/ActiveBoard/update.php",{
+		        new Ajax.Request("modules/Base/Dashboard/update.php",{
 					method: "post",
 					parameters: { data: Sortable.serialize(c.id) }
 			});
