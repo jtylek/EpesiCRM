cf4829ac7d03ebc72b0383d5af72e6ae9acc45f5
Paweł Bukowski <pbukowski@telaxus.com>
2007-09-29 16:18:01 +0000

    clock fix

--- modules/Applets/Clock/Clock_0.php
+++ modules/Applets/Clock/Clock_0.php
@@ -19,8 +19,8 @@ class Applets_Clock extends Module {
 	public function applet($conf) {
 		load_js($this->get_module_dir().'excanvas.js');
 		load_js($this->get_module_dir().'coolclock.js');
+		print('<canvas id="'.$this->get_path().'canvas" class="CoolClock:'.$conf['skin'].'"></canvas>');
 		eval_js('CoolClock.findAndCreateClocks()');
-		print('<canvas class="CoolClock:'.$conf['skin'].'"></canvas>');
 	}
 
 }
