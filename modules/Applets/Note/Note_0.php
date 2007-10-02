906f1823da397825d998803e7f5cf93c4bc4bb65
Janusz Tylek <jtylek@telaxus.com>
2007-10-02 17:07:22 +0000

    Added yellow background

--- modules/Applets/Note/Note_0.php
+++ modules/Applets/Note/Note_0.php
@@ -16,8 +16,25 @@ class Applets_Note extends Module {
 	}
 
 	public function applet($values, $opts) { //available applet options: toggle,href,title,go,go_function,go_arguments,go_contruct_arguments
-		$opts['title'] = $values['title'];
-		print(str_replace("\n",'<br>',$values['text']));
+	$opts['title'] = $values['title'];
+
+	print ('
+	<style type="text/css">
+	.postit {
+	border: 1px solid #FFFF00;
+	}
+	.postit div {
+	color: black;
+	background: #FFFF00;
+	padding: .5em;
+	position: relative;
+	}
+	</style>');
+	
+	print ('<div class="postit">');
+ 	print ('<div>');
+ 	print (str_replace("\n",'<br>',$values['text']));
+ 	print ('</div>');
 	}
 
 }
