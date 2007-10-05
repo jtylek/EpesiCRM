b655783610f5b316e0217af3ff72b888b16be62a
Paweł Bukowski <pbukowski@telaxus.com>
2007-10-05 14:20:24 +0000

    bug fixes

--- modules/Apps/SessionKeeper/SessionKeeperCommon_0.php
+++ modules/Apps/SessionKeeper/SessionKeeperCommon_0.php
@@ -11,8 +11,20 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Apps_SessionKeeperCommon extends ModuleCommon {
 	public static function user_settings(){
-		return array('Session settings'=>array(
-			array('name'=>'time','label'=>'Keep session at least','type'=>'select','values'=>array('default'=>'default server time','1800'=>'30 minutes','3600'=>'1 hour', '7200'=>'2 hours', '14400'=>'4 hours', '28800'=>'8 hours'),'default'=>'default','reload'=>true)
+		$time = ini_get("session.gc_maxlifetime");
+		$def = array('default'=>'default server time ('.($time/60).' minutes)');
+		if($time<1800)
+			$def['1800']='30 minutes';
+		if($time<3600)
+			$def['3600']='1 hour';
+		if($time<7200)
+			$def['7200']='2 hours';
+		if($time<14400)
+			$def['14400']='4 hours';
+		if($time<28800)
+			$def['28800']='8 hours';
+		return array('Session'=>array(
+			array('name'=>'time','label'=>'Keep session at least','type'=>'select','values'=>$def,'default'=>'default','reload'=>true)
 			));
 	}
 
@@ -23,10 +35,14 @@ if(Acl::is_user()) {
 		load_js('modules/Apps/SessionKeeper/sk.js');
 		$sys_time = ini_get("session.gc_maxlifetime");
 		$x_time = $time-$sys_time;
-		if($x_time>0)
+		if($x_time>0) {
+			$interval = $sys_time/2;
+			if($x_time<$interval)
+				$interval = $x_time;
 			eval_js_once('SessionKeeper.maxtime='.$x_time.';'.
 				'SessionKeeper.interval='.($sys_time/2).';'.
 				'SessionKeeper.load()');
+		}
 	}
 }
 ?>
