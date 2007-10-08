512f18f873c4c2cac1a6a667a039ab2e904cd787
Janusz Tylek <jtylek@telaxus.com>
2007-10-08 15:16:13 +0000

    Fix for error: message=DOMDocument::load() [function.DOMDocument-load]: php_network_getaddresses: getaddrinfo failed: No such host is known. error file=D:\www\epesi\modules\Applets\RssFeed\rsslib.php

--- modules/Applets/RssFeed/RssFeed_0.php
+++ modules/Applets/RssFeed/RssFeed_0.php
@@ -35,7 +35,22 @@ class Applets_rssfeed extends Module {
 			$new_title = $this->get_page_title($values['rssfeed']);
 			if($new_title!==null)
 				$opts['title'] = substr($new_title,0,15).'...';
-			echo RSS_Display(($values['rssfeed']), ($values['rssnumber']));
+			
+		// Check if RSS can be read
+		$rss = curl_init();
+		curl_setopt($rss, CURLOPT_URL, $values['rssfeed']);
+		curl_setopt($rss, CURLOPT_RETURNTRANSFER, true);
+		$output = curl_exec($rss);
+		$response_code = curl_getinfo($rss, CURLINFO_HTTP_CODE);
+				
+		if ($response_code == '404') {
+    		echo "Error: ".$response_code."<BR>";
+			echo $values['rssfeed'];
+			echo ("<BR>Invalid RSS feed.<BR>Please check your configuration.<BR>");
+		} else {
+        	echo RSS_Display(($values['rssfeed']), ($values['rssnumber']));
+		}
+		
 		} else print('RSS feed not defined');
 	}
 }
