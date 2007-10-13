cc0808fde8740fbfa66bd0974f4e0348c12531fa
Paweł Bukowski <pbukowski@telaxus.com>
2007-10-13 13:47:59 +0000

    error fixes

--- include/error.php
+++ include/error.php
@@ -74,7 +74,7 @@ class ErrorHandler {
 				} else $backtrace = '';
 				
 				while(@ob_end_clean());
-				echo self::notify_client('type='.$type.'<br>message='.$message.'<br>error file='.$errfile.'<br>error line='.$errline.'<br>error context='.print_r($errcontext,true).$backtrace.'<hr>');
+				echo self::notify_client('Type: '.$type.'<br>Message: '.$message.'<br>File: '.$errfile.'<br>Line='.$errline.$backtrace.'<hr>');
 				exit();
 			}
 		}
