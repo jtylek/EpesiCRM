6400fb0920db965f5b6af9fe3e016d4d1719dacf
Paweł Bukowski <pbukowski@telaxus.com>
2007-10-21 18:00:39 +0000

    removed old datepicker

--- modules/Libs/QuickForm/QuickFormCommon_0.php
+++ modules/Libs/QuickForm/QuickFormCommon_0.php
@@ -14,7 +14,6 @@ require_once('Renderer/TCMSArraySmarty.php');
 require_once('Renderer/TCMSDefault.php');
 
 $GLOBALS['_HTML_QuickForm_default_renderer'] = new HTML_QuickForm_Renderer_TCMSDefault();
-//$GLOBALS['HTML_QUICKFORM_ELEMENT_TYPES']['datepicker'] = array('modules/Libs/QuickForm/datepicker.php','HTML_QuickForm_datepicker');
 $GLOBALS['HTML_QUICKFORM_ELEMENT_TYPES']['multiselect'] = array('modules/Libs/QuickForm/multiselect.php','HTML_QuickForm_multiselect');
 $GLOBALS['_HTML_QuickForm_registered_rules']['comparestring'] = array('HTML_QuickForm_Rule_CompareString', 'Rule/CompareString.php');
 
