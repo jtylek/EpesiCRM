37da2079df6817081098c8d9708da2e669f3bc6c
Paweł Bukowski <pbukowski@telaxus.com>
2007-10-25 15:07:29 +0000

    new script aculo us

--- modules/Libs/ScriptAculoUs/ScriptAculoUsInstall.php
+++ modules/Libs/ScriptAculoUs/ScriptAculoUsInstall.php
@@ -19,7 +19,7 @@ class Libs_ScriptAculoUsInstall extends ModuleInstall {
 	}
 	
 	public function version() {
-		return array('1.7.1');
+		return array('1.8.0_pre1');
 	}
 	public function requires($v) {
 		return array();
