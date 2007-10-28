8820d2733aa070335ae3ca909694a69bb2da9e18
Paweł Bukowski <pbukowski@telaxus.com>
2007-10-28 12:55:32 +0000

    qf single elem seems to work

--- modules/Utils/CommonData/qf.js
+++ modules/Utils/CommonData/qf.js
@@ -10,12 +10,15 @@ Utils_CommonData.prototype = {
 		this.add_empty = ae;
 		this.def_val = val;
 		var obj = this.obj;
-		for(var i=1,max=this.path.length-1; i<max; i++)
-			Event.observe(eval('obj.form.'+this.path[i]),'change',function(){obj.options.length=0;obj.disabled=true;});
+//		for(var i=1,max=this.path.length-1; i<max; i++)
+//			Event.observe(eval('obj.form.'+this.path[i]),'e_u_cd:clear',function(){obj.options.length=0;obj.disabled=true;alert('clear '+obj.name);});
 
 //		alert('observe '+obj.name);
-		Event.observe(eval('obj.form.'+this.path[this.path.length-1]),'change',this.request.bindAsEventListener(this));
-	
+		var prev_obj = eval('obj.form.'+this.path[this.path.length-1]);
+		Event.observe(prev_obj,'change',this.request.bindAsEventListener(this));
+		Event.observe(prev_obj,'e_u_cd:load',this.request.bindAsEventListener(this));
+		Event.observe(prev_obj,'e_u_cd:clear',function(){obj.options.length=0;obj.disabled=true;});
+		
 		if(this.path.length==2)
 			this.request(null);
 	},
@@ -26,11 +29,6 @@ Utils_CommonData.prototype = {
 		var curr_root = this.path[0];
 		for(var i=1; i<this.path.length; i++) {
 			var val = eval('obj.form.'+this.path[i]).value;
-			if(val=='') {
-				obj.disabled=true;
-				obj.options.length=0;
-				return;
-			}
 			curr_root += '/' + val;
 		}
 		new Ajax.Request('modules/Utils/CommonData/update.php',{
@@ -45,17 +43,20 @@ Utils_CommonData.prototype = {
 		var new_opts = t.responseText.evalJSON();
 		var opts = this.obj.options;
 		opts.length=0;
-		if(new_opts.length==0) this.obj.disabled=true;
-		else {
+		if(new_opts.length==0) {
+			this.obj.fire('e_u_cd:clear');
+			this.obj.disabled=true;
+		} else {
+			this.obj.disabled=false;
 			if(this.add_empty==1) opts[0] = new Option('---','');
 			$H(new_opts).each(function(x) {opts[opts.length] = new Option(x[1],x[0]);});
-			this.obj.disabled=false;
 			if(this.def_val!=null) {
 				this.obj.value = this.def_val;
 				this.def_val=null;
-	//			alert('def is '+this.def_val);
 			}
-			this.obj.fire('change'); //doesn't work now, but with new prototype it will work!:P
+//			alert('fire '+this.obj.name);
+//			this.obj.fire('e_u_cd:load');
+			setTimeout(this.obj.fire.bind(this.obj,'e_u_cd:load'),1);
 		}
 	}
 };
