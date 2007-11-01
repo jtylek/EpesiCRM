a7df05d1fb21f4b69ec2357ad8e16d84628d24f3
Paweł Bukowski <pbukowski@telaxus.com>
2007-11-01 14:41:35 +0000

    update.php, demo, checkmod, 0.9.0 release

--- update.php
+++ update.php
@@ -45,9 +45,11 @@ function mod_cmp($a, $b){
 }
 function update_from_0_8_11_to_0_9_0() {
 	DB::DropTable('session');
-	DB::CreateTable('session',"name C(255) NOTNULL KEY, " .
-			"expires I NOTNULL DEFAULT 0, data X2");
-	DB::CreateTable('history',"session_name C(255) NOTNULL, page_id I, client_id I," .
+	DB::CreateTable('session',"name C(32) NOTNULL," .
+			"expires I NOTNULL DEFAULT 0, data X",array('constraints'=>', PRIMARY KEY(name)'));
+	DB::CreateTable('session_client',"session_name C(32) NOTNULL, client_id I2," .
+			"data X2",array('constraints'=>', FOREIGN KEY(session_name) REFERENCES session(name)'));
+	DB::CreateTable('history',"session_name C(32) NOTNULL, page_id I, client_id I2," .
 			"data X2",array('constraints'=>', FOREIGN KEY(session_name) REFERENCES session(name)'));
 
 	// Reducing Base_Admin version number
