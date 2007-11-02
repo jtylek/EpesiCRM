a0ed35a04cbcd1f204c873f70160b3ec2dddc568
Paweł Bukowski <pbukowski@telaxus.com>
2007-11-02 21:35:13 +0000

    datepicker validator

--- modules/Utils/Calendar/datepicker.php
+++ modules/Utils/Calendar/datepicker.php
@@ -26,12 +26,17 @@ class HTML_QuickForm_datepicker extends HTML_QuickForm_input {
 				$id = 'datepicker_field_'.$name;
 				$this->updateAttributes(array('id'=>$id));
 			}
-			$str .= $this->_getTabs() . '<input ' . $this->_getAttrString($this->_attributes) . ' '.Utils_Tooltip::open_tag_attrs(Base_RegionalSettingsCommon::date_format(), false ).' />'.
+			$date_format = Base_RegionalSettingsCommon::date_format();
+			$str .= $this->_getTabs() . '<input ' . $this->_getAttrString($this->_attributes) . ' '.Utils_Tooltip::open_tag_attrs($date_format, false ).' />'.
 				Utils_CalendarCommon::show($name,
 					'new Ajax.Request(\'modules/Utils/Calendar/up.php\','.
 					'{method:\'post\', parameters:{date: __YEAR__+\'-\'+__MONTH__+\'-\'+__DAY__},'.
 					'onSuccess:function(t){$(\''.Epesi::escapeJS($id,false).'\').value=t.responseText;}})',
 					false,'expression( ($(\''.$id.'\').getStyle(\'top\') )+\'px\')','expression( ($(\''.$id.'\').getStyle(\'left\') )+\'px\')');
+				
+			load_js('modules/Utils/Calendar/datepicker.js');
+			eval_js('Event.observe(\''.$id.'\',\'keypress\',Utils_CalendarDatePicker.validate.bindAsEventListener(Utils_CalendarDatePicker,\''.Epesi::escapeJS($date_format,false).'\'))');
+			eval_js('Event.observe(\''.$id.'\',\'blur\',Utils_CalendarDatePicker.validate_blur.bindAsEventListener(Utils_CalendarDatePicker,\''.Epesi::escapeJS($date_format,false).'\'))');
 		}
 		return $str;
 	} //end func toHtml
