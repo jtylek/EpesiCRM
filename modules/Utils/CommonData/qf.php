7a7bde290a5791d45b90b9838455fe3481df1808
Paweł Bukowski <pbukowski@telaxus.com>
2007-11-12 20:53:11 +0000

    bug fix

--- modules/Utils/CommonData/qf.php
+++ modules/Utils/CommonData/qf.php
@@ -26,12 +26,6 @@ class HTML_QuickForm_commondata extends HTML_QuickForm_select {
 		if (isset($options['empty_option']))
 			$this->_add_empty_fields = $options['empty_option'];
 
-		if(count($this->_cd)==1) {
-			$root_data = Utils_CommonDataCommon::get_array($this->_cd[0]);
-			if($this->_add_empty_fields)
-				$root_data = array_merge(array(''=>'---'),$root_data);
-			$this->loadArray($root_data);
-		}
 	} //end constructor
 	
 	function toHtml() {
@@ -44,6 +38,11 @@ class HTML_QuickForm_commondata extends HTML_QuickForm_select {
 			}
 			$val = $this->getValue();
 			eval_js('new Utils_CommonData(\''.Epesi::escapeJS($id,false).'\', \''.Epesi::escapeJS($val[0],false).'\', \''.Epesi::escapeJS(json_encode($this->_cd),false).'\', '.($this->_add_empty_fields?1:0).')');
+		} else {
+			$root_data = Utils_CommonDataCommon::get_array($this->_cd[0]);
+			if($this->_add_empty_fields)
+				$root_data = array_merge(array(''=>'---'),$root_data);
+			$this->loadArray($root_data);
 		}
 	        return parent::toHtml();
 	}
