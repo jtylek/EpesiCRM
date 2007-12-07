9b13219e0ad0e5f1b7886846563c7d70dd0fa72f
Paweł Bukowski <pbukowski@telaxus.com>
2007-12-07 18:45:00 +0000

    mime.php

---
+++ modules/Utils/Attachment/mime.php
@@ -0,0 +1,25 @@
+<?php
+function get_mime_type($filepath) {
+	//new method, but not compiled in by default
+	if(extension_loaded('fileinfo')) {
+        	$fff = new finfo(FILEINFO_MIME);
+	        $ret = $fff->file($filepath);
+        	$fff->close();
+	        return $ret;
+    	}
+
+	//deprecated method
+	if(function_exists('mime_content_type'))
+        	return mime_content_type($filepath);
+
+	//unix system
+	ob_start();
+	system("file -i -b {$filepath}");
+	$output = ob_get_clean();
+	$output = explode("; ",$output);
+	if ( is_array($output) ) {
+        	$output = $output[0];
+	}
+	return $output;
+}
+?>
