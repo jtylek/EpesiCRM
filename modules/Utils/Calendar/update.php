3040979be0ab311207e80f109996d489f36dbd6f
Paweł Bukowski <pbukowski@telaxus.com>
2007-12-11 22:00:44 +0000

    missing file

---
+++ modules/Utils/Calendar/update.php
@@ -0,0 +1,15 @@
+<?php
+if(!isset($_POST['path']) || !isset($_POST['cid']) ||  !isset($_POST['ev_id']) || !isset($_POST['cell_id']))
+	die('alert(\'Invalid request\')');
+	
+define('JS_OUTPUT',1);
+define('CID',$_POST['cid']);
+require_once('../../../include.php');
+ModuleManager::load_modules();
+
+$mod = Module::static_get_module_variable($_POST['path'],'event_module');
+if(!$mod)
+	die('alert(\'Invalid request!\')');
+$cc = explode('_',$_POST['cell_id']);
+call_user_func(array($mod.'Common','update'),$_POST['ev_id'],$cc[0],isset($cc[1]));
+?>
