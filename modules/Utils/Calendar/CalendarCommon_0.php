da3ef62c0a4ff9fb785f8682637aabc52fb7369c
Paweł Bukowski <pbukowski@telaxus.com>
2007-12-14 20:32:19 +0000

    missing file

---
+++ modules/Utils/Calendar/CalendarCommon_0.php
@@ -0,0 +1,86 @@
+<?php
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+
+class Utils_CalendarCommon extends ModuleCommon {
+	public static function print_event($ev) {
+		$th = Base_ThemeCommon::init_smarty();
+		$ex = self::process_event($ev);
+		$th->assign('event_id',$ev['id']);
+		$th->assign('title',strip_tags($ev['title']));
+		$th->assign('description',$ev['description']);
+		$th->assign('start',$ex['start']);
+		$th->assign('end',$ex['end']);
+		$th->assign('duration',$ex['duration']);
+		ob_start();
+		Base_ThemeCommon::display_smarty($th,'Utils_Calendar','event_tip');
+		$tip = ob_get_clean();
+		$th->assign('tip_tag_attrs',Utils_TooltipCommon::open_tag_attrs($tip));
+		$th->assign('handle_class','handle');
+		Base_ThemeCommon::display_smarty($th,'Utils_Calendar','event');
+	}
+
+	public static function process_event($row) {
+		if(!isset($row['start']) || !isset($row['duration']) || !is_numeric($row['duration'])
+		   || !isset($row['title']) || !isset($row['description'])
+		   || !isset($row['timeless']) || !isset($row['id']))
+			trigger_error('Invalid return of event method: get',E_USER_ERROR);
+
+		if(!is_numeric($row['start']) && is_string($row['start'])) $row['start'] = strtotime($row['start']);
+		if($row['start']===false)
+			trigger_error('Invalid return of event method: get',E_USER_ERROR);
+
+		$row['end'] = $row['start']+$row['duration'];
+
+		$ev_start = $row['start'];
+		$ev_end = $row['end'];
+		$oneday = (date('Y-m-d',$ev_end)==date('Y-m-d',$ev_start));
+
+		Base_RegionalSettingsCommon::set_locale();
+		$start_day = date('D',$ev_start);
+		if(!$oneday)
+			$end_day = date('D',$ev_end);
+		else
+			$end_t = Base_RegionalSettingsCommon::convert_24h($ev_end);
+		Base_RegionalSettingsCommon::restore_locale();
+
+		if($row['timeless']) {
+			$start_t = $start_day.', '.Base_RegionalSettingsCommon::time2reg($ev_start,false);
+			if($oneday)
+				$end_t = $start_t;
+			else
+				$end_t = $end_day.', '.Base_RegionalSettingsCommon::time2reg($ev_end,false);
+		} else {
+			$start_t = $start_day.', '.Base_RegionalSettingsCommon::time2reg($ev_start);
+			if(!$oneday)
+				$end_t = $end_day.', '.Base_RegionalSettingsCommon::time2reg($ev_end);
+		}
+
+		$duration_str = self::duration2str($row['duration']);
+		return array('duration'=>$duration_str,'start'=>$start_t,'end'=>$end_t);
+	}
+
+	private static function duration2str($duration) {
+		$sec = $duration%60;
+		$duration = floor($duration/60);
+		if($duration>0) {
+			$min = $duration%60;
+			$duration = floor($duration/60);
+			if($duration>0) {
+				$hour = $duration%24;
+				$duration = floor($duration/24);
+				if($duration>0) {
+					$days = $duration;
+					$duration_str = Base_LangCommon::ts('Utils_Calendar','%d day(s) %d:%s',array($days, $hour,str_pad($min, 2, "0", STR_PAD_LEFT)));
+				} else
+					$duration_str = Base_LangCommon::ts('Utils_Calendar','%d:%s',array($hour,str_pad($min, 2, "0", STR_PAD_LEFT)));
+			} else
+				$duration_str = Base_LangCommon::ts('Utils_Calendar','00:%s',array(str_pad($min, 2, "0", STR_PAD_LEFT)));
+		} else
+			$duration_str = Base_LangCommon::ts('Utils_Calendar','%d sec',array($sec));
+		return $duration_str;
+	}
+
+}
+
+
+?>
