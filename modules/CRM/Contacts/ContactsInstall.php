45fe1fe4e5b14dda53a4672d0d499146570d08c9
Paweł Bukowski <pbukowski@telaxus.com>
2007-12-20 19:31:44 +0000

    deletion of attachments

--- modules/CRM/Contacts/ContactsInstall.php
+++ modules/CRM/Contacts/ContactsInstall.php
@@ -112,6 +112,8 @@ class CRM_ContactsInstall extends ModuleInstall {
 		Base_ThemeCommon::uninstall_default_theme('CRM/Contacts');
 		Utils_RecordBrowserCommon::delete_addon('company', 'CRM/Contacts', 'company_addon');
 		Utils_RecordBrowserCommon::delete_addon('company', 'CRM/Contacts', 'company_attachment_addon');
+		Utils_AttachmentCommon::persistent_mass_delete(null,'CRM/Contact/');
+		Utils_AttachmentCommon::persistent_mass_delete(null,'CRM/Company/');
 		Utils_RecordBrowserCommon::delete_addon('contact', 'CRM/Contacts', 'contact_attachment_addon');
 		Utils_RecordBrowserCommon::uninstall_recordset('company');
 		Utils_RecordBrowserCommon::uninstall_recordset('contact');
