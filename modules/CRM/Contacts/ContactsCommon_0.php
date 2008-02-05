d6b3bec5754e4fe21e51da0cecc9401f7f819e9c
Paweł Bukowski <pbukowski@telaxus.com>
2008-02-05 15:35:15 +0000

    bug fix

--- modules/CRM/Contacts/ContactsCommon_0.php
+++ modules/CRM/Contacts/ContactsCommon_0.php
@@ -309,12 +309,12 @@ class CRM_ContactsCommon extends ModuleCommon {
 			$result = self::get_contacts(array('"~first_name'=>DB::Concat('\'%\'',DB::qstr($word),'\'%\'')));
 
 	 		foreach ($result as $row)
- 				$ret['Contact #'.$row['id'].', '.$row['first_name'].' '.$row['last_name']] = array('search_contact'=>$row['id']);
+ 				$ret['Contact #'.$row['id'].', '.$row['first_name'].' '.$row['last_name']] = Utils_RecordBrowserCommon::get_record_href_array('contact',$row['id']);
 	 		
 			$result = self::get_contacts(array('"~last_name'=>DB::Concat('\'%\'',DB::qstr($word),'\'%\'')));
 
 	 		foreach ($result as $row)
- 				$ret['Contact #'.$row['id'].', '.$row['first_name'].' '.$row['last_name']] = array('search_contact'=>$row['id']);
+ 				$ret['Contact #'.$row['id'].', '.$row['first_name'].' '.$row['last_name']] = Utils_RecordBrowserCommon::get_record_href_array('contact',$row['id']);
  		}
 		if(self::Instance()->acl_check('browse companies')) {
 			$result = self::get_companies(array('"~company_name'=>DB::Concat('\'%\'',DB::qstr($word),'\'%\'')));
