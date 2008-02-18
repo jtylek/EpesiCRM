a8d6407b4fd1cc3d2d3e7987172d1147b8f29df0
Paweł Bukowski <pbukowski@telaxus.com>
2008-02-18 16:45:25 +0000

    bug fix

--- modules/Base/Lang/LangCommon_0.php
+++ modules/Base/Lang/LangCommon_0.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Lang class.
- * 
+ *
  * This class provides translations manipulation.
- * 
+ *
  * @author Paul Bukowski <pbukowski@telaxus.com>
  * @copyright Copyright &copy; 2006, Telaxus LLC
  * @version 1.0
@@ -15,7 +15,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 /**
  * This class provides translations manipulation.
- * Translation files are kept in 'modules/Lang/translations'. 
+ * Translation files are kept in 'modules/Lang/translations'.
  * Http server user should have write access to those files.
  */
 class Base_LangCommon extends ModuleCommon {
@@ -28,8 +28,8 @@ class Base_LangCommon extends ModuleCommon {
 	 * print(self::ts('group or parent module', 'some text'));
 	 * </pre>
 	 * Note that it's slower then t function.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param string group or parent module
 	 * @param string string to translate
 	 * @return string
@@ -37,13 +37,13 @@ class Base_LangCommon extends ModuleCommon {
 	 public static function ts($group, $original, array $arg=array()) {
 		global $translations;
 		$group = str_replace('/','_',$group);
-		
+
 		if(!isset($translations)) {
 			$translations = array();
 			include_once('data/Base_Lang/'.self::get_lang_code().'.php');
 		}
-		
-		if(!array_key_exists($group, $translations) || 
+
+		if(!array_key_exists($group, $translations) ||
 			!array_key_exists($original, $translations[$group])) {
 			$translations[$group][$original] = '';
 			//only first display of the string is not in translations database... slows down loading of the page only once...
@@ -66,43 +66,43 @@ class Base_LangCommon extends ModuleCommon {
 		if (!isset($lang)) $lang = self::get_lang_code();
 		$f = @fopen('data/Base_Lang/'.$lang.'.php', 'w');
 		if(!$f)	return false;
-		
+
 		fwrite($f, "<?php\n");
 		fwrite($f, "/**\n * Translation file.\n * @package epesi-translations\n * @subpackage $lang\n */\n");
 		fwrite($f, 'global $translations;'."\n");
-		foreach($translations as $p=>$xxx) 
+		foreach($translations as $p=>$xxx)
 			foreach($xxx as $k=>$v)
 				fwrite($f, '$translations[\''.addcslashes($p,'\\\'').'\'][\''.addcslashes($k,'\\\'').'\']=\''.addcslashes($v,'\\\'')."';\n");
-		
+
 		fwrite($f, '?>');
 		fclose($f);
 		return true;
 	}
-	
+
 	/**
 	 * For internal use only.
 	 */
 	public static function load() {
-		if(!@include_once('data/Base_Lang/'.self::get_lang_code().'.php')) {
-			global $translations;
+		@include_once('data/Base_Lang/'.self::get_lang_code().'.php');
+		global $translations;
+		if(!is_array($translations))
 			$translations=array();
-		}
 	}
-	
+
 	public static function get_lang_code() {
 		static $lang_code;
 		if(!isset($lang_code)) {
 			if (!Acl::check('Data','View') ||
-				ModuleManager::is_installed('Base/User/Settings')==-1 || 
-				ModuleManager::is_installed('Base/Lang/Administrator')==-1 || 
-				(ModuleManager::is_installed('Base/Lang/Administrator')!=-1 && !Variable::get('allow_lang_change'))) 
+				ModuleManager::is_installed('Base/User/Settings')==-1 ||
+				ModuleManager::is_installed('Base/Lang/Administrator')==-1 ||
+				(ModuleManager::is_installed('Base/Lang/Administrator')!=-1 && !Variable::get('allow_lang_change')))
 					return Variable::get('default_lang');
 			if(class_exists('Base_User_SettingsCommon'))
 				$lang_code = Base_User_SettingsCommon::get('Base_Lang_Administrator','language');
 		}
 		return $lang_code;
 	}
-	
+
 	/**
 	 * For internal use only.
 	 */
@@ -120,7 +120,7 @@ class Base_LangCommon extends ModuleCommon {
 			include_once($directory.'/'.$name);
 			Base_LangCommon::save($langcode);
 		}
-		$translations = $trans_backup; 
+		$translations = $trans_backup;
 	}
 
 	/**
