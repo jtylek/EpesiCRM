e187387069ecc3f31bf7d1baa2c1244e58f3bdb9
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-02-21 15:26:30 +0000

    minor bug fix

--- modules/CRM/Contacts/update_contact.php
+++ modules/CRM/Contacts/update_contact.php
@@ -12,11 +12,11 @@ $contacts = CRM_ContactsCommon::get_contacts(array('company_name'=>array($ret)))
 $res = array();
 if (isset($_POST['parameters']['no_company']) && $_POST['parameters']['no_company']) {
 	foreach($contacts as $k=>$v){
-		$res[$v['id']] = CRM_ContactsCommon::contact_format_default($v, true);
+		$res[$v['id']] = CRM_ContactsCommon::contact_format_no_company($v, true);
 	}
 } else {
 	foreach($contacts as $k=>$v){
-		$res[$v['id']] = CRM_ContactsCommon::contact_format_no_company($v, true);
+		$res[$v['id']] = CRM_ContactsCommon::contact_format_default($v, true);
 	}
 }
 asort($res);
