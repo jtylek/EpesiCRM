13e9530d6b4f167eca9b2f8d84f2973f365762ce
Marcin Steczkiewicz <marcin.steczkiewicz@gmail.com>
2008-04-04 17:21:24 +0000

    Button name (shorter)

--- modules/Base/HomePage/HomePageCommon_0.php
+++ modules/Base/HomePage/HomePageCommon_0.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * HomePage class.
- * 
+ *
  * This class provides saving any page as homepage for each user.
- * 
+ *
  * @author Paul Bukowski <pbukowski@telaxus.com>
  * @copyright Copyright &copy; 2006, Telaxus LLC
  * @version 0.9
@@ -15,7 +15,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Base_HomePageCommon extends ModuleCommon {
 	public static $logged;
-	
+
 	public static function load() {
 		if(!Acl::is_user()) return;
 		$uid = Acl::get_user();
@@ -29,20 +29,20 @@ class Base_HomePageCommon extends ModuleCommon {
 		$_SESSION['client']['__module_vars__'] = unserialize($ret);
 		location(array());
 	}
-	
+
 	public static function save() {
 		if(!Acl::is_user()) return;
 		$uid = Acl::get_user();
 		$url = serialize($_SESSION['client']['__module_vars__']);
 		DB::Replace('home_page',array('user_login_id'=>$uid,'url'=>$url), 'user_login_id',true);
 	}
-	
+
 	public static function menu() {
 		if(Acl::is_user())
-			return array('My settings'=>array('__submenu__'=>1,'Set my epesi home page'=>array('Base_HomePage_save'=>'1','__module__'=>null)));
+			return array('My settings'=>array('__submenu__'=>1,'Set home page'=>array('Base_HomePage_save'=>'1','__module__'=>null)));
 		return array();
 	}
-	
+
 	public static function login_check_init() {
 		self::$logged = Acl::is_user();
 	}
@@ -54,7 +54,7 @@ class Base_HomePageCommon extends ModuleCommon {
 				else Base_BoxCommon::location(Base_BoxCommon::get_main_module_name());
 		}
 	}
-	
+
 	public static function homepage_icon() {
 		Base_ActionBarCommon::add('home','Home',Module::create_href(array('Base_HomePage_load'=>'1')));
 	}
