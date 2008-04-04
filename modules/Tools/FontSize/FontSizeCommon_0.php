2f35ca3ef766933d24c5d97f8ce4567560e9cbd0
Marcin Steczkiewicz <marcin.steczkiewicz@gmail.com>
2008-04-04 17:21:06 +0000

    Buttons names

--- modules/Tools/FontSize/FontSizeCommon_0.php
+++ modules/Tools/FontSize/FontSizeCommon_0.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Tools_FontSize class.
- * 
+ *
  * @author Paul Bukowski <pbukowski@telaxus.com>
  * @copyright Copyright &copy; 2006, Telaxus LLC
  * @version 0.9
@@ -12,7 +12,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Tools_FontSizeCommon extends ModuleCommon {
 	public static function menu() {
-		return array('My settings'=>array('__submenu__'=>1,'Font size'=>array('__submenu__'=>1,'Big'=>array('__url__'=>'javascript:Tools_FontSize.change(3)'),'Normal'=>array('__url__'=>'javascript:Tools_FontSize.change(0)'),'Small'=>array('__url__'=>'javascript:Tools_FontSize.change(-3)'))));
+		return array('My settings'=>array('__submenu__'=>1,'Font size'=>array('__submenu__'=>1,'Font big'=>array('__url__'=>'javascript:Tools_FontSize.change(3)'),'Font normal'=>array('__url__'=>'javascript:Tools_FontSize.change(0)'),'Font small'=>array('__url__'=>'javascript:Tools_FontSize.change(-3)'))));
 	}
 }
 
