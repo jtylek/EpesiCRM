978a5dc553dcccd182ee8ea9736c7940d23380e1
Paweł Bukowski <pbukowski@telaxus.com>
2008-05-12 12:25:22 +0000

    calendar tpls

--- modules/Utils/Calendar/theme/event.js
+++ modules/Utils/Calendar/theme/event.js
@@ -1,3 +1,12 @@
 function event_menu(event_id) {
-    Effect.toggle('event_menu_' + event_id, 'appear', {duration:0.3});
+	var menu = $('event_menu_' + event_id);
+	var ev = $('utils_calendar_event:'+event_id);
+	if(menu.parentNode.id=='Utils_Calendar__event') {
+		menu.style.position = 'absolute';
+		menu.style.zIndex = 21;
+		ev.parentNode.appendChild(menu);
+	}
+	menu.clonePosition(ev,{setHeight: false, setWidth: false, offsetLeft: 10});
+
+	Effect.toggle(menu, 'appear', {duration:0.3});
 }
