8bbdc2214ac1d55599440d42639eff078e466b23
Paweł Bukowski <pbukowski@telaxus.com>
2008-05-28 10:55:05 +0000

    tabbed browser show/hide

--- modules/Utils/TabbedBrowser/tb.js
+++ modules/Utils/TabbedBrowser/tb.js
@@ -10,3 +10,21 @@ tabbed_browser_switch = function(id,max,elem,path){
 		$(path+"_c"+id).className="tabbed_browser_selected";
 	} else eval(elem.getAttribute("original_action"));
 };
+
+tabbed_browser_hide = function(path,id){
+	var x = $(path+"_d"+id);
+	var y = $(path+"_c"+id);
+	if(x && y) {
+		x.hide();
+		y.hide();
+	}
+};
+
+tabbed_browser_show = function(path,id){
+	var x = $(path+"_d"+id);
+	var y = $(path+"_c"+id);
+	if(x && y) {
+		x.show();
+		y.show();
+	}
+};
