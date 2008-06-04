b7cc4e4d1f42b904d94b56b454085b1cb4a9ddfa
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-06-04 02:09:15 +0000

    local time in shoutbox

--- modules/Apps/Shoutbox/refresh.php
+++ modules/Apps/Shoutbox/refresh.php
@@ -8,6 +8,6 @@ $arr = DB::GetAll('SELECT ul.login, asm.message, asm.posted_on FROM apps_shoutbo
 //print it out
 foreach($arr as $row) {
 	if(!$row['login']) $row['login']='Anonymous';
-	print(Base_LangCommon::ts('Apps_Shoutbox','<span class="time">[%s]</span> <span class="author">%s</span>: %s',array(date('j M H:i',DB::UnixTimeStamp($row['posted_on'])), $row['login'], $row['message'])).'<br>');
+	print(Base_LangCommon::ts('Apps_Shoutbox','<span class="time">[%s]</span> <span class="author">%s</span>: %s',array(date('j M H:i',strtotime(Base_RegionalSettingsCommon::time2reg(DB::UnixTimeStamp($row['posted_on']),true,true,true,false))), $row['login'], $row['message'])).'<br>');
 }
 ?>
