22755216d1d6b8f28a6b55d7856d6a2b988e8057
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-06-10 15:28:54 +0000

    Improved calendar applet

--- modules/CRM/Calendar/CalendarCommon_0.php
+++ modules/CRM/Calendar/CalendarCommon_0.php
@@ -78,7 +78,7 @@ class CRM_CalendarCommon extends ModuleCommon {
 	public static function applet_settings() {
 		$cols = CRM_Calendar_EventCommon::get_available_colors();
 		$cols[0] = 'All';
-		return array(	array('name'=>'days', 'label'=>'Look for events in', 'type'=>'select', 'default'=>'7', 'values'=>array('1'=>'1 day','2'=>'2 days','3'=>'3 days','5'=>'5 days','7'=>'1 week','14'=>'2 weeks')),
+		return array(	array('name'=>'days', 'label'=>'Look for events in', 'type'=>'select', 'default'=>'7', 'values'=>array('1'=>'1 day','2'=>'2 days','3'=>'3 days','5'=>'5 days','7'=>'1 week','14'=>'2 weeks', '30'=>'1 month', '61'=>'2 months')),
 						array('name'=>'color', 'label'=>'Only events with selected color', 'type'=>'select', 'default'=>'0', 'values'=>$cols));
 	}
 
