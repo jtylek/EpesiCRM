94b4308f50a1e79e948ffee8f1bdf6007a5b49b7
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-06-16 18:49:48 +0000

    bug fix

--- modules/Applets/MonthView/MonthViewCommon_0.php
+++ modules/Applets/MonthView/MonthViewCommon_0.php
@@ -18,13 +18,6 @@ class Applets_MonthViewCommon extends ModuleCommon {
 		return "Displays Month and marks days with events";
 	}
 
-	public static function applet_settings() {
-		$cols = CRM_Calendar_EventCommon::get_available_colors();
-		$cols[0] = 'All';
-		return array(	array('name'=>'days', 'label'=>'Look for events in', 'type'=>'select', 'default'=>'7', 'values'=>array('1'=>'1 day','2'=>'2 days','3'=>'3 days','5'=>'5 days','7'=>'1 week','14'=>'2 weeks', '30'=>'1 month', '61'=>'2 months')),
-						array('name'=>'color', 'label'=>'Only events with selected color', 'type'=>'select', 'default'=>'0', 'values'=>$cols));
-	}
-
 }
 
 ?>
