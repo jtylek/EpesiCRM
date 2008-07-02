c07d43661f05b0ec4c15aeb5a1a729ce3ac2af61
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-07-02 12:56:43 +0000

    bug fix

--- modules/Tests/Calendar/Event/EventCommon_0.php
+++ modules/Tests/Calendar/Event/EventCommon_0.php
@@ -10,6 +10,10 @@
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Tests_Calendar_EventCommon extends Utils_Calendar_EventCommon {
+	public static function get_event_days($start,$end) {
+		return array();
+	}
+
 	public static function get_all($start,$end) {
 		return DB::GetAll('SELECT \'\' as additional_info,\'\' as additional_info2, color,start,duration,title,description,id,timeless FROM tests_calendar_event WHERE ((start>=%d AND start<%d))',array($start,$end));
 	}
