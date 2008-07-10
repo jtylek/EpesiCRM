204318b30e006777a9efb847117944c897d3c2d5
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-07-10 14:17:59 +0000

    Shoutbox - message coloring depending on message age

--- modules/Apps/Shoutbox/refresh.php
+++ modules/Apps/Shoutbox/refresh.php
@@ -8,6 +8,13 @@ $arr = DB::GetAll('SELECT ul.login, asm.message, asm.posted_on FROM apps_shoutbo
 //print it out
 foreach($arr as $row) {
 	if(!$row['login']) $row['login']='Anonymous';
-	print(Base_LangCommon::ts('Apps_Shoutbox','<span class="time">[%s]</span> <span class="author">%s</span>: %s',array(date('j M H:i',strtotime(Base_RegionalSettingsCommon::time2reg(DB::UnixTimeStamp($row['posted_on']),true,true,true,false))), $row['login'], $row['message'])).'<br>');
+	$daydiff = floor((time()-strtotime($row['posted_on']))/86400);
+	switch (true) {
+		case ($daydiff<1): $fcolor = '#000000'; break;
+		case ($daydiff<3): $fcolor = '#444444'; break;
+		case ($daydiff<7): $fcolor = '#888888'; break;
+		default : $fcolor = '#AAAAAA';
+	}
+	print(Base_LangCommon::ts('Apps_Shoutbox','<span class="time">[%s]</span> <span class="author">%s</span>: %s',array(date('j M H:i',strtotime(Base_RegionalSettingsCommon::time2reg(DB::UnixTimeStamp($row['posted_on']),true,true,true,false))), $row['login'], '<span style="color:'.$fcolor.';">'.$row['message'].'</span>')).'<br>');
 }
 ?>
