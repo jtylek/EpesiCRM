3e88fdf06f0ff4939a0cfc177b1bd4cfdb940340
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-07-15 22:04:34 +0000

    Fixed "today marker" in week/month/year view when local time is different than server time

--- modules/Applets/MonthView/MonthView_0.php
+++ modules/Applets/MonthView/MonthView_0.php
@@ -25,7 +25,7 @@ class Applets_MonthView extends Module {
 		$curmonth = date('m', $date);
 
 		$month = array();
-		$today = date('Y-m-d');
+		$today = date('Y-m-d',strtotime(Base_RegionalSettingsCommon::time2reg(null,false)));
 		$colors = CRM_Calendar_EventCommon::get_available_colors();
 		while (date('m', $currday) != ($curmonth)%12+1) {
 			$week = array();
