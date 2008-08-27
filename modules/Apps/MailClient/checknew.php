0b696aad095a7154dc27b616b7e248be4ee600eb
Paweł Bukowski <pbukowski@telaxus.com>
2008-08-27 13:44:11 +0000

    ob_flush fix?

--- modules/Apps/MailClient/checknew.php
+++ modules/Apps/MailClient/checknew.php
@@ -20,8 +20,8 @@ require_once('Mail/mimeDecode.php');
 
 function message($id,$text) {
 	echo('<script>parent.Apps_MailClient.progress_bar.set_text(parent.$(\''.$_GET['id'].'progresses\'),\''.$id.'\',\''.Epesi::escapeJS($text,false).'\')</script>');
-	@ob_flush();
 	flush();
+	@ob_flush();
 }
 
 function rm_lock($lock) {
@@ -46,8 +46,8 @@ foreach($accounts as $account) {
 	echo('<script>parent.Apps_MailClient.progress_bar.set_text(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\',\''.Epesi::escapeJS($account['mail'],false).'\');');
 	echo('parent.Apps_MailClient.progress_bar.set_progress(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\', 0)</script>');
 }
-@ob_flush();
 flush();
+@ob_flush();
 foreach($accounts as $account) {
 	$host = explode(':',$account['incoming_server']);
 	if(isset($host[1])) $port=$host[1];
@@ -213,8 +213,8 @@ foreach($accounts as $account) {
 		
 		$num++;
 		echo('<script>parent.Apps_MailClient.progress_bar.set_progress(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\', '.ceil($num*100/$count).')</script>');
-		@ob_flush();
 		flush();
+		@ob_flush();
 	}
 	
 	echo('<script>parent.Apps_MailClient.progress_bar.set_progress(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\', 100)</script>');
