1f8cfee2fa4b1791699682260bfd3e9913bd9a1f
Paweł Bukowski <pbukowski@telaxus.com>
2008-08-27 13:49:55 +0000

    flush

--- modules/Apps/MailClient/checknew.php
+++ modules/Apps/MailClient/checknew.php
@@ -21,7 +21,6 @@ require_once('Mail/mimeDecode.php');
 function message($id,$text) {
 	echo('<script>parent.Apps_MailClient.progress_bar.set_text(parent.$(\''.$_GET['id'].'progresses\'),\''.$id.'\',\''.Epesi::escapeJS($text,false).'\')</script>');
 	flush();
-	flush();
 }
 
 function rm_lock($lock) {
@@ -46,7 +45,6 @@ foreach($accounts as $account) {
 	echo('<script>parent.Apps_MailClient.progress_bar.set_text(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\',\''.Epesi::escapeJS($account['mail'],false).'\');');
 	echo('parent.Apps_MailClient.progress_bar.set_progress(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\', 0)</script>');
 }
-flush();
 flush();
 foreach($accounts as $account) {
 	$host = explode(':',$account['incoming_server']);
@@ -213,7 +211,6 @@ foreach($accounts as $account) {
 		
 		$num++;
 		echo('<script>parent.Apps_MailClient.progress_bar.set_progress(parent.$(\''.$_GET['id'].'progresses\'),\''.$account['id'].'\', '.ceil($num*100/$count).')</script>');
-		flush();
 		flush();
 	}
 	
