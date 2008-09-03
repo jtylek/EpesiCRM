35286901c955afa32e61c88b6a4c5ba9d4ff2fa2
Paweł Bukowski <pbukowski@telaxus.com>
2008-09-03 16:45:52 +0000

    qf fix

--- modules/Libs/QuickForm/requires.php
+++ modules/Libs/QuickForm/requires.php
@@ -10,7 +10,7 @@
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 $dir = dirname(__FILE__);
-ini_set('include_path',$dir.'/3.2.9'.PATH_SEPARATOR.ini_get('include_path'));
+ini_set('include_path',ini_get('include_path').PATH_SEPARATOR.$dir.'/3.2.9');
 
 require_once('HTML/QuickForm.php');
 ?>
