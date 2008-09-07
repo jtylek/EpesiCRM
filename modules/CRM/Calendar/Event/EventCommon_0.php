4d62889217d497ed3360df4e79b5b7c4faf1c400
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-09-07 09:04:01 +0000

    bug fix

--- modules/CRM/Calendar/Event/EventCommon_0.php
+++ modules/CRM/Calendar/Event/EventCommon_0.php
@@ -58,6 +58,7 @@ class CRM_Calendar_EventCommon extends Utils_Calendar_EventCommon {
 			if ($action == 'set_in_progress') location(array());
 
 			$values = $def;
+			$values['id'] = $id;
 			$values['date_and_time'] = date('Y-m-d H:i:s');
 			$values['title'] = Base_LangCommon::ts('CRM/Calendar/Event','Follow up: ').$values['title'];
 			$values['status'] = 0;
