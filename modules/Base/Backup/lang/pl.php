f96bbcfe14fa6be60e6f1f034f027de89f7d8ee7
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-09-12 16:33:21 +0000

    pl.php for Base_Backup

---
+++ modules/Base/Backup/lang/pl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage en
+ */
+global $translations;
+$translations['Base_Backup']['Available backups']='Dostêpne kopie zapasowe';
+$translations['Base_Backup']['Name']='Nazwa';
+$translations['Base_Backup']['Version']='Wersja';
+$translations['Base_Backup']['Date']='Data';
+$translations['Base_Backup']['Create backup']='Stwórz kopiê zapasow¹';
+?>
