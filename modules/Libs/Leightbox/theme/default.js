b74e58a0378acebba383de5c7bfd94309227e500
Marcin Steczkiewicz <marcin.steczkiewicz@gmail.com>
2008-09-23 12:36:44 +0000

    Leightbox for Customers size and resizing fixed.

--- modules/Libs/Leightbox/theme/default.js
+++ modules/Libs/Leightbox/theme/default.js
@@ -1,12 +1,20 @@
 function libs_leightbox_resize(elem) {
- 
+
 	var s = elem.style;
 
 	if(s.width == '100%') {
-		s.top = '25%';
-		s.left = '15%';
-		s.width = '70%';
-		s.height = '50%';
+		if(elem.id == "leightbox_customers") {
+			s.top = '5%';
+			s.left = '5%';
+			s.width = '90%';
+			s.height = '90%';
+		}
+		else {
+			s.top = '25%';
+			s.left = '15%';
+			s.width = '70%';
+			s.height = '50%';
+		}
 		s.border = '10px solid #b3b3b3';
 		s.padding = '0px';
 		s.background = 'white';
