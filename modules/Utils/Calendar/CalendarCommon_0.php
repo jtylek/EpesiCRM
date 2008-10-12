00a4d9062973f739a6189ee1541a26ab9497bb4e
Paweł Bukowski <pbukowski@telaxus.com>
2008-10-12 23:02:33 +0000

    iphone agenda

--- modules/Utils/Calendar/CalendarCommon_0.php
+++ modules/Utils/Calendar/CalendarCommon_0.php
@@ -146,19 +146,22 @@ class Utils_CalendarCommon extends ModuleCommon {
 
 		$start = time()+$time_shift;
 		$end = $start + (7 * 24 * 60 * 60)+$time_shift;
-		$columns = array(
-			array('name'=>Base_LangCommon::ts('Utils_Calendar','Start'), 'order'=>'start', 'width'=>10),
-			array('name'=>Base_LangCommon::ts('Utils_Calendar','Duration'), 'order'=>'end', 'width'=>5),
-			array('name'=>Base_LangCommon::ts('Utils_Calendar','Title'), 'order'=>'title','width'=>10));
-		$add_cols = array();
-		if(is_array($settings['custom_agenda_cols'])) {
-			$w = 50/count($settings['custom_agenda_cols']);
-			foreach($settings['custom_agenda_cols'] as $k=>$col) {
-				$columns[] = array('name'=>Base_LangCommon::ts('Utils_Calendar',$col), 'order'=>'cus_col_'.$k,'width'=>$w);
-				$add_cols[] = $k;
+		
+		if(!IPHONE) {
+			$columns = array(
+				array('name'=>Base_LangCommon::ts('Utils_Calendar','Start'), 'order'=>'start', 'width'=>10),
+				array('name'=>Base_LangCommon::ts('Utils_Calendar','Duration'), 'order'=>'end', 'width'=>5),
+				array('name'=>Base_LangCommon::ts('Utils_Calendar','Title'), 'order'=>'title','width'=>10));
+			$add_cols = array();
+			if(is_array($settings['custom_agenda_cols'])) {
+				$w = 50/count($settings['custom_agenda_cols']);
+				foreach($settings['custom_agenda_cols'] as $k=>$col) {
+					$columns[] = array('name'=>Base_LangCommon::ts('Utils_Calendar',$col), 'order'=>'cus_col_'.$k,'width'=>$w);
+					$add_cols[] = $k;
+				}
 			}
 		}
-
+		
 		//add data
 		ob_start();
 		$ret = call_user_func(array(str_replace('/','_',$evmod).'Common','get_all'),date('Y-m-d',$start),date('Y-m-d',$end));
@@ -166,18 +169,38 @@ class Utils_CalendarCommon extends ModuleCommon {
 		if(!is_array($ret))
 			trigger_error('Invalid return of event method: get_all (not an array)',E_USER_ERROR);
 
-		$data = array();
+		if(IPHONE) {
+			print('<ul>');
+			$date = null;
+		} else {
+			$data = array();
+		}
 		foreach($ret as $row) {
 			$ex = Utils_CalendarCommon::process_event($row);
 
-			$rrr = array(array('label'=>$ex['start'],'order_value'=>isset($row['timeless'])?strtotime($row['timeless']):$row['start']),Utils_TooltipCommon::create($ex['duration'],$ex['end'],false),$row['title']);
-			foreach($add_cols as $a)
-				$rrr[] = $row['custom_agenda_col_'.$a];
+			if(IPHONE) {
+				if($date!==$ex['start_date']) {
+					$date=$ex['start_date'];
+					print('</ul><h4>'.$date.'</h4><ul>');
+				}
+				$start = '<a>'.$ex['start'].'</a>';
+				$duration = '<a>'.$ex['duration'].'</a>';
+				$title = '<a>'.$row['title'].'</a>';
+				print('<li class="arrow">'.$start.$duration.$title.'</li>');
+			} else {
+				$rrr = array(array('label'=>$ex['start'],'order_value'=>isset($row['timeless'])?strtotime($row['timeless']):$row['start']),Utils_TooltipCommon::create($ex['duration'],$ex['end'],false),$row['title']);
+				foreach($add_cols as $a)
+					$rrr[] = $row['custom_agenda_col_'.$a];
 
-			$data[] = $rrr;
+				$data[] = $rrr;
+			}
 		}
 
-		Utils_GenericBrowserCommon::mobile_table($columns,$data,'start');
+		if(IPHONE) {
+			print('</ul>');
+		} else {
+			Utils_GenericBrowserCommon::mobile_table($columns,$data,'start');
+		}
 	}
 }
 
