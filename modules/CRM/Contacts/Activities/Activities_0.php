23d01526fd3d9949995e5b05f957533371096497
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-10-22 10:19:12 +0000

    Activities filters bug fixes

--- modules/CRM/Contacts/Activities/Activities_0.php
+++ modules/CRM/Contacts/Activities/Activities_0.php
@@ -33,10 +33,17 @@ class CRM_Contacts_Activities extends Module {
 		if ($this->activities_date==0) $date_filter = ' cce.starts>'.Base_RegionalSettingsCommon::reg2time(date('Y-m-d 0:00:00')).' AND';
 		if ($this->activities_date==1) $date_filter = ' cce.ends<'.Base_RegionalSettingsCommon::reg2time(date('Y-m-d 0:00:00')).' AND';
 		// TODO: recurring events
-		if ($this->display['events']) $events = DB::GetAll('SELECT * FROM crm_calendar_event AS cce WHERE'.$date_filter.' true AND (EXISTS (SELECT contact FROM crm_calendar_event_group_emp AS ccegp WHERE ccegp.id=cce.id AND (false'.$db_string.')) OR EXISTS (SELECT contact FROM crm_calendar_event_group_cus AS ccegc WHERE ccegc.id=cce.id AND (false'.$db_string.'))) ORDER BY starts DESC', array_merge($ids, $ids));
+		// TODO: check if statsu<2 if fine for closed
+		if ($this->display['events']) $events = DB::GetAll('SELECT * FROM crm_calendar_event AS cce WHERE'.$date_filter.' '.(!$this->display['closed']?'cce.status<2':'').' AND (EXISTS (SELECT contact FROM crm_calendar_event_group_emp AS ccegp WHERE ccegp.id=cce.id AND (false'.$db_string.')) OR EXISTS (SELECT contact FROM crm_calendar_event_group_cus AS ccegc WHERE ccegc.id=cce.id AND (false'.$db_string.'))) ORDER BY starts DESC', array_merge($ids, $ids));
 		$crits = array('(employees'=>$ids, '|customers'=>$ids);
-		if ($this->activities_date==0) $crits['>=deadline'] = date('Y-m-d');
-		if ($this->activities_date==1) $crits['<deadline'] = date('Y-m-d');
+		if ($this->activities_date==0) {
+			$crits['(>=deadline'] = date('Y-m-d');
+			$crits['|is_deadline'] = '';
+		}
+		if ($this->activities_date==1) {
+			$crits['(<deadline'] = date('Y-m-d');
+			$crits['|is_deadline'] = '';
+		}
 		if (!$this->display['closed']) $crits['!status'] = array(2,3);
 		if ($this->display['tasks']) $tasks = CRM_TasksCommon::get_tasks($crits, array(), array('deadline'=>'DESC'));
 		$crits = array('(employees'=>$ids, '|contact'=>$ids);
@@ -57,10 +64,16 @@ class CRM_Contacts_Activities extends Module {
 		$date_filter = '';
 		if ($this->activities_date==0) $date_filter = ' cce.starts>'.Base_RegionalSettingsCommon::reg2time(date('Y-m-d 0:00:00')).' AND';
 		if ($this->activities_date==1) $date_filter = ' cce.ends<'.Base_RegionalSettingsCommon::reg2time(date('Y-m-d 0:00:00')).' AND';
-		if ($this->display['events']) $events = DB::GetAll('SELECT * FROM crm_calendar_event AS cce WHERE'.$date_filter.' true AND (EXISTS (SELECT contact FROM crm_calendar_event_group_emp AS ccegp WHERE ccegp.id=cce.id AND contact=%d) OR EXISTS (SELECT contact FROM crm_calendar_event_group_cus AS ccegc WHERE ccegc.id=cce.id AND contact=%d)) ORDER BY starts DESC', array($me['id'], $me['id']));
+		if ($this->display['events']) $events = DB::GetAll('SELECT * FROM crm_calendar_event AS cce WHERE'.$date_filter.' '.(!$this->display['closed']?'cce.status<2':'').' AND (EXISTS (SELECT contact FROM crm_calendar_event_group_emp AS ccegp WHERE ccegp.id=cce.id AND contact=%d) OR EXISTS (SELECT contact FROM crm_calendar_event_group_cus AS ccegc WHERE ccegc.id=cce.id AND contact=%d)) ORDER BY starts DESC', array($me['id'], $me['id']));
 		$crits = array('(employees'=>$me['id'], '|customers'=>$me['id']);
-		if ($this->activities_date==0) $crits['>=deadline'] = date('Y-m-d');
-		if ($this->activities_date==1) $crits['<deadline'] = date('Y-m-d');
+		if ($this->activities_date==0) {
+			$crits['(>=deadline'] = date('Y-m-d');
+			$crits['|is_deadline'] = '';
+		}
+		if ($this->activities_date==1) {
+			$crits['(<deadline'] = date('Y-m-d');
+			$crits['|is_deadline'] = '';
+		}
 		if (!$this->display['closed']) $crits['!status'] = array(2,3);
 		if ($this->display['tasks']) $tasks = CRM_TasksCommon::get_tasks($crits, array(), array('deadline'=>'DESC'));
 		$crits = array('(employees'=>$me['id'], '|contact'=>$me['id']);
