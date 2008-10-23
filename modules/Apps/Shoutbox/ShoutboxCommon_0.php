f98397c83e0c95a3ac47f234799b971284509523
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-10-23 12:36:14 +0000

    Shotbox - turned off reporting own comments to Tray

--- modules/Apps/Shoutbox/ShoutboxCommon_0.php
+++ modules/Apps/Shoutbox/ShoutboxCommon_0.php
@@ -22,7 +22,7 @@ class Apps_ShoutboxCommon extends ModuleCommon {
 	}
 
 	public static function tray_notification() {
-		$arr = DB::GetAll('SELECT ul.login, asm.id, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id WHERE asm.posted_on>%T ORDER BY asm.posted_on DESC LIMIT 50',array(time()-24*3600));
+		$arr = DB::GetAll('SELECT ul.login, asm.id, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id WHERE asm.posted_on>%T AND asm.base_user_login_id!=%d ORDER BY asm.posted_on DESC LIMIT 50',array(time()-24*3600, Acl::get_user()));
 		if(empty($arr)) return array();
 		//print it out
 		$ret = array();
