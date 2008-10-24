ced8b3e920d856a090237dc36311be2b73760dde
Arkadiusz Bisaga <ethnar86@gmail.com>
2008-10-24 09:38:36 +0000

    Shoutbox - tray notification bug fix

--- modules/Apps/Shoutbox/ShoutboxCommon_0.php
+++ modules/Apps/Shoutbox/ShoutboxCommon_0.php
@@ -23,7 +23,7 @@ class Apps_ShoutboxCommon extends ModuleCommon {
 
 	public static function tray_notification($time) {
 		if(!$time)
-			$time = $t-24*3600;
+			$time = time()-24*3600;
 		$arr = DB::GetAll('SELECT ul.login, asm.id, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id WHERE asm.posted_on>=%T AND asm.base_user_login_id!=%d ORDER BY asm.posted_on DESC LIMIT 10',array($time, Acl::get_user()));
 		if(empty($arr)) return array();
 		//print it out
