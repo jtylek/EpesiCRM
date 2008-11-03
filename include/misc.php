e64af960e66d24d745ec7dd98fe2bf90ce4ab83c
Paweł Bukowski <pbukowski@telaxus.com>
2008-11-03 12:52:33 +0000

    [ticket]#0319[/ticket] fixed... added EpesiHTML class with static method parse to strip all JS, ids, and everything what can cause html injection...

--- include/misc.php
+++ include/misc.php
@@ -520,4 +520,151 @@ function detect_iphone(){
   }
 }
 
+
+////////////////////////////
+// strip epesi invalid html code
+
+class EpesiHTML {
+	public static function startElement($parser, $name, $attrs) {
+		$r_attrs = '';
+		foreach($attrs as $k=>$v) {
+			if(eregi('(id|onMouseMove|onMouseUp|onMouseOut|onKeyPress|onLoad|onClick|onMouseOver)',$k) || ($name=='A' && strcasecmp($k,'href')==0 && strncasecmp($v,'javascript:',11)==0)) continue;
+			$r_attrs .= ' '.$k.'="'.str_replace(array('"','\\'),array('\"','\\\\'),$v).'"';
+		}
+		echo '<'.$name.$r_attrs.'>';
+	}
+
+	public static function endElement($parser, $name) {
+		echo "</$name>";
+	}
+	
+	public static function characterData($parser, $data) {
+		echo $data;
+	}
+
+	public static function parse($val,$return_error=false) {
+		$xml_parser = xml_parser_create('UTF-8');
+		xml_parser_set_option( $xml_parser, XML_OPTION_CASE_FOLDING, 1 );
+		xml_parser_set_option( $xml_parser, XML_OPTION_SKIP_WHITE, 1 );
+		xml_set_element_handler($xml_parser, array('EpesiHTML',"startElement"), array('EpesiHTML',"endElement"));
+		xml_set_character_data_handler($xml_parser, array('EpesiHTML',"characterData"));
+
+		$val = '<?xml version="1.0" standalone="yes" ?>
+<!DOCTYPE epesi_note [
+<!ENTITY nbsp   "&#160;">
+<!ENTITY iexcl  "&#161;">
+<!ENTITY cent   "&#162;">
+<!ENTITY pound  "&#163;">
+<!ENTITY curren "&#164;">
+<!ENTITY yen    "&#165;">
+<!ENTITY brvbar "&#166;">
+<!ENTITY sect   "&#167;">
+<!ENTITY uml    "&#168;">
+<!ENTITY copy   "&#169;">
+<!ENTITY ordf   "&#170;">
+<!ENTITY laquo  "&#171;">
+<!ENTITY not    "&#172;">
+<!ENTITY shy    "&#173;">
+<!ENTITY reg    "&#174;">
+<!ENTITY macr   "&#175;">
+<!ENTITY deg    "&#176;">
+<!ENTITY plusmn "&#177;">
+<!ENTITY sup2   "&#178;">
+<!ENTITY sup3   "&#179;">
+<!ENTITY acute  "&#180;">
+<!ENTITY micro  "&#181;">
+<!ENTITY para   "&#182;">
+<!ENTITY middot "&#183;">
+<!ENTITY cedil  "&#184;">
+<!ENTITY sup1   "&#185;">
+<!ENTITY ordm   "&#186;">
+<!ENTITY raquo  "&#187;">
+<!ENTITY frac14 "&#188;">
+<!ENTITY frac12 "&#189;">
+<!ENTITY frac34 "&#190;">
+<!ENTITY iquest "&#191;">
+<!ENTITY Agrave "&#192;">
+<!ENTITY Aacute "&#193;">
+<!ENTITY Acirc  "&#194;">
+<!ENTITY Atilde "&#195;">
+<!ENTITY Auml   "&#196;">
+<!ENTITY Aring  "&#197;">
+<!ENTITY AElig  "&#198;">
+<!ENTITY Ccedil "&#199;">
+<!ENTITY Egrave "&#200;">
+<!ENTITY Eacute "&#201;">
+<!ENTITY Ecirc  "&#202;">
+<!ENTITY Euml   "&#203;">
+<!ENTITY Igrave "&#204;">
+<!ENTITY Iacute "&#205;">
+<!ENTITY Icirc  "&#206;">
+<!ENTITY Iuml   "&#207;">
+<!ENTITY ETH    "&#208;">
+<!ENTITY Ntilde "&#209;">
+<!ENTITY Ograve "&#210;">
+<!ENTITY Oacute "&#211;">
+<!ENTITY Ocirc  "&#212;">
+<!ENTITY Otilde "&#213;">
+<!ENTITY Ouml   "&#214;">
+<!ENTITY times  "&#215;">
+<!ENTITY Oslash "&#216;">
+<!ENTITY Ugrave "&#217;">
+<!ENTITY Uacute "&#218;">
+<!ENTITY Ucirc  "&#219;">
+<!ENTITY Uuml   "&#220;">
+<!ENTITY Yacute "&#221;">
+<!ENTITY THORN  "&#222;">
+<!ENTITY szlig  "&#223;">
+<!ENTITY agrave "&#224;">
+<!ENTITY aacute "&#225;">
+<!ENTITY acirc  "&#226;">
+<!ENTITY atilde "&#227;">
+<!ENTITY auml   "&#228;">
+<!ENTITY aring  "&#229;">
+<!ENTITY aelig  "&#230;">
+<!ENTITY ccedil "&#231;">
+<!ENTITY egrave "&#232;">
+<!ENTITY eacute "&#233;">
+<!ENTITY ecirc  "&#234;">
+<!ENTITY euml   "&#235;">
+<!ENTITY igrave "&#236;">
+<!ENTITY iacute "&#237;">
+<!ENTITY icirc  "&#238;">
+<!ENTITY iuml   "&#239;">
+<!ENTITY eth    "&#240;">
+<!ENTITY ntilde "&#241;">
+<!ENTITY ograve "&#242;">
+<!ENTITY oacute "&#243;">
+<!ENTITY ocirc  "&#244;">
+<!ENTITY otilde "&#245;">
+<!ENTITY ouml   "&#246;">
+<!ENTITY divide "&#247;">
+<!ENTITY oslash "&#248;">
+<!ENTITY ugrave "&#249;">
+<!ENTITY uacute "&#250;">
+<!ENTITY ucirc  "&#251;">
+<!ENTITY uuml   "&#252;">
+<!ENTITY yacute "&#253;">
+<!ENTITY thorn  "&#254;">
+<!ENTITY yuml   "&#255;">
+]>'.$val;
+		ob_start();
+		$ret_ok = true;
+		if(!xml_parse($xml_parser, $val, true)) {
+        	echo(sprintf("XML error: %s at line %d",
+                    xml_error_string(xml_get_error_code($xml_parser)),
+                    xml_get_current_line_number($xml_parser)));
+			$ret_ok = false;
+		}
+		$ret = ob_get_clean();
+		xml_parser_free($xml_parser);
+		if(!$ret_ok) {
+			if($return_error)
+				return $ret;
+			else
+				return false;
+		}
+		return $ret;
+	}
+}
 ?>
