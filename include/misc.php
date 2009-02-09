d4f1e4aa871bb09acc2f177cf7d1feb78794b805
Paweł Bukowski <pbukowski@telaxus.com>
2009-02-09 16:34:21 +0000

    command line bug fix

--- include/misc.php
+++ include/misc.php
@@ -356,6 +356,8 @@ function recursive_copy($src, $dest) {
 function escapeJS($str,$double=true,$single=true) {return Epesi::escapeJS($str,$double,$single);}
 
 function get_epesi_url() {
+	if(isset($GLOBALS['argv']))
+		return getcwd();
 	$protocol = (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS'])!== "off") ? 'https://' : 'http://';
 	return $protocol.$_SERVER['HTTP_HOST'].EPESI_DIR;
 }
