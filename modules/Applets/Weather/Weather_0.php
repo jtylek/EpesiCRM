691c32bab39e8dae4fc166746f35d4a54d3173ae
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-02-13 11:40:55 +0000

    Applet/Weather - CSS loading

--- modules/Applets/Weather/Weather_0.php
+++ modules/Applets/Weather/Weather_0.php
@@ -31,6 +31,7 @@ class Applets_Weather extends Module {
 	}
 
 	public function applet($values, $opts) { //available applet options: toggle,href,title,go,go_function,go_arguments,go_contruct_arguments
+		Base_ThemeCommon::load_css('Applets_Weather');
 
 		$opts['title'] = 'Weather';
 
