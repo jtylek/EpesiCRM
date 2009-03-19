d528a4fbf40684c833ab4ccc4d7456302377a278
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-03-19 14:01:44 +0000

    LeightboxPrompt - minor bug fix

--- modules/Utils/LeightboxPrompt/LeightboxPrompt_0.php
+++ modules/Utils/LeightboxPrompt/LeightboxPrompt_0.php
@@ -64,9 +64,9 @@ class Utils_LeightboxPrompt extends Module {
 						if (!empty($params))
 							foreach ($params as $w)
 								$v['form']->addElement('hidden', $this->group.'_'.$w, $w, array('id'=>$this->group.'_'.$w));
-						$v['form']->addElement('button', 'cancel', $this->t('Cancel'), array('onclick'=>count($this->options)==1?'f'.$this->group.'_followups_deactivate();':'$(\''.$this->group.'_buttons_section\').style.display=\'block\';$(\''.$k.'_'.$this->group.'_form_section\').style.display=\'none\';'));
-						$v['form']->addElement('submit', 'submit', $this->t('OK'), array('onclick'=>'f'.$this->group.'_followups_deactivate();'));
 					}
+					$v['form']->addElement('button', 'cancel', $this->t('Cancel'), array('onclick'=>count($this->options)==1?'f'.$this->group.'_followups_deactivate();':'$(\''.$this->group.'_buttons_section\').style.display=\'block\';$(\''.$k.'_'.$this->group.'_form_section\').style.display=\'none\';'));
+					$v['form']->addElement('submit', 'submit', $this->t('OK'), array('onclick'=>'f'.$this->group.'_followups_deactivate();'));
 					ob_start();
 					$th = $this->init_module('Base/Theme');
 					$v['form']->assign_theme('form', $th);
