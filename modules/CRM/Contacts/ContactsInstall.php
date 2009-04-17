432662fc4eaae63485ff843704c4b219de51f18c
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-04-17 12:01:37 +0000

    Tax ID added to company and renamed in Warehouses (from SSN)

--- modules/CRM/Contacts/ContactsInstall.php
+++ modules/CRM/Contacts/ContactsInstall.php
@@ -35,7 +35,8 @@ class CRM_ContactsInstall extends ModuleInstall {
 			array('name'=>'City',			'type'=>'text', 'required'=>true, 'param'=>'64', 'extra'=>false, 'visible'=>true, 'display_callback'=>array('CRM_ContactsCommon','maplink')),
 			array('name'=>'Country',		'type'=>'commondata', 'required'=>true, 'param'=>array('Countries'), 'extra'=>false, 'QFfield_callback'=>array('Data_CountriesCommon', 'QFfield_country')),
 			array('name'=>'Zone',			'type'=>'commondata', 'required'=>false, 'param'=>array('Countries','Country'), 'extra'=>false, 'visible'=>true, 'QFfield_callback'=>array('Data_CountriesCommon', 'QFfield_zone')),
-			array('name'=>'Postal Code',	'type'=>'text', 'required'=>false, 'param'=>'64', 'extra'=>false)
+			array('name'=>'Postal Code',	'type'=>'text', 'required'=>false, 'param'=>'64', 'extra'=>false),
+			array('name'=>'Tax ID', 		'type'=>'text', 'required'=>false, 'param'=>'64', 'extra'=>false)
 		);
 		Utils_RecordBrowserCommon::install_new_recordset('company', $fields);
 // ************ contacts ************** //
