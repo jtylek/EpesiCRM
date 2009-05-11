3e60b6933582def8515c7bf50152d7541e59db88
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-05-11 18:35:17 +0000

    RecordBrowser - minor failsafe

--- modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
+++ modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
@@ -963,8 +963,10 @@ class Utils_RecordBrowserCommon extends ModuleCommon {
 		if (!empty($orderby)) $orderby = ' ORDER BY'.implode(', ',$orderby);
 		else $orderby = '';
 		$final_tab = str_replace('('.$tab.'_data_1 AS r'.')',$tab.'_data_1 AS r',$final_tab);
-		$ret = array('sql'=>' '.$final_tab.' WHERE true'.($admin?Utils_RecordBrowser::$admin_filter:' AND active=1').$where.$having.$orderby,'vals'=>$vals);
-//		print_r($ret);
+		$default_filter = (class_exists('Utils_RecordBrowser') && isset(Utils_RecordBrowser::$admin_filter))?Utils_RecordBrowser::$admin_filter:'';
+		$ret = array('sql'=>' '.$final_tab.' WHERE true'.($admin?$default_filter:' AND active=1').$where.$having.$orderby,'vals'=>$vals);
+		print_r($ret);
+		die();
 		return $cache[$key] = $ret;
 	}
 	public static function get_records_limit( $tab = null, $crits = null, $admin = false) {
