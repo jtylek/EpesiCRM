f22b93bab238bf50c4b4676056d0fcd1bd404193
Marcin Steczkiewicz <marcin.steczkiewicz@gmail.com>
2009-06-09 15:01:05 +0000

    fullScreen mode fixed

--- modules/Base/Box/theme/default.js
+++ modules/Base/Box/theme/default.js
@@ -43,6 +43,8 @@ base_box_roll_topbar = function() {
 
 full_screen = function(URL) {
    //window.open(URL,"","fullscreen,scrollbars");
-   win = window.open(URL, 'epesi', 'fullscreen=yes, scrollbars, menubar=no, toolbar=no, location=no, resizable=yes, status=no, left=0, top=0');
+   var width = screen.width;
+   var height = screen.height;
+   win = window.open(URL, 'epesi', 'fullscreen=yes, scrollbars, menubar=no, toolbar=no, location=no, directories=no, resizable=yes, status=no, left=0, top=0, width=' + width + ', height=' + height);
    win.focus();
 }
