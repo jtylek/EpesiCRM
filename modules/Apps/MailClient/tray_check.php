fe9cd6a3dc3789e369bbe3b7e2f7b3b84a925850
Paweł Bukowski <pbukowski@telaxus.com>
2009-06-17 11:41:30 +0000

    tray check

---
+++ modules/Apps/MailClient/tray_check.php
@@ -0,0 +1,41 @@
+<?php
+header("Content-type: text/javascript");
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // date in the past
+
+if(!isset($_POST['cid']) || !is_numeric($_POST['cid']))
+	die('Client id not defined.');
+
+define('CID',false);
+define('READ_ONLY_SESSION',true);
+define('MAILCLIENT_CACHE',true);
+require_once('../../../include.php');
+ModuleManager::load_modules();
+@set_time_limit(0);
+$mail_size_limit = Variable::get('max_mail_size');
+ini_set("memory_limit",$mail_size_limit+32*1024*1024); // max mail size is
+
+if(!Acl::is_user()) {
+	exit();
+}
+
+$accounts = DB::GetAll('SELECT * FROM apps_mailclient_accounts WHERE user_login_id=%d AND incoming_protocol=1',array(Acl::get_user()));
+if(empty($accounts)) {
+	exit();
+}
+foreach($accounts as $a) {
+	$online = Apps_MailClientCommon::is_online($a['id']);
+	//sync dirs
+	Apps_MailClientCommon::imap_sync_mailbox_dir($a['id']);
+	//sync inbox(without subdirs) messages
+	$local_dirs = Apps_MailClientCommon::get_mailbox_structure($a['id']);
+	$inbox = null;
+	foreach($local_dirs as $k=>$arr)
+		if(strcasecmp($k,'inbox')==0) {
+			$inbox = $k;
+			break;
+		}
+	if($inbox) Apps_MailClientCommon::imap_get_new_messages($a['id'],$inbox.'/');
+}
+
+?>
