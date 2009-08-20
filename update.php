8bd65e838310d212e17116a4c0a2e9d9b0a092e5
Paweł Bukowski <pbukowski@telaxus.com>
2009-08-20 12:57:32 +0000

    1.0.1 release

--- update.php
+++ update.php
@@ -135,7 +135,7 @@ function themeup(){
 	install_default_theme_common_files('modules/Base/Theme/','images');
 }
 
-$versions = array('0.8.5','0.8.6','0.8.7','0.8.8','0.8.9','0.8.10','0.8.11','0.9.0','0.9.1','0.9.9beta1','0.9.9beta2','1.0.0rc1','1.0.0rc2','1.0.0rc3','1.0.0rc4','1.0.0rc5','1.0.0rc6','1.0.0');
+$versions = array('0.8.5','0.8.6','0.8.7','0.8.8','0.8.9','0.8.10','0.8.11','0.9.0','0.9.1','0.9.9beta1','0.9.9beta2','1.0.0rc1','1.0.0rc2','1.0.0rc3','1.0.0rc4','1.0.0rc5','1.0.0rc6','1.0.0','1.0.1');
 
 /****************** 0.8.5 to 0.8.6 **********************/
 function update_from_0_9_9beta1_to_0_9_9beta2() {
@@ -1068,6 +1068,330 @@ function update_from_1_0_0rc6_to_1_0_0() {
 			value C(128)',
 			array('constraints'=>', FOREIGN KEY (filter_id) REFERENCES apps_mailclient_filters(ID)'));
     }    
+
+    //ok, done
+    ModuleManager::create_common_cache();
+    ModuleManager::load_modules();
+    themeup();
+    langup();
+    Base_ThemeCommon::create_cache();
+}
+
+function update_from_1_0_0_to_1_0_1() {
+	ob_start();
+	ModuleManager::load_modules();
+	ob_end_clean();
+
+	if (ModuleManager::is_installed('Utils/RecordBrowser')>=0) {
+		$tabs = DB::GetAssoc('SELECT tab, tab FROM recordbrowser_table_properties');
+		foreach ($tabs as $t) {
+			DB::Execute('UPDATE '.$t.'_field SET type=%s WHERE type=%s', array('float', 'integer'));
+			DB::Execute('UPDATE '.$t.'_field SET style=%s WHERE style=%s', array('number', 'integer'));
+		}
+	}
+
+	if (ModuleManager::is_installed('Data_Countries')>=0) {
+
+		$calling_codes = array(
+			'US'=>'+1',
+			'CA'=>'+1',
+			'BS'=>'+1242',
+			'BB'=>'+1246',
+			'AI'=>'+1264',
+			'AG'=>'+1268',
+			'VG'=>'+1284',
+			'VI'=>'+1340',
+			'KY'=>'+1345',
+			'BM'=>'+1441',
+			'GD'=>'+1473',
+			'TC'=>'+1649',
+			'MS'=>'+1664',
+			'MP'=>'+1670',
+			'GU'=>'+1671',
+			'AS'=>'+1684',
+			'LC'=>'+1758',
+			'DM'=>'+1767',
+			'VC'=>'+1784',
+			'PR'=>'+1787',
+			'DO'=>'+1809',
+			'DO'=>'+1829',
+			'DO'=>'+1849',
+			'TT'=>'+1868',
+			'KN'=>'+1869',
+			'JM'=>'+1876',
+			'PR'=>'+1939',
+			'EG'=>'+20',
+			'MA'=>'+212',
+			'EH'=>'+212',
+			'DZ'=>'+213',
+			'TN'=>'+216',
+			'LY'=>'+218',
+			'GM'=>'+220',
+			'SN'=>'+221',
+			'MR'=>'+222',
+			'ML'=>'+223',
+			'GN'=>'+224',
+			'CI'=>'+225',
+			'BF'=>'+226',
+			'NE'=>'+227',
+			'TG'=>'+228',
+			'BJ'=>'+229',
+			'MU'=>'+230',
+			'LR'=>'+231',
+			'SL'=>'+232',
+			'GH'=>'+233',
+			'NG'=>'+234',
+			'TD'=>'+235',
+			'CF'=>'+236',
+			'CM'=>'+237',
+			'CV'=>'+238',
+			'ST'=>'+239',
+			'GQ'=>'+240',
+			'GA'=>'+241',
+			'CG'=>'+242',
+			'CD'=>'+243',
+			'AO'=>'+244',
+			'GW'=>'+245',
+			'IO'=>'+246',
+			'AC'=>'+247',
+			'SC'=>'+248',
+			'SD'=>'+249',
+			'RW'=>'+250',
+			'ET'=>'+251',
+			'SO'=>'+252',
+			'QS'=>'+252',
+			'DJ'=>'+253',
+			'KE'=>'+254',
+			'TZ'=>'+255',
+			'UG'=>'+256',
+			'BI'=>'+257',
+			'MZ'=>'+258',
+			'ZM'=>'+260',
+			'MG'=>'+261',
+			'RE'=>'+262',
+			'YT'=>'+262',
+			'ZW'=>'+263',
+			'NA'=>'+264',
+			'MW'=>'+265',
+			'LS'=>'+266',
+			'BW'=>'+267',
+			'SZ'=>'+268',
+			'KM'=>'+269',
+			'ZA'=>'+27',
+			'SH'=>'+290',
+			'TA'=>'+290',
+			'ER'=>'+291',
+			'AW'=>'+297',
+			'FO'=>'+298',
+			'GL'=>'+299',
+			'GR'=>'+30',
+			'NL'=>'+31',
+			'BE'=>'+32',
+			'FR'=>'+33',
+			'ES'=>'+34',
+			'GI'=>'+350',
+			'PT'=>'+351',
+			'LU'=>'+352',
+			'IE'=>'+353',
+			'IS'=>'+354',
+			'AL'=>'+355',
+			'MT'=>'+356',
+			'CY'=>'+357',
+			'FI'=>'+358',
+			'AX'=>'+358',
+			'BG'=>'+359',
+			'HU'=>'+36',
+			'LT'=>'+370',
+			'LV'=>'+371',
+			'EE'=>'+372',
+			'MD'=>'+373',
+			'AM'=>'+374',
+			'QN'=>'+374',
+			'BY'=>'+375',
+			'AD'=>'+376',
+			'MC'=>'+377',
+			'SM'=>'+378',
+			'VA'=>'+379',
+			'UA'=>'+380',
+			'RS'=>'+381',
+			'ME'=>'+382',
+			'HR'=>'+385',
+			'SI'=>'+386',
+			'BA'=>'+387',
+			'EU'=>'+388',
+			'MK'=>'+389',
+			'IT'=>'+39',
+			'VA'=>'+39',
+			'RO'=>'+40',
+			'CH'=>'+41',
+			'CZ'=>'+420',
+			'SK'=>'+421',
+			'LI'=>'+423',
+			'AT'=>'+43',
+			'GB'=>'+44',
+			'GG'=>'+44',
+			'IM'=>'+44',
+			'JE'=>'+44',
+			'DK'=>'+45',
+			'SE'=>'+46',
+			'NO'=>'+47',
+			'SJ'=>'+47',
+			'PL'=>'+48',
+			'DE'=>'+49',
+			'FK'=>'+500',
+			'BZ'=>'+501',
+			'GT'=>'+502',
+			'SV'=>'+503',
+			'HN'=>'+504',
+			'NI'=>'+505',
+			'CR'=>'+506',
+			'PA'=>'+507',
+			'PM'=>'+508',
+			'HT'=>'+509',
+			'PE'=>'+51',
+			'MX'=>'+52',
+			'CU'=>'+53',
+			'AR'=>'+54',
+			'BR'=>'+55',
+			'CL'=>'+56',
+			'CO'=>'+57',
+			'VE'=>'+58',
+			'GP'=>'+590',
+			'BL'=>'+590',
+			'MF'=>'+590',
+			'BO'=>'+591',
+			'GY'=>'+592',
+			'EC'=>'+593',
+			'GF'=>'+594',
+			'PY'=>'+595',
+			'MQ'=>'+596',
+			'SR'=>'+597',
+			'UY'=>'+598',
+			'AN'=>'+599',
+			'MY'=>'+60',
+			'AU'=>'+61',
+			'CX'=>'+61',
+			'CC'=>'+61',
+			'ID'=>'+62',
+			'PH'=>'+63',
+			'NZ'=>'+64',
+			'SG'=>'+65',
+			'TH'=>'+66',
+			'TL'=>'+670',
+			'NF'=>'+672',
+			'AQ'=>'+672',
+			'BN'=>'+673',
+			'NR'=>'+674',
+			'PG'=>'+675',
+			'TO'=>'+676',
+			'SB'=>'+677',
+			'VU'=>'+678',
+			'FJ'=>'+679',
+			'PW'=>'+680',
+			'WF'=>'+681',
+			'CK'=>'+682',
+			'NU'=>'+683',
+			'WS'=>'+685',
+			'KI'=>'+686',
+			'NC'=>'+687',
+			'TV'=>'+688',
+			'PF'=>'+689',
+			'TK'=>'+690',
+			'FM'=>'+691',
+			'MH'=>'+692',
+			'RU'=>'+7',
+			'KZ'=>'+7',
+			'XT'=>'+800',
+			'XS'=>'+808',
+			'JP'=>'+81',
+			'KR'=>'+82',
+			'VN'=>'+84',
+			'KP'=>'+850',
+			'HK'=>'+852',
+			'MO'=>'+853',
+			'KH'=>'+855',
+			'LA'=>'+856',
+			'CN'=>'+86',
+			'XN'=>'+870',
+			'PN'=>'+872',
+			'XP'=>'+878',
+			'BD'=>'+880',
+			'XG'=>'+881',
+			'XV'=>'+882',
+			'XL'=>'+883',
+			'TW'=>'+886',
+			'XD'=>'+888',
+			'TR'=>'+90',
+			'QY'=>'+90',
+			'IN'=>'+91',
+			'PK'=>'+92',
+			'AF'=>'+93',
+			'LK'=>'+94',
+			'MM'=>'+95',
+			'MV'=>'+960',
+			'LB'=>'+961',
+			'JO'=>'+962',
+			'SY'=>'+963',
+			'IQ'=>'+964',
+			'KW'=>'+965',
+			'SA'=>'+966',
+			'YE'=>'+967',
+			'OM'=>'+968',
+			'PS'=>'+970',
+			'AE'=>'+971',
+			'IL'=>'+972',
+			'PS'=>'+972',
+			'BH'=>'+973',
+			'QA'=>'+974',
+			'BT'=>'+975',
+			'MN'=>'+976',
+			'NP'=>'+977',
+			'XR'=>'+979',
+			'IR'=>'+98',
+			'XC'=>'+991',
+			'TJ'=>'+992',
+			'TM'=>'+993',
+			'AZ'=>'+994',
+			'QN'=>'+994',
+			'GE'=>'+995',
+			'KG'=>'+996',
+			'UZ'=>'+998');
+		Utils_CommonDataCommon::new_array('Calling_Codes',$calling_codes);
+	}
+	
+	if(DATABASE_DRIVER=='mysqlt' || DATABASE_DRIVER=='mysqli') {
+		DB::Execute('SET FOREIGN_KEY_CHECKS=0');
+
+		$tables = DB::MetaTables();
+		foreach($tables as $t) {
+		        $ret = DB::GetRow('SHOW CREATE TABLE '.$t);
+			if(ereg('utf8_unicode_ci',$ret[1])) continue;
+			$cols = explode("\n",$ret[1]);
+		        foreach($cols as $c) {
+				if(ereg('(varchar|text)',$c) && !ereg('latin',$c)) {
+					if($t=='aro' || $t=='axo') $c = str_replace('240','160',$c);
+					DB::Execute('ALTER TABLE `'.$t.'` MODIFY '.rtrim($c,',').' COLLATE utf8_unicode_ci');
+				}
+			}
+			$mcols = DB::MetaColumns($t);
+			foreach($mcols as $c) {
+				if(isset($mcols['ID']) && ($c->type=="text" || $c->type=="varchar")) {
+					DB::Execute('SET NAMES latin1');
+					$data = DB::GetAssoc('SELECT id,'.$c->name.' FROM '.$t);
+					DB::Execute('SET NAMES utf8');
+					foreach($data as $id=>$val) {
+						DB::Execute('UPDATE '.$t.' SET '.$c->name.'=%s WHERE id=%s',array($val,$id));
+					}
+				}
+			}
+		}
+		DB::Execute('DELETE FROM history');
+		DB::Execute('DELETE FROM session_client');
+		DB::Execute('DELETE FROM session');
+
+		if (ModuleManager::is_installed('Base_HomePage')>=0)
+			DB::Execute('DELETE FROM home_page');
+	}
 
     //ok, done
     ModuleManager::create_common_cache();
