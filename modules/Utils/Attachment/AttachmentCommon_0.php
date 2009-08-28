9b584c0c1f6df7a9d33626b40542fe40647fc428
Paweł Bukowski <pbukowski@telaxus.com>
2009-08-28 17:38:44 +0000

    bug fix

--- modules/Utils/Attachment/AttachmentCommon_0.php
+++ modules/Utils/Attachment/AttachmentCommon_0.php
@@ -58,6 +58,7 @@ class Utils_AttachmentCommon extends ModuleCommon {
 	public static function add($group,$permission,$user,$note=null,$oryg=null,$file=null,$func=null,$args=null,$add_func=null,$add_args=array()) {
 		DB::Execute('INSERT INTO utils_attachment_link(local,permission,permission_by,func,args) VALUES(%s,%d,%d,%s,%s)',array($group,$permission,$user,serialize($func),serialize($args)));
 		$id = DB::Insert_ID('utils_attachment_link','id');
+		if($oryg===null) $oryg = '';
 		DB::Execute('INSERT INTO utils_attachment_file(attach_id,original,created_by,revision) VALUES(%d,%s,%d,0)',array($id,$oryg,$user));
 		DB::Execute('INSERT INTO utils_attachment_note(attach_id,text,created_by,revision) VALUES(%d,%s,%d,0)',array($id,$note,$user));
 		if($file) {
