e22c433d75bc7203be6711dbd448c4133b67e13f
Janusz Tylek <jtylek@telaxus.com>
2009-09-11 22:39:03 +0000

    Icon and description added

--- modules/Base/Error/ErrorInstall.php
+++ modules/Base/Error/ErrorInstall.php
@@ -13,6 +13,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Base_ErrorInstall extends ModuleInstall {
 	public function install() {
+		Base_ThemeCommon::install_default_theme($this->get_type());
 		Base_LangCommon::install_translations($this->get_type());
 		Variable::set('error_mail','');
 		$this->create_data_dir();
@@ -26,6 +27,14 @@ class Base_ErrorInstall extends ModuleInstall {
 	
 	public function version() {
 		return array('1.0.0');
+	}
+
+	// ************************************
+	public static function info() {
+		return array(
+			'Description'=>'Error Reporting',
+			'Author'=>'pbukowski@telaxus.com',
+			'License'=>'MIT');
 	}
 
 	public function requires($v) {
