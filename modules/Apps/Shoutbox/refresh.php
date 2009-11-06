586e29438ed6f0bd5196343e86329549b805191b
Paweł Bukowski <pbukowski@telaxus.com>
2009-11-06 08:34:17 +0000

    shoutbox fix

--- modules/Apps/Shoutbox/refresh.php
+++ modules/Apps/Shoutbox/refresh.php
@@ -13,6 +13,9 @@ define('CID',false);
 require_once('../../../include.php');
 ModuleManager::load_modules();
 
+if(!Acl::is_user())
+	exit();
+
 //get last 20 messages
 $arr = DB::GetAll('SELECT ul.login, asm.message, asm.posted_on FROM apps_shoutbox_messages asm LEFT JOIN user_login ul ON ul.id=asm.base_user_login_id ORDER BY asm.posted_on DESC LIMIT 20');
 //print it out
