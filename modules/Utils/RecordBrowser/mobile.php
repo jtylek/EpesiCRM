9c83d8559f61f9dc41b7e3f9413f77315c25a1e9
Paweł Bukowski <pbukowski@telaxus.com>
2009-11-14 19:39:46 +0000

    relations fix

--- modules/Utils/RecordBrowser/mobile.php
+++ modules/Utils/RecordBrowser/mobile.php
@@ -2,7 +2,7 @@
 /**
  * RecordBrowserCommon class.
  *
- * @author Arkadiusz Bisaga <abisaga@telaxus.com>
+ * @author Paul Bukowski <pbukowski@telaxus.com> and Arkadiusz Bisaga <abisaga@telaxus.com>
  * @copyright Copyright &copy; 2008, Telaxus LLC
  * @license MIT
  * @version 1.0
