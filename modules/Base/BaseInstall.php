0e6f0277072a7a9019148a34808647c056b02907
Adam Bukowski <abukowski@telaxus.com>
2009-12-07 21:49:26 +0000

    Base Backup dependecies fix

--- modules/Base/BaseInstall.php
+++ modules/Base/BaseInstall.php
@@ -41,7 +41,6 @@ class BaseInstall extends ModuleInstall {
 		    array('name'=>'Base/About','version'=>0),
 		    array('name'=>'Base/ActionBar','version'=>0),
 		    array('name'=>'Base/Dashboard','version'=>0),
-		    array('name'=>'Base/Backup','version'=>0),
 		    array('name'=>'Base/Setup','version'=>0),
 		    array('name'=>'Base/Lang/Administrator','version'=>0),
 		    array('name'=>'Base/Mail/ContactUs','version'=>0),
