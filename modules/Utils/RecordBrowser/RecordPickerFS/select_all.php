deec2926ae57e0a54bd7e2183e2e94caedd8ab5b
Paweł Bukowski <pbukowski@telaxus.com>
2010-02-18 13:26:34 +0000

    bug fix

--- modules/Utils/RecordBrowser/RecordPickerFS/select_all.php
+++ modules/Utils/RecordBrowser/RecordPickerFS/select_all.php
@@ -16,6 +16,7 @@ require_once('../../../../include.php');
 foreach ($_POST as $k=>$v)
 	$_POST[$k] = trim($v,'"');
 $path = $_POST['path'];
+$select = json_decode($_POST['select']);
 $tab = Module::static_get_module_variable($path, 'tab', null);
 $crits = Module::static_get_module_variable($path, 'crits_stuff', null);
 $rp_path = Module::static_get_module_variable($path, 'rp_fs_path', null);
@@ -25,13 +26,14 @@ if ($tab===null || $crits===null || $rp_path===null) die('alert(\'Invalid usage 
 
 $tab_info = Utils_RecordBrowserCommon::init($tab);
 $records = Utils_RecordBrowserCommon::get_records($tab, $crits, array('id'));
-$selected = array();
-if($_POST['select']) {
-	foreach($records as $r) {
+foreach($records as $r) {
+	if($select) {
 		$selected[$r['id']] = 1;
+	} else {
+		unset($selected[$r['id']]);
 	}
 }
 session_commit();
 
-print('Epesi.procOn--;_chj(\'\');');
+print('Epesi.procOn--;_chj(\'\',\'\',\'queue\');');
 ?>
