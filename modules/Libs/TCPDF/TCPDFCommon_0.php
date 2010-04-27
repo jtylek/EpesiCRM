53af4d68caf04d7acca5dcc53af9af8b10721785
Paweł Bukowski <pbukowski@telaxus.com>
2010-04-27 08:51:57 +0000

    pdf logo height fixes

--- modules/Libs/TCPDF/TCPDFCommon_0.php
+++ modules/Libs/TCPDF/TCPDFCommon_0.php
@@ -58,12 +58,16 @@ class Libs_TCPDFCommon extends ModuleCommon {
 
     public function prepare_header(& $tcpdf, $title='', $subject='', $printed_by=true, $logo_filename=null) {
         if ($logo_filename===null) $logo_filename = Libs_TCPDFCommon::get_logo_filename();
-        if (!file_exists($logo_filename)) $logo_filename = Base_ThemeCommon::get_template_file('Libs/TCPDF','logo-small.png');
-        $logo_size = getimagesize($logo_filename);
-        $logo_width = $tcpdf->getHTMLUnitToUnits(PDF_HEADER_LOGO_WIDTH,1,'mm');
-        $margins = $tcpdf->getMargins();
-        $logo_height = $logo_size[1]*$logo_width/$logo_size[0];
-        $tcpdf->SetTopMargin($logo_height+$margins['top']);
+        $default_filename = Base_ThemeCommon::get_template_file('Libs/TCPDF','logo-small.png');
+        if (!file_exists($logo_filename)) $logo_filename = $default_filename;
+        else {
+            $logo_size = getimagesize($logo_filename);
+//            $default_size = getimagesize($default_filename);
+//            $default_height = $default_size[1]*PDF_HEADER_LOGO_WIDTH/$default_size[0];
+            $margins = $tcpdf->getMargins();
+            $logo_height = $logo_size[1]*PDF_HEADER_LOGO_WIDTH/$logo_size[0];
+            $tcpdf->SetTopMargin($logo_height+10);//+$margins['top']-$default_height);
+        }
         if ($title!==null) $tcpdf->SetHeaderData($logo_filename, PDF_HEADER_LOGO_WIDTH, $title, $subject);
 
         //set some language-dependent strings
