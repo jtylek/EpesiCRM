19d3bff6b24dbce9c6e3cb6bda30725a81664b44
Adam Bukowski <abukowski@telaxus.com>
2010-06-13 12:52:53 +0000

    Module downloader - install modules

--- modules/Base/ModuleDownloader/ModuleDownloaderInstall.php
+++ modules/Base/ModuleDownloader/ModuleDownloaderInstall.php
@@ -12,18 +12,22 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Base_ModuleDownloaderInstall extends ModuleInstall {
 	public function install() {
+        Base_ThemeCommon::install_default_theme($this->get_type());
+
         if($this->create_data_dir())
             return true;
         return false;
 	}
 	
 	public function uninstall() {
+        Base_ThemeCommon::uninstall_default_theme($this->get_type());
+
         $this->remove_data_dir();
 		return true;
 	}
 
     public function version() {
-        return array("0.1");
+        return array("0.9");
     }
 	public function requires($v) {
 		return array(
--- modules/Base/ModuleDownloader/ModuleDownloader_0.php
+++ modules/Base/ModuleDownloader/ModuleDownloader_0.php
@@ -18,14 +18,14 @@ class Base_ModuleDownloader extends Module {
 
     public function navigate($func, $params = array()) {
         $x = ModuleManager::get_instance('/Base_Box|0');
-        if(!$x) trigger_error('There is no base box module instance',E_USER_ERROR);
+        if (!$x) trigger_error('There is no base box module instance',E_USER_ERROR);
         $x->push_main('Base_ModuleDownloader', $func, $params);
         return false;
     }
 
     public function pop_main() {
         $x = ModuleManager::get_instance('/Base_Box|0');
-        if(!$x) trigger_error('There is no base box module instance',E_USER_ERROR);
+        if (!$x) trigger_error('There is no base box module instance',E_USER_ERROR);
         $x->pop_main();
     }
 
@@ -34,74 +34,193 @@ class Base_ModuleDownloader extends Module {
         $form = $this->init_module('Libs/QuickForm');
         $form->addElement('text', 'mid', 'Download Code');
         $form->addElement('submit', 'submit', 'Download');
-        if($form->validate()) {
+        if ($form->validate()) {
             $this->navigate('processQuery', array($form->exportValue('mid')));
         }
         $form->display();
 	}
 
     protected function processQuery($mid /* Module ID */) {
-        if($this->is_back()) {
+        if ($this->is_back()) {
             $this->pop_main();
+            return;
         }
         Base_ActionBarCommon::add('back', 'Back', $this->create_back_href());
 
+        /* cache last module list to decrease server requests */
+        $modules_id = $this->get_module_variable('mid', null);
+        $modules = $this->get_module_variable('modules', array());
         /* request modules list */
-        $modules = unserialize($this->request(array('action'=>'list', 'id'=>$mid)));
+        if ( $modules_id != $mid ) {
+            $modules = unserialize($this->request(array('action'=>'list', 'id'=>$mid)));
+            $this->set_module_variable('modules', $modules);
+            $this->set_module_variable('mid', $mid);
+        }
 
-        if($modules === false) {
+        /* check code */
+        if ($modules === false) {
             print($this->t('This code is not valid!'));
-        } elseif($modules === true) {
+        } elseif ($modules === true) {
             print($this->t('This code is no longer valid! Please request appropriate person for valid code!').'<br/>');
         } else {
-            print($this->t('Click "Download now!" to download listed modules.<br/>If you have those modules, in "modules" directory they will be <b>overwritten</b>!'));
+            print($this->t('Click "Download now!" to download listed modules.<br/>If you have those modules in "modules" directory, they will be <b>overwritten</b>!'));
             print("<ul>");
             foreach($modules as $m) {
                 print('<li>'. $m .'</li>');
             }
             print("</ul>");
+
             $form = $this->init_module('Libs/QuickForm');
-            if($form->validate()) {
-                $this->processDownload($mid);
-            } else $form->addElement('submit', 'submit', 'Download Now!');
+
+            /* get result of downloading - display button to download if no result */
+            $download_info = $this->get_module_variable('download_info', '');
+            if ($download_info) {
+                echo $download_info . '<br/>';
+                /* check if downloading succeded */
+                if($this->get_module_variable('download_success', false)) {
+                    /* refresh available modules */
+                    if ( $this->get_module_variable('modules_refreshed', false) === false) {
+                        Base_SetupCommon::refresh_available_modules();
+                        $this->set_module_variable('modules_refreshed', true);
+                    }
+
+                    /* display installation process results */
+                    $install_info = $this->get_module_variable('install_info', '');
+                    echo $install_info. '<br/><br/>';
+
+                    /* check every module if it's installed already
+                     * count not installed modules and display for every install
+                     * button and if count > 1 display "Install all"
+                     */
+                    $install_cnt = 0;
+                    $modules_to_install = array();
+                    $html = '';
+                    foreach ($modules as $mo) {
+                        /* obtain module name */
+                        $m = rtrim($mo, DIRECTORY_SEPARATOR);
+                        $m = str_replace(DIRECTORY_SEPARATOR, '_', $m);
+                        $inst = ModuleManager::is_installed($m) === -1;
+                        $html .= '<tr><td>'.$mo.'</td><td class="element_button" style="text-align: center">';
+                        /* if installed print button to form */
+                        if($inst) {
+                            $el = $form->createElement('button', 'install', 'Install', $this->create_callback_href(array($this, 'install'), array(array($m))));
+                            $html .= $el->toHtml();
+                            $install_cnt ++;
+                            $modules_to_install[] = $m;
+                        }
+                        else $html .= '<b>already installed</b>';
+                        $html .= '</td></tr>';
+
+                    }
+                    /* add prepared list of modules with buttons */
+                    $form->addElement('html', $html);
+
+                    /* add "Install All" and "Restart" button */
+                    if($install_cnt > 1) $form->addElement('button', 'install', 'Install All', $this->create_callback_href(array($this, 'install'), array($modules)));
+                    $form->addElement('button', 'restart', 'Restart Epesi', 'onclick="document.location=\'\'"');
+                }
+            } else {
+                $form->addElement('button', 'download', 'Download Now!', $this->create_callback_href(array($this, 'processDownload'), array($mid)));
+            }
+
             $form->display();
             return false;
         }
     }
 
     protected function processDownload($mid /* Module ID */) {
+        $success = true;
+        $text = '';
         // make temp destination filename
         $destfile = $this->get_data_dir() . escapeshellcmd($mid);
         while(file_exists($destfile.'.zip')) $destfile .= '0';
         $destfile .= '.zip';
         // download file
         if( ($ret = $this->request(array('action'=>'file', 'id'=>$mid), $destfile)) === true ) {
-            print($this->t('File download succeded!').'<br/>');
+            $text .= $this->t('File download succeded!').'<br/>';
         } else {
-            print($this->t('Error downloading file!').'<br/>');
-            return;
+            $text .= $this->t('Error downloading file!').'<br/>';
+            $success = false;
         }
         // request md5 sum
-        $md5 = unserialize($this->request(array('action'=>'md5', 'id'=>$mid)));
-        if( $md5 == md5_file($destfile) ) {
-            print('File consistency  OK');
-        } else {
-            print('File consistency  <b>Error</b>');
-            return;
+        if($success) {
+            $md5 = unserialize($this->request(array('action'=>'md5', 'id'=>$mid)));
+            if( $md5 == md5_file($destfile) ) {
+                $text .= $this->t('File consistency  OK').'<br/>';
+            } else {
+                $text .= $this->t('File consistency  <b>Error</b>').'<br/>';
+                $success = false;
+            }
         }
         // extract file contents
-        if(class_exists('ZipArchive')) {
-            $zip = new ZipArchive();
-            if( filesize($destfile) == 0 || $zip->open($destfile) != true || $zip->extractTo('./modules') == false ) {
-                print($this->t("Archive error!").'<br/>');
+        if($success) {
+            if(class_exists('ZipArchive')) {
+                $zip = new ZipArchive();
+                if( filesize($destfile) == 0 || $zip->open($destfile) != true || $zip->extractTo('./modules') == false ) {
+                    $text .= $this->t("Archive error!").'<br/>';
+                    $success = false;
+                } else {
+                    $zip->close();
+                }
             } else {
-                $zip->close();
+                $text .= $this->t("Please enable zip extension in server configuration!").'<br/>';
+                $success = false;
             }
-        } else {
-            print($this->t("Please enable zip extension in server configuration!").'<br/>');
         }
         // remove file
-        unlink($destfile);
+        @unlink($destfile);
+        $this->set_module_variable('download_success', $success);
+        $this->set_module_variable('download_info', $text);
+    }
+
+    public function install($modules) {
+        $text = '<b>Installation process:</b><br/>';
+
+        foreach ($modules as $mo) {
+            $m = rtrim($mo, DIRECTORY_SEPARATOR);
+            $m = str_replace(DIRECTORY_SEPARATOR, '_', $m);
+            $success = ModuleManager::install($m);
+            $text .= $mo . ' - <b>' . ($success ? 'Success!' : 'Failure!') . '</b><br/>';
+        }
+
+        $processed = ModuleManager::get_processed_modules();
+        if(is_array($processed['install']) && sizeof($processed['install'])) {
+            $this->navigate('post_install', array($processed['install']));
+        }
+
+        $this->set_module_variable('install_info', $text);
+    }
+
+    function post_install($modules) {
+        if (! $this->isset_module_variable('post_install')) $this->set_module_variable('post_install', $modules);
+
+        $post_install = & $this->get_module_variable('post_install', array());
+
+        foreach($post_install as $i=>$v) {
+            ModuleManager::include_install($i);
+            $f = array($i.'Install','post_install');
+            $fs = array($i.'Install','post_install_process');
+            if(!is_callable($f) || !is_callable($fs)) {
+                unset($post_install[$i]);
+                continue;
+            }
+            $ret = call_user_func($f);
+            $form = $this->init_module('Libs/QuickForm',null,$i);
+            $form->addElement('header',null,'Post installation of '.str_replace('_','/',$i));
+            $form->add_array($ret);
+            $form->addElement('submit',null,'OK');
+            if($form->validate()) {
+                $form->process($fs);
+                unset($post_install[$i]);
+            } else {
+                $form->display();
+                break;
+            }
+        }
+        if( ! sizeof($post_install) ) {
+            $this->pop_main();
+            return;
+        }
     }
 
     // *********** Function download_remote_file **************
