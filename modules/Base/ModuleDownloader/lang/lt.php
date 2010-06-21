b20f4af6a9d39bd414612eb01559fd43fdc9c9d5
Paweł Bukowski <pbukowski@telaxus.com>
2010-06-21 12:22:24 +0000

    lt translation

---
+++ modules/Base/ModuleDownloader/lang/lt.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage lt
+ */
+global $translations;
+$translations['Base_ModuleDownloader']['Error downloading file!']='Klaida siunčiant failą';
+?>
---
+++ modules/CRM/Meeting/lang/lt.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage lt
+ */
+global $translations;
+$translations['CRM_Meeting']['---']='';
+$translations['CRM_Meeting']['5 minutes']='5 minutės';
+$translations['CRM_Meeting']['15 minutes']='15 minučių';
+$translations['CRM_Meeting']['30 minutes']='30 minučių';
+$translations['CRM_Meeting']['45 minutes']='45 minutės';
+$translations['CRM_Meeting']['1 hour']='1 valanda';
+$translations['CRM_Meeting']['2 hours']='2 valandos';
+$translations['CRM_Meeting']['4 hours']='4 valandos';
+$translations['CRM_Meeting']['8 hours']='8 valandos';
+$translations['CRM_Meeting']['End Time']='Pabaigos laikas';
+$translations['CRM_Meeting']['Popup alert']='Pranešimas su pop-up';
+$translations['CRM_Meeting']['on event start']='kai įvykis turi prasidėti';
+$translations['CRM_Meeting']['15 minutes before event']='prieš 15 min. įvykiau prasidedant';
+$translations['CRM_Meeting']['30 minutes before event']='prieš 30 min. įvykiau prasidedant';
+$translations['CRM_Meeting']['45 minutes before event']='prieš 45 min. įvykiau prasidedant';
+$translations['CRM_Meeting']['1 hour before event']='prieš 1 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['2 hours before event']='prieš 2 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['3 hours before event']='prieš 3 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['4 hours before event']='prieš 4 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['8 hours before event']='prieš 8 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['12 hours before event']='prieš 12 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['24 hours before event']='prieš 24 val. įvykiau prasidedant';
+$translations['CRM_Meeting']['Popup message']='Pop-up pranešimas';
+$translations['CRM_Meeting']['Alert']='Žadintuvas';
+$translations['CRM_Meeting']['None']='Nėra';
+$translations['CRM_Meeting']['me']='mane';
+$translations['CRM_Meeting']['all selected employees']='visus pasiriktus darbuotojus';
+$translations['CRM_Meeting']['You have to select your contact to set alarm on it']='Jus pasirinkote kontantus kuries bus pranešta';
+$translations['CRM_Meeting']['Everyday']='Kiekvieną dieną';
+$translations['CRM_Meeting']['Every second day']='Kiekviena dieną';
+$translations['CRM_Meeting']['Every third day']='Kiekvieną 3 dieną';
+$translations['CRM_Meeting']['Every fourth day']='Kiekvieną 4 dieną';
+$translations['CRM_Meeting']['Every fifth day']='Kiekvieną 5 dieną';
+$translations['CRM_Meeting']['Every sixth day']='Kiekvieną 6 dieną';
+$translations['CRM_Meeting']['Once every week']='Vieną kartą savaitėje';
+$translations['CRM_Meeting']['Customize week']='Pasirinkti dienas';
+$translations['CRM_Meeting']['Every two weeks']='Kas 2 savaites';
+$translations['CRM_Meeting']['Every month']='Kiekvieną mėnesį';
+$translations['CRM_Meeting']['Every year']='Kiekveinais metais';
+?>
