06f06064affb280738eedca12923649c4bb8ddf1
Paweł Bukowski <pbukowski@telaxus.com>
2010-07-29 09:52:08 +0000

    webmin svnup

--- map.php
+++ map.php
@@ -1,6 +1,7 @@
 <?php
 /*
 global $virtual_hosts;
+//domain regex=>data dir
 $virtual_hosts = array(
 	'/^http:\/\/localhost\/epesi/' => false, //deny http://localhost/epesi
 	'/^/' => 'data' //allow http://192.168.13.10/epesi
