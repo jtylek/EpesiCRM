5f97382b76fa9d73ce22d10303bd13977333451a
Paweł Bukowski <pbukowski@telaxus.com>
2010-09-20 17:31:12 +0000

    QF 3.2.11

--- modules/Libs/QuickForm/requires.php
+++ modules/Libs/QuickForm/requires.php
@@ -14,9 +14,9 @@ ob_start();
 $ret = @readfile('HTML/QuickForm.php',true);
 ob_get_clean();
 if($ret===false) //more efficient... less invalid requests by php, but not work with QuickForm installed in pear
-	ini_set('include_path',ini_get('include_path').PATH_SEPARATOR.$dir.'/3.2.10');
+	ini_set('include_path',ini_get('include_path').PATH_SEPARATOR.$dir.'/3.2.11');
 else
-	ini_set('include_path',$dir.'/3.2.10'.PATH_SEPARATOR.ini_get('include_path'));
+	ini_set('include_path',$dir.'/3.2.11'.PATH_SEPARATOR.ini_get('include_path'));
 
 require_once('HTML/QuickForm.php');
 ?>
