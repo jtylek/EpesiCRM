f28961b58228f8ba7dc73fdc02418f1c9675e361
Paweł Bukowski <pbukowski@telaxus.com>
2010-11-05 17:25:00 +0000

    Utils_Calendar - failsafe

--- modules/Utils/Calendar/Calendar_0.php
+++ modules/Utils/Calendar/Calendar_0.php
@@ -739,7 +739,8 @@ class Utils_Calendar extends Module {
 //				print($ev['title'].' '.$ev['start'].'<hr>');
 				$day_start = explode(':',$this->settings['start_day']);
 				$day_start = ($day_start[0]*60+$day_start[1])*60;
-				$diff = ($day_start - ($ev['start'] - $today_t))/3600;
+				if (!isset($ev['start'])) $diff = 1;
+				else $diff = ($day_start - ($ev['start'] - $today_t))/3600;
 				$this->print_event($ev);
 //				$this->js('Utils_Calendar.add_event(\''.Epesi::escapeJS($dest_id,false).'\', \''.$ev['id'].'\', '.((!isset($ev['draggable']) || $ev['draggable']==true)?1:0).', '.ceil($ev['duration']/(strtotime($this->settings['interval'])-strtotime('0:00'))).')');
 				$ev_out .= 'Utils_Calendar.add_event(\''.Epesi::escapeJS($dest_id,false).'\', \''.$ev['id'].'\', '.((!isset($ev['draggable']) || $ev['draggable']==true)?1:0).', '.ceil($ev['duration']/(strtotime($this->settings['interval'])-strtotime('0:00'))).', '.$diff.');';
