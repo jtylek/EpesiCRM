34264260568ea5e65c32418c4a4c2bdf5de844fc
Paweł Bukowski <pbukowski@telaxus.com>
2010-12-15 13:51:13 +0000

    index fix

--- index.php
+++ index.php
@@ -44,7 +44,6 @@ if($cur_ver!==EPESI_VERSION) {
 	} else {
 		?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<!DOCTYPE html PUBLIC  "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 <head>
       <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
