420cdddce036d3767ed89b353954f72b131fba92
Paweł Bukowski <pbukowski@telaxus.com>
2011-04-12 12:12:27 +0000

    fckeditor removed from svn, and patch to delete it from database

--- modules/Libs/FCKeditor/onsubmit.js
+++
@@ -1,4 +0,0 @@
-fckeditor_onsubmit = function(ff) {
-if(typeof(FCKeditorAPI)!='undefined')
-for(var name in FCKeditorAPI.__Instances){var oEditor=FCKeditorAPI.__Instances[ name ];if(!ff || (oEditor.GetParentForm && oEditor.GetParentForm()==ff))oEditor.UpdateLinkedField();}
-}
