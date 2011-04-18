843267365751a221e7d7bba4733e64889248dea1
Janusz Tylek <jtylek@telaxus.com>
2011-04-18 14:30:51 +0000

    Added user id [uid] in the log

--- modules/CRM/LoginAudit/LoginAudit_0.php
+++ modules/CRM/LoginAudit/LoginAudit_0.php
@@ -30,7 +30,7 @@ class CRM_LoginAudit extends Module {
 		$gb = & $this->init_module('Utils/GenericBrowser',null,'login_audit');
 
 		$gb->set_table_columns(array(
-						array('name'=>$this->t('<b>Login</b> -> User Name'),'order'=>'b.user_login_id','width'=>20),
+						array('name'=>$this->t('<b>Login</b> [uid] -> User Name'),'order'=>'b.user_login_id','width'=>20),
 						array('name'=>$this->t('Start'), 'order'=>'b.start_time', 'width'=>15),
 						array('name'=>$this->t('End'),'order'=>'b.end_time','width'=>15),
                         array('name'=>$this->t('Duration'),'width'=>10),
@@ -59,7 +59,7 @@ class CRM_LoginAudit extends Module {
                         }
                 $offset=strtotime("1970-01-01 00:00:00");
                 $sess_time=date("G:i:s",strtotime($row['end_time'])-strtotime($row['start_time'])+$offset);
-                $gb->add_row('<b>'.$ulogin.'</b> -> '.$uid,$row['start_time'],$row['end_time'],$sess_time,$row['ip_address'],$row['host_name']);
+                $gb->add_row('<b>'.$ulogin.' ['.$row['user_login_id'].']</b> -> '.$uid,$row['start_time'],$row['end_time'],$sess_time,$row['ip_address'],$row['host_name']);
 			}
 
 		$this->display_module($gb);
