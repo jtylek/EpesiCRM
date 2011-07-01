667394f9028e47864dc76671a37dab9b7d041b20
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-07-01 11:12:31 +0000

    Calendar applet - minor bug fix

--- modules/CRM/Calendar/Calendar_0.php
+++ modules/CRM/Calendar/Calendar_0.php
@@ -206,7 +206,7 @@ class CRM_Calendar extends Module {
             // right column
             $title = Utils_TooltipCommon::create($row['title'],$row['custom_tooltip']);
 			
-			$day = $row['timeless']?$row['timeless']:Base_RegionalSettingsCommon::time2reg($row['start'], false, true, true, false);
+			$day = (isset($row['timeless']) && $row['timeless'])?$row['timeless']:Base_RegionalSettingsCommon::time2reg($row['start'], false, true, true, false);
 			if ($day<date('Y-m-d')) $class = 'past';
 			elseif ($day==date('Y-m-d')) $class = 'today';
 			elseif ($day==date('Y-m-d', strtotime('+1 day'))) $class = 'tomorrow';
