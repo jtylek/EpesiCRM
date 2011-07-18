909c8de90ff091cd4aeaab312c885df80a255c35
Adam Bukowski <abukowski@telaxus.com>
2011-07-18 13:39:33 +0000

    app store - list modules for client in GenericBrowser

--- modules/Base/AppStore/AppStore_0.php
+++ modules/Base/AppStore/AppStore_0.php
@@ -13,8 +13,10 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Base_AppStore extends Module {
 
+    protected $banned_columns = array();
+
     public function body() {
-        
+
     }
 
     public function admin() {
@@ -23,13 +25,49 @@ class Base_AppStore extends Module {
         }
         Base_ActionBarCommon::add('back', 'Back', $this->create_back_href());
 
-//        Base_EssClientCommon::set_license_key("");
         if (Base_EssClientCommon::get_license_key() == "") {
             // push main
             $m = $this->init_module('Base/EssClient');
             $this->display_module($m, null, 'register');
         } else {
-            echo "App Store";
+            $this->store_form();
+        }
+    }
+
+    public function store_form() {
+        $total = Base_EssClientCommon::server()->get_list_of_modules_total_amount();
+        if ($total) {
+            /* @var $gb Utils_GenericBrowser */
+            $gb = $this->init_module('Utils/GenericBrowser', null, 'moduleslist');
+            $x = $gb->get_limit($total);
+            // fetch data
+            $t = Base_EssClientCommon::server()->get_list_of_modules($x['offset'], $x['numrows']);
+            if ($t) {
+                $first = true;
+                foreach ($t as $r) {
+                    if ($first) {
+                        /* set table columns names */
+                        $columns = array();
+                        foreach ($r as $k => $v) {
+                            if (array_key_exists($k, $this->banned_columns))
+                                continue;
+                            $columns[] = array('name' => ucwords($k));
+                        }
+                        $gb->set_table_columns($columns);
+                        $first = false;
+                    }
+                    $v = array();
+                    foreach ($r as $k => $x) {
+                        if (array_key_exists($k, $this->banned_columns))
+                            continue;
+                        $v[] = $x;
+                    }
+                    /* @var $row Utils_GenericBrowser_Row_Object */
+                    $row = $gb->get_new_row();
+                    $row->add_data_array($v);
+                }
+            }
+            $this->display_module($gb);
         }
     }
 
