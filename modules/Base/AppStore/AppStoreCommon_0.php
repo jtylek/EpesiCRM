f18dac1cb30a193f60ee7c7083b7daa8ea934b79
Adam Bukowski <abukowski@telaxus.com>
2011-07-22 13:46:40 +0000

    AppStore - list orders, queue download, some refactor and caching

--- modules/Base/AppStore/AppStoreCommon_0.php
+++ modules/Base/AppStore/AppStoreCommon_0.php
@@ -44,6 +44,46 @@ class Base_AppStoreCommon extends Base_AdminModuleCommon {
         return Module::static_set_module_variable(self::MOD_PATH, self::DOWNLOAD_QUEUE_VAR, array());
     }
 
+    public static function module_format_info($r) {
+        $x = array();
+        $x[] = "<big><strong>{$r['name']}</strong></big>";
+        if ($r['description'])
+            $x[] = "<b>Description:</b><br/>{$r['description']}";
+        $x[] = "<b>Repository:</b> {$r['repository']}";
+        $x[] = "<b>Modules:</b><br/>{$r['path']}";
+        $x[] = "<b>Price:</b> {$r['price']}";
+        $x[] = "<b>Version:</b> {$r['version']}";
+        $x[] = "<b>Active:</b> {$r['active']}";
+
+        return implode('<br/>', $x);
+    }
+
+    public static function modules_total_amount() {
+        $total = Module::static_get_module_variable(self::MOD_PATH, 'modules_total_amount');
+        if ($total === null) {
+            $total = Base_EssClientCommon::server()->modules_list_total_amount();
+            Module::static_set_module_variable(self::MOD_PATH, 'modules_total_amount', $total);
+        }
+        return $total;
+    }
+
+    public static function modules_list($offset, $amount) {
+        $modules = Module::static_get_module_variable(self::MOD_PATH, 'modules_list', array());
+        $start = $offset;
+        $end = $offset + $amount - 1;
+        while (isset($modules[$start]))
+            $start++;
+        while (isset($modules[$end]))
+            $end--;
+        $modules_from_serv = $end >= $start ? Base_EssClientCommon::server()->modules_list($start, $end - $start + 1) : array();
+        $i = $start;
+        foreach ($modules_from_serv as $m) {
+            $modules[$i++] = $m;
+        }
+        Module::static_set_module_variable(self::MOD_PATH, 'modules_list', $modules);
+        return array_slice($modules, $offset, $amount);
+    }
+
 }
 
 ?>
--- modules/Base/AppStore/AppStore_0.php
+++ modules/Base/AppStore/AppStore_0.php
@@ -14,7 +14,8 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 class Base_AppStore extends Module {
     const refresh_info_text = 'Data is stored until close or refresh of browser\'s Epesi window or tab';
 
-    protected $banned_columns = array('id', 'owner_id', 'path');
+    protected $banned_columns_module = array('id', 'owner_id', 'path');
+    protected $banned_columns_order = array('id', 'installation_id');
 
     public function body() {
 
@@ -37,6 +38,20 @@ class Base_AppStore extends Module {
         }
     }
 
+    public function store_form() {
+        $total = Base_AppStoreCommon::modules_total_amount();
+        if ($total) {
+            /* @var $gb Utils_GenericBrowser */
+            $gb = $this->init_module('Utils/GenericBrowser', null, 'moduleslist');
+            $x = $gb->get_limit($total);
+            // fetch data
+            $t = Base_AppStoreCommon::modules_list($x['offset'], $x['numrows']);
+//            $t = Base_EssClientCommon::server()->modules_list($x['offset'], $x['numrows']);
+            $gb = $this->GB_module($gb, $t, array($this, 'GB_row_additional_actions_store'));
+            $this->display_module($gb);
+        }
+    }
+
     public function cart_button() {
         $cart = Base_AppStoreCommon::get_cart();
         $amount = count($cart);
@@ -56,34 +71,12 @@ class Base_AppStore extends Module {
             return;
         }
         $gb = $this->init_module('Utils/GenericBrowser', null, 'cartlist');
-        $first = true;
-        foreach ($items as $array_key => $r) {
-            if ($first) {
-                /* set table columns names */
-                $columns = array();
-                foreach ($r as $k => $v) {
-                    if (in_array($k, $this->banned_columns))
-                        continue;
-                    $columns[] = array('name' => ucwords($k));
-                }
-                $gb->set_table_columns($columns);
-                $first = false;
-            }
-            $v = array();
-            foreach ($r as $k => $x) {
-                if (in_array($k, $this->banned_columns))
-                    continue;
-                if ($k == 'price') {
-                    $total_price += $x;
-                    $x = $this->t('Free');
-                }
-                $v[] = $x;
-            }
-            /* @var $row Utils_GenericBrowser_Row_Object */
-            $row = $gb->get_new_row();
-            $row->add_data_array($v);
-            $row->add_action($this->create_callback_href(array($this, 'cart_remove_item'), array($array_key)), $this->t('Remove from cart'));
+        $gb = $this->GB_module($gb, $items, array($this, 'GB_row_additional_actions_cart'));
+        foreach ($items as $x) {
+            $total_price += $x['price'];
         }
+
+        // handle buy
         $f = $this->init_module('Libs/QuickForm');
         $f->addElement('static', 'price', null, $this->t('Total price') . ': ' . $total_price);
         $f->addElement('submit', 'submit', $this->t('Buy!'));
@@ -96,7 +89,7 @@ class Base_AppStore extends Module {
             }
             $ret = Base_EssClientCommon::server()->order_submit($modules);
             foreach ($ret as $id => $success) {
-                print("$module_names[$id] - <span style=\"color: " . ($success ? "green" : "red") . "\">" . $this->t($success ? 'Ordered' : 'Some error') . "</span><br/>");
+                print("$module_names[$id] - <span style=\"color: " . ($success ? "green" : "gray") . "\">" . $this->t($success ? 'Ordered' : 'Not ordered') . "</span><br/>");
             }
             Base_AppStoreCommon::empty_cart();
         } else {
@@ -111,10 +104,13 @@ class Base_AppStore extends Module {
         Base_AppStoreCommon::set_cart($items);
     }
 
-    public function cart_remove_item($array_key) {
+    public function cart_remove_item($r) {
         $items = Base_AppStoreCommon::get_cart();
-        unset($items[$array_key]);
-        Base_AppStoreCommon::set_cart($items);
+        $key = array_search($r, $items);
+        if ($key !== false) {
+            unset($items[$key]);
+            Base_AppStoreCommon::set_cart($items);
+        }
     }
 
     public function orders_form() {
@@ -128,38 +124,7 @@ class Base_AppStore extends Module {
         }
 
         $gb = $this->init_module('Utils/GenericBrowser', null, 'orderslist');
-        $first = true;
-        foreach ($orders as $array_key => $r) {
-            if ($first) {
-                /* set table columns names */
-                $columns = array();
-                foreach ($r as $k => $v) {
-                    if (in_array($k, $this->banned_columns))
-                        continue;
-                    $columns[] = array('name' => ucwords($k));
-                }
-                $gb->set_table_columns($columns);
-                $first = false;
-            }
-            $v = array();
-            foreach ($r as $k => $x) {
-                if (in_array($k, $this->banned_columns))
-                    continue;
-                if ($k == 'price') {
-                    $total_price += $x;
-                    $x = $this->t('Free');
-                }
-                if ($k == 'paid') {
-                    $x = $this->t($x ? 'Yes' : 'No');
-                }
-                $v[] = $x;
-            }
-            /* @var $row Utils_GenericBrowser_Row_Object */
-            $row = $gb->get_new_row();
-            $row->add_data_array($v);
-            if ($r['paid'])
-                $row->add_action($this->create_callback_href(array($this, 'download_queue_item'), array($r)), $this->t('Queue download'));
-        }
+        $this->GB_order($gb, $orders, array($this, 'GB_row_additional_actions_orders'));
         $this->display_module($gb);
     }
 
@@ -167,6 +132,15 @@ class Base_AppStore extends Module {
         $q = Base_AppStoreCommon::get_download_queue();
         $q[] = $r;
         Base_AppStoreCommon::set_download_queue($q);
+    }
+
+    public function download_dequeue_item($r) {
+        $q = Base_AppStoreCommon::get_download_queue();
+        $k = array_search($r, $q);
+        if ($k !== false) {
+            unset($q[$k]);
+            Base_AppStoreCommon::set_download_queue($q);
+        }
     }
 
     public function download_button() {
@@ -180,47 +154,91 @@ class Base_AppStore extends Module {
     public function download_form() {
         $this->back_button();
         Base_ActionBarCommon::add('delete', 'Clear list', $this->create_callback_href(array('Base_AppStoreCommon', 'empty_download_queue')));
-        var_dump(Base_AppStoreCommon::get_download_queue());
+        $downloads = Base_AppStoreCommon::get_download_queue();
+        if (count($downloads) == 0) {
+            print($this->t('No items'));
+            return;
+        }
+
+        $gb = $this->init_module('Utils/GenericBrowser', null, 'downloadslist');
+        $gb = $this->GB_order($gb, $downloads, array($this, 'GB_row_additional_actions_downloads'));
+        $this->display_module($gb);
     }
 
-    public function store_form() {
-        $total = Base_EssClientCommon::server()->modules_list_total_amount();
-        if ($total) {
-            /* @var $gb Utils_GenericBrowser */
-            $gb = $this->init_module('Utils/GenericBrowser', null, 'moduleslist');
-            $x = $gb->get_limit($total);
-            // fetch data
-            $t = Base_EssClientCommon::server()->modules_list($x['offset'], $x['numrows']);
-            if ($t) {
-                $first = true;
-                foreach ($t as $r) {
-                    if ($first) {
-                        /* set table columns names */
-                        $columns = array();
-                        foreach ($r as $k => $v) {
-                            if (in_array($k, $this->banned_columns))
-                                continue;
-                            $columns[] = array('name' => ucwords($k));
-                        }
-                        $gb->set_table_columns($columns);
-                        $first = false;
-                    }
-                    $v = array();
-                    foreach ($r as $k => $x) {
-                        if (in_array($k, $this->banned_columns))
-                            continue;
-                        if ($k == 'price')
-                            $x = $this->t('Free');
-                        $v[] = $x;
-                    }
-                    /* @var $row Utils_GenericBrowser_Row_Object */
-                    $row = $gb->get_new_row();
-                    $row->add_data_array($v);
-                    $row->add_action($this->create_callback_href(array($this, 'cart_add_item'), array($r)), $this->t('Add to cart'));
-                }
+    protected function GB_module(Utils_GenericBrowser $gb, array $items, $row_additional_actions_callback) {
+        return $this->GB_generic($gb, $items, $this->banned_columns_module, array($this, 'GB_row_data_transform_module'), $row_additional_actions_callback);
+    }
+
+    protected function GB_order(Utils_GenericBrowser $gb, array $items, $row_additional_actions_callback) {
+        return $this->GB_generic($gb, $items, $this->banned_columns_order, array($this, 'GB_row_data_transform_order'), $row_additional_actions_callback);
+    }
+
+    protected function GB_row_data_transform_module(array $data) {
+        if ($data['price'] == 0)
+            $data['price'] = $this->t('Free');
+
+        return $data;
+    }
+
+    protected function GB_row_data_transform_order(array $data) {
+        // price
+        if ($data['pay_price'] == 0)
+            $data['pay_price'] = $this->t('Free');
+        // paid
+        $data['paid'] = $this->t($data['paid'] ? 'Yes' : 'No');
+        // module info
+        $module = Base_EssClientCommon::server()->module_get_info($data['module_id']);
+        $tooltip = Utils_TooltipCommon::ajax_open_tag_attrs(array('Base_AppStoreCommon', 'module_format_info'), array($module));
+        $data['module'] = "<a $tooltip>{$module['name']}</a>";
+        unset($data['module_id']);
+
+        return $data;
+    }
+
+    protected function GB_row_additional_actions_store($row, $data) {
+        $row->add_action($this->create_callback_href(array($this, 'cart_add_item'), array($data)), $this->t('Add to cart'));
+    }
+
+    protected function GB_row_additional_actions_cart($row, $data) {
+        $row->add_action($this->create_callback_href(array($this, 'cart_remove_item'), array($data)), $this->t('Remove from cart'));
+    }
+
+    protected function GB_row_additional_actions_orders($row, $data) {
+        if ($data['paid'])
+            $row->add_action($this->create_callback_href(array($this, 'download_queue_item'), array($data)), $this->t('Queue download'));
+    }
+
+    protected function GB_row_additional_actions_downloads($row, $data) {
+        $row->add_action($this->create_callback_href(array($this, 'download_dequeue_item'), array($data)), 'delete');
+    }
+
+    protected function GB_generic(Utils_GenericBrowser $gb, array $items, $banned_columns, $row_data_transform_callback, $row_additional_actions_callback) {
+        if (count($items)) {
+            // add column headers
+            $first_el = call_user_func($row_data_transform_callback, reset($items));
+            $columns = array();
+            foreach ($first_el as $k => $v) {
+                if (in_array($k, $banned_columns))
+                    continue;
+                $columns[] = array('name' => ucwords($k));
             }
-            $this->display_module($gb);
+            $gb->set_table_columns($columns);
+            // add elements
+            foreach ($items as $r) {
+                $v = array();
+                $r_modified = call_user_func($row_data_transform_callback, $r);
+                foreach ($r_modified as $k => $x) {
+                    if (in_array($k, $banned_columns))
+                        continue;
+                    $v[] = $x;
+                }
+                /* @var $row Utils_GenericBrowser_Row_Object */
+                $row = $gb->get_new_row();
+                $row->add_data_array($v);
+                call_user_func($row_additional_actions_callback, $row, $r);
+            }
         }
+        return $gb;
     }
 
     public function navigate($func, $params = array()) {
