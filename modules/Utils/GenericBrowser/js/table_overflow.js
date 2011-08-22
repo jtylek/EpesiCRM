1b18a3ce74f877b08f16fe39e44ce622de4c7c04
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-08-22 14:24:47 +0000

    GeneGeneric Browser - hide overflow div bug fix

--- modules/Utils/GenericBrowser/js/table_overflow.js
+++ modules/Utils/GenericBrowser/js/table_overflow.js
@@ -2,28 +2,32 @@ var utils_genericbrowser__hidetip = false;
 var utils_genericbrowser__hide_current = 0;
 
 table_overflow_show = function (e_td) {
-	utils_genericbrowser__hidetip = false;
 	var e_tip = $("table_overflow");
 	if (!e_tip) return;
 	if (e_td.scrollHeight>e_td.clientHeight || e_td.scrollWidth>e_td.clientWidth) {
+//		$("iid").innerHTML += '<br>'+'table_overflow_show';
+		utils_genericbrowser__hidetip = false;
 		e_tip.style.minWidth = e_td.clientWidth+"px";
 		e_tip.style.minHeight = e_td.clientHeight+"px";
 		$("table_overflow_content").innerHTML = e_td.innerHTML;
 		e_tip.clonePosition(e_td,{setHeight: false, setWidth: false, offsetTop: -1, offsetLeft: -1});
 		e_tip.show();
 		if (e_tip.clientWidth<=e_td.clientWidth) e_tip.hide(); // Work-around for firefox, because it cannot handle scrollWidth in <td>
-		table_overflow_hide_delayed();
+		else table_overflow_hide_delayed();
 	}
 };
 table_overflow_stop_hide = function() {
+//	$("iid").innerHTML += '<br>'+'table_overflow_stop_hide';
 	utils_genericbrowser__hidetip = false;
 }
 table_overflow_hide_delayed = function () {
 	utils_genericbrowser__hide_current++;
+//	$("iid").innerHTML += '<br>'+'table_overflow_hide_delayed('+utils_genericbrowser__hide_current+')';
 	utils_genericbrowser__hidetip = true;
 	setTimeout("table_overflow_hide("+utils_genericbrowser__hide_current+");", 800);
 };
 table_overflow_hide = function (current) {
+//	$("iid").innerHTML += '<br>'+'table_overflow_hide('+current+')';
 	if (!utils_genericbrowser__hidetip || utils_genericbrowser__hide_current!=current) return;
 	var e_tip = $("table_overflow");
 	if (!e_tip) return;
