ce137076e60ebce940cb83a417dbfc0f512b60cb
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-09-05 13:20:33 +0000

    Base_Theme admin - select modules to use themeup every time.

--- modules/Base/Theme/Administrator/AdministratorCommon_0.php
+++ modules/Base/Theme/Administrator/AdministratorCommon_0.php
@@ -21,7 +21,8 @@ class Base_Theme_AdministratorCommon extends Base_AdminModuleCommon {
 	}
 	
 	public static function themeup_selected_modules() {
-		$cur = DB::GetAssoc('SELECT module, id FROM base_theme_themeup');
+		$cur = @DB::GetAssoc('SELECT module, id FROM base_theme_themeup');
+		if (!$cur) return;
 		foreach ($cur as $m=>$v)
 			Base_ThemeCommon::install_default_theme($m);
 	}
