9283889b6f3c8ee27aacb761f6bb222079f4e136
Paweł Bukowski <pbukowski@telaxus.com>
2011-10-06 10:41:16 +0000

    Roundcube - additional debug in RC applet

--- modules/CRM/Roundcube/applet_refresh.php
+++ modules/CRM/Roundcube/applet_refresh.php
@@ -15,16 +15,22 @@ if($rec['epesi_user']!==Acl::get_user()) die('invalid account id');
 
 $port = $rec['security']=='ssl'?993:143;
 
-$mailbox = imap_open('{'.$rec['server'].'/imap/readonly/novalidate-cert'.($rec['security']?'/'.$rec['security']:'').':'.$port.'}',$rec['login'],$rec['password'],OP_READONLY);
-if(!$mailbox) die('connection error');
-$uns = imap_search($mailbox,'UNSEEN ALL');
+$mailbox = @imap_open('{'.$rec['server'].'/imap/readonly/novalidate-cert'.($rec['security']?'/'.$rec['security']:'').':'.$port.'}',$rec['login'],$rec['password'],OP_READONLY || OP_SILENT);
+$err = imap_errors();
+if(!$mailbox || $err) die('connection error');
+$uns = @imap_search($mailbox,'UNSEEN ALL');
 $unseen = array();
 if($uns) {
-    $l=imap_fetch_overview($mailbox,implode(',',$uns),0);
-    if(!$l) die('error reading messages overview');
+    $l=@imap_fetch_overview($mailbox,implode(',',$uns),0);
+    $err = imap_errors();
+    if(!$l || $err) die('error reading messages overview');
     foreach($l as $msg) {
         	$unseen[] = htmlspecialchars(imap_utf8($msg->from)).': <i>'.htmlspecialchars(imap_utf8($msg->subject)).'</i>';
     }
 }
 print(Utils_TooltipCommon::create(count($unseen),implode('<br />',$unseen)));
+
+$err = imap_errors();
+print_r('<span style="display:none;">'.$err.'</span>');
+
 ?>
