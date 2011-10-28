470937aac42d92512b608b9cfa22c872cbc39a3d
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-10-28 11:17:22 +0000

    FileUpload - allow submitting form with file field disabled (iPad issue)

--- modules/Utils/FileUpload/upload.php
+++ modules/Utils/FileUpload/upload.php
@@ -12,7 +12,7 @@
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // date in the past
 
-if(!isset($_REQUEST['form_name']) || !isset($_FILES['file']) || !isset($_REQUEST['required'])) {
+if(!isset($_REQUEST['form_name']) || !isset($_REQUEST['required'])) {
 	?>
 	<script type="text/javascript">
 	<!--
@@ -32,56 +32,63 @@ require_once('../../../include.php');
 if(!Acl::is_user())
 	exit();
 $form_name = $_REQUEST['form_name'];
-$doc = $_FILES['file'];
-$ext = strrchr($doc['name'],'.');
-$dest_filename  = 'tmp_'.microtime(true).$ext;
-$dest_path  = DATA_DIR.'/Utils_FileUpload/'.$dest_filename;
-$required = $_REQUEST['required'];
+if (isset($_FILES['file'])) {
+	$doc = $_FILES['file'];
+	$ext = strrchr($doc['name'],'.');
+	$dest_filename  = 'tmp_'.microtime(true).$ext;
+	$dest_path  = DATA_DIR.'/Utils_FileUpload/'.$dest_filename;
+	$required = $_REQUEST['required'];
 
-if($doc['error']==UPLOAD_ERR_INI_SIZE || $doc['error']==UPLOAD_ERR_FORM_SIZE) {
-	?>
-	<script type="text/javascript">
-	<!--
-	alert('Specified file is too big');
-	-->
-	</script>
-	<?php
-} elseif($doc['error']==UPLOAD_ERR_PARTIAL || $doc['error']==UPLOAD_ERR_EXTENSION) {
-	?>
-	<script type="text/javascript">
-	<!--
-	alert('Upload failed');
-	-->
-	</script>
-	<?php
-} elseif($doc['error']==UPLOAD_ERR_NO_TMP_DIR || $doc['error']==UPLOAD_ERR_CANT_WRITE) {
-	?>
-	<script type="text/javascript">
-	<!--
-	alert('Invalid server setup: cannot write to temporary directory');
-	-->
-	</script>
-	<?php
-} elseif($doc['error']==UPLOAD_ERR_NO_FILE) {
-	if($required) {
-	?>
-	<script type="text/javascript">
-	<!--
-	alert('Please specify file to upload');
-	-->
-	</script>
-	<?php
+	if($doc['error']==UPLOAD_ERR_INI_SIZE || $doc['error']==UPLOAD_ERR_FORM_SIZE) {
+		?>
+		<script type="text/javascript">
+		<!--
+		alert('Specified file is too big');
+		-->
+		</script>
+		<?php
+	} elseif($doc['error']==UPLOAD_ERR_PARTIAL || $doc['error']==UPLOAD_ERR_EXTENSION) {
+		?>
+		<script type="text/javascript">
+		<!--
+		alert('Upload failed');
+		-->
+		</script>
+		<?php
+	} elseif($doc['error']==UPLOAD_ERR_NO_TMP_DIR || $doc['error']==UPLOAD_ERR_CANT_WRITE) {
+		?>
+		<script type="text/javascript">
+		<!--
+		alert('Invalid server setup: cannot write to temporary directory');
+		-->
+		</script>
+		<?php
+	} elseif($doc['error']==UPLOAD_ERR_NO_FILE) {
+		if($required) {
+		?>
+		<script type="text/javascript">
+		<!--
+		alert('Please specify file to upload');
+		-->
+		</script>
+		<?php
+		} else {
+			$ok = true;
+			$_SESSION['client']['uploaded_file'] = false;
+			$_SESSION['client']['uploaded_original_file'] = false;
+			session_commit();
+		}
 	} else {
 		$ok = true;
-		$_SESSION['client']['uploaded_file'] = false;
-		$_SESSION['client']['uploaded_original_file'] = false;
+		move_uploaded_file($doc['tmp_name'], $dest_path);
+		$_SESSION['client']['uploaded_file'] = $dest_path;
+		$_SESSION['client']['uploaded_original_file'] = $doc['name'];
 		session_commit();
 	}
 } else {
 	$ok = true;
-	move_uploaded_file($doc['tmp_name'], $dest_path);
-	$_SESSION['client']['uploaded_file'] = $dest_path;
-	$_SESSION['client']['uploaded_original_file'] = $doc['name'];
+	$_SESSION['client']['uploaded_file'] = false;
+	$_SESSION['client']['uploaded_original_file'] = false;
 	session_commit();
 }
 ?>
