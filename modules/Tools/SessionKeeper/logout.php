3c77f47f96b17ddc5b6b827ca6359270e0ef0ff5
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-11-02 13:01:46 +0000

    Removed autologin deletion when session expires

--- modules/Tools/SessionKeeper/logout.php
+++ modules/Tools/SessionKeeper/logout.php
@@ -4,7 +4,6 @@ header("Content-type: text/javascript");
 define('JS_OUTPUT',1);
 require_once('../../../include.php');
 if(Acl::is_user()) {
-    DB::Execute('UPDATE user_password SET autologin_id=\'\' WHERE user_login_id=%d',array(Acl::get_user()));
     Acl::set_user();
     die('document.location=\'index.php\';');
 }
