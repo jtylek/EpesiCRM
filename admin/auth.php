1b4e1d38225c746d6be11db7f2bfb0d1f9a86abd
Arkadiusz Bisaga <ethnar86@gmail.com>
2011-11-08 14:05:22 +0000

    /admin backdoor - disabled in DEMO MODE

--- admin/auth.php
+++ admin/auth.php
@@ -1,6 +1,8 @@
 <?php
 require_once('include.php');
 
+if (DEMO_MODE) die('Feature unavailable');
+
 $lpa = ModuleManager::get_load_priority_array();
 $load = array('Base_User'=>1,'Base_User_Login'=>1);
 
