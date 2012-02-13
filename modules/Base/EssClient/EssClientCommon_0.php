322dfb12d18e960ed50cc13cf52656999e010fb4
Adam Bukowski <abukowski@telaxus.com>
2012-02-13 16:43:15 +0000

    fix download modules callbacks, fix module dependencies

--- modules/Base/EssClient/EssClientCommon_0.php
+++ modules/Base/EssClient/EssClientCommon_0.php
@@ -24,6 +24,7 @@ class Base_EssClientCommon extends Base_AdminModuleCommon {
     }
     
     public static function get_server_url() {
+        return 'http://localhost/epesi/modules/Custom/ESS/serv/';
         return 'https://ess.epesibim.com/';
     }
     
