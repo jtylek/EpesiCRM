317de5287a6e354594799a9041a2d5021a355bea
Adam Bukowski <abukowski@telaxus.com>
2012-03-02 11:25:19 +0000

    fix bug in user common

--- modules/Base/User/UserCommon_0.php
+++ modules/Base/User/UserCommon_0.php
@@ -80,7 +80,7 @@ class Base_UserCommon extends ModuleCommon {
 	}
 
 	public static function get_user_label($uid) {
-		if (!$uid) return self::ts('front-end user');
+		if (!$uid) return Base_LangCommon::ts('Base_User', 'front-end user');
         if (ModuleManager::is_installed('CRM_Contacts')>=0)
 			return CRM_ContactsCommon::get_user_label($uid);
 		else
