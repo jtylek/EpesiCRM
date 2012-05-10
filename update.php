c14e070d8f35f864d41b9725ff66a47f216888ab
Janusz Tylek <jtylek@telaxus.com>
2012-05-10 07:36:44 +0000

    update fix

--- update.php
+++ update.php
@@ -2812,6 +2812,8 @@ function update_from_1_2_1_to_1_2_2() {
 
 $versions[] = '1.3';
 function update_from_1_2_2_to_1_3() {
+        DB::Execute('delete from modules where name="Utils_BookmarkBrowser"');
+        ModuleManager::create_load_priority_array();
 	PatchUtil::apply_new();
 }
 
