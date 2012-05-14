75e772a1e1b4ca155ef0b69a0e6b102d9ee64b6b
Janusz Tylek <jtylek@telaxus.com>
2012-05-14 12:30:17 +0000

    mime fixes

--- modules/Utils/Attachment/mime.php
+++ modules/Utils/Attachment/mime.php
@@ -17,10 +17,6 @@ function get_mime_type($filepath,$original) {
 	        return $ret;
     	}
 
-	//deprecated method
-	if(function_exists('mime_content_type'))
-        	return mime_content_type($filepath);
-
 	//unix system
 	$ret = 0;
 	ob_start();
@@ -34,13 +30,19 @@ function get_mime_type($filepath,$original) {
 		return $output;
 	}
 
+	//deprecated method
+	if(function_exists('mime_content_type'))
+        	return mime_content_type($filepath);
+
 	preg_match("/\.(.*?)$/", $original, $m);
 	if(!isset($m[1])) return "application/octet-stream";
 	switch(strtolower($m[1])){
        // case "js": return "application/javascript";
        // case "json": return "application/json";
        case "jpg": case "jpeg": case "jpe": return "image/jpg";
+	   case "xlsx": 
 	   case "xls": return "application/vnd.ms-excel";
+	   case "docx": 
 	   case "doc": return "application/msword";
    	   case "pdf": return "application/pdf";
        case "png": case "gif": case "bmp": return "image/".strtolower($m[1]);
