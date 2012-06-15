89b86c65d03d1cd603607d275dd950fac8f737c9
Adam Bukowski <abukowski@telaxus.com>
2012-06-15 09:13:57 +0000

    RB check for submited value

--- modules/Utils/RecordBrowser/RecordBrowser_0.php
+++ modules/Utils/RecordBrowser/RecordBrowser_0.php
@@ -1117,7 +1117,7 @@ class Utils_RecordBrowser extends Module {
                 if (!$access[$args['id']])
                     $form->freeze($args['id']);
             }
-        if ($form->validate() && $form->isSubmitted()) {
+        if ($form->validate() && $form->exportValue('submited')) {
             $values = $form->exportValues();
 			foreach ($defaults as $k=>$v)
 				if (!isset($values[$k]) && isset($this->view_fields_permission[$k]) && !$this->view_fields_permission[$k]) $values[$k] = $v;
