66ee8e67b2283fd3ed89fc1c0910bdb683e5d444
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-07-02 11:15:02 +0000

    Attachments - search label fix

--- modules/Utils/Attachment/AttachmentCommon_0.php
+++ modules/Utils/Attachment/AttachmentCommon_0.php
@@ -128,7 +128,7 @@ class Utils_AttachmentCommon extends ModuleCommon {
 					$view = call_user_func_array($func,unserialize($row['args']));
 				}
 				if(!$view) continue;
-				$ret[$row['id']] = $this->t('Note: ').$view;
+				$ret[$row['id']] = Base_LangCommon::ts('Utils_Attachment','Note: ').$view;
 			} else {
 				$ret[] = array('id'=>$row['id'],'group'=>$row['local']);
 			}
