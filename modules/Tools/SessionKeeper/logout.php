eab3be8acba4fb2999c669f10ba2dbd41c3b72bb
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-07-14 11:30:26 +0000

    SessionKeeper - bug fix

--- modules/Tools/SessionKeeper/logout.php
+++ modules/Tools/SessionKeeper/logout.php
@@ -3,6 +3,7 @@ header("Content-type: text/javascript");
 
 define('JS_OUTPUT',1);
 require_once('../../../include.php');
+ModuleManager::load_modules();
 if(Acl::is_user()) {
     Acl::set_user(null, true);
     die('document.location=\'index.php\';');
