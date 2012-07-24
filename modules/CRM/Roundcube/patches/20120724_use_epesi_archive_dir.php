eac44b66c11e9d696d4ea939888b02cd91f3a2cf
Paweł Bukowski <pbukowski@telaxus.com>
2012-07-24 17:50:33 +0000

    fix

--- modules/CRM/Roundcube/patches/20120724_use_epesi_archive_dir.php
+++ modules/CRM/Roundcube/patches/20120724_use_epesi_archive_dir.php
@@ -1,2 +1,3 @@
 <?php
 Utils_RecordBrowserCommon::new_record_field('rc_accounts', array('name' => _M('Use Epesi Archive directories'), 'type'=>'checkbox', 'extra'=>true, 'visible'=>false));
+DB::Execute('UPDATE rc_accounts_data_1 SET f_use_epesi_archive_directories=1');
