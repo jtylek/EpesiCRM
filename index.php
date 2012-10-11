3cd0dd12120a3c9990cd375f18d259500917b61d
Adam Bukowski <abukowski@telaxus.com>
2012-10-11 18:46:49 +0000

    Capitalize epesi text

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(version_compare(phpversion(), '5.0.0')==-1)
 	die("You are running an old version of PHP, php5 required.");
 
 if(trim(ini_get("safe_mode")))
-	die('You cannot use epesi with PHP safe mode turned on - please disable it. Please notice this feature is deprecated since PHP 5.3 and will be removed in PHP 6.0.');
+	die('You cannot use EPESI with PHP safe mode turned on - please disable it. Please notice this feature is deprecated since PHP 5.3 and will be removed in PHP 6.0.');
 
 define('_VALID_ACCESS',1);
 require_once('include/data_dir.php');
@@ -47,7 +47,7 @@ if($cur_ver!==EPESI_VERSION) {
 <html>
 <head>
       <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-      <title>Epesi update</title>
+      <title>EPESI update</title>
       <link href="setup.css" type="text/css" rel="stylesheet"/>
 </head>
 <body>
@@ -62,7 +62,7 @@ if($cur_ver!==EPESI_VERSION) {
         <table id="main" border="0" cellpadding="0" cellspacing="0">
             <tr>
                 <td>
-				Updating epesi from version <?php echo $cur_ver; ?> to <?php echo EPESI_VERSION; ?>. This operation may take several minutes.
+				Updating EPESI from version <?php echo $cur_ver; ?> to <?php echo EPESI_VERSION; ?>. This operation may take several minutes.
 				<a href="index.php?up=1">Click here to proceed</a>
                 </td>
             </tr>
@@ -84,7 +84,7 @@ if($cur_ver!==EPESI_VERSION) {
 
 $tables = DB::MetaTables();
 if(!in_array('modules',$tables) || !in_array('variables',$tables) || !in_array('session',$tables))
-	die('Database structure you are using is apparently out of date or damaged. If you didn\'t perform application update recently you should try to restore the database. Otherwise, please refer to epesi documentation in order to perform database update.');
+	die('Database structure you are using is apparently out of date or damaged. If you didn\'t perform application update recently you should try to restore the database. Otherwise, please refer to EPESI documentation in order to perform database update.');
 
 require_once('include/misc.php');
 
@@ -96,7 +96,7 @@ if(IPHONE) {
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta id="viewport" name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-	<title>epesi CRM</title>
+	<title>EPESI CRM</title>
 	<link rel="stylesheet" href="libs/UiUIKit/stylesheets/iphone.css" />
 	<link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
 	<script type="text/javascript" charset="utf-8">
@@ -108,10 +108,10 @@ if(IPHONE) {
 
 <body>
 <div id="header">
-		<h1>epesi CRM</h1>
+		<h1>EPESI CRM</h1>
 </div>
 
-Please choose epesi version:<ul>
+Please choose EPESI version:<ul>
 <li><a href="mobile.php" class="white button">mobile</a><br>
 <li><a href="index.php?force_epesi=1" class="green button">desktop</a>
 </ul>
@@ -134,7 +134,7 @@ Please choose epesi version:<ul>
 		<head profile="http://www.w3.org/2005/11/profile">
 		<link rel="icon" type="image/png" href="images/favicon.png" />
 		<link rel="apple-touch-icon" href="images/apple-favicon.png" />
-		<title>Epesi</title>
+		<title>EPESI</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=8" />
 		<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" /> 
