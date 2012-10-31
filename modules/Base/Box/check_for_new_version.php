e1e17fd2bf3baa822764a73f972ab40818ed175c
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-10-31 15:28:01 +0000

    Check for new version - minor bug fix

--- modules/Base/Box/check_for_new_version.php
+++ modules/Base/Box/check_for_new_version.php
@@ -19,8 +19,8 @@ ModuleManager::load_modules();
 $registered = Base_EssClientCommon::is_registered();
 $ver = __('version %s',array(EPESI_VERSION));
 if (!$registered) {
-    print($ver);
-    return;
+	print($ver);
+	return;
 }
 
 $updates = Base_EpesiStoreCommon::is_update_available();
@@ -33,7 +33,7 @@ if(!$updates) {
 if (Base_AclCommon::i_am_sa()) $tooltip = __('There are updates available for download, click to go to EPESI store.');
 else $tooltip = __('There are updates available for download. Please contact your administrator.');
 
-$message = Utils_TooltipCommon::create(__('version %s<br/><b>(Update Available!)</b>',array(EPESI_VERSION)), $tooltip, false);
+$message = Utils_TooltipCommon::create(__('version %s',array(EPESI_VERSION)).'<br/><b>('.__('Update Available!').')</b>', $tooltip, false);
 
 if (Base_AclCommon::i_am_sa()) $message = '<a '.Module::create_href(array('go_to_epesi_store_for_updates'=>true)).'class="version">'.$message.'</a>';
 
