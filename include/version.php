ae70ed983ee933226beedf897f934019a1027ccf
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-10-31 12:56:43 +0000

    Revision updated

--- include/version.php
+++ include/version.php
@@ -1,7 +1,7 @@
 <?php
 define("EPESI_VERSION", '1.4.0'); 
 
-$svnid = '$Rev: 9928 $'; 
+$svnid = '$Rev: 9943 $'; 
 $scid = substr($svnid, 6); 
 define("EPESI_REVISION", intval(substr($scid, 0, strlen($scid) - 2)));
 ?>
