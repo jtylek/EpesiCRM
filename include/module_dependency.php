dbb85ff87518558ab720e9a8c9c6709fb0315029
Adam Bukowski <abukowski@telaxus.com>
2012-12-07 14:03:25 +0000

    make some improvements to unused module dependencies class

--- include/module_dependency.php
+++ include/module_dependency.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Adam Bukowski <abukowski@telaxus.com>
  * @version 0.1
@@ -18,11 +19,13 @@ class Dependency {
     private $module_name;
     private $version_min;
     private $version_max;
+    private $compare_max;
 
-    private function __construct($module_name, $version_min, $version_max) {
+    private function __construct($module_name, $version_min, $version_max, $version_max_is_ok = true) {
         $this->module_name = $module_name;
         $this->version_min = $version_min;
         $this->version_max = $version_max;
+        $this->compare_max = $version_max_is_ok ? '<=' : '<';
     }
 
     /**
@@ -51,27 +54,37 @@ class Dependency {
 
     /**
      * Check if version satisfies dependency
-     * @param string $version alphabetical comparable version string
+     * @param string $version version string comparable with version_compare
      * @return bool does version satisfy dependency 
      */
     function is_satisfied_by($version) {
-        return ($this->version_min === null || $version >= $this->version_min) &&
-                ($this->version_max === null || $version <= $this->version_max);
+        return ($this->version_min === null
+                || version_compare($version, $this->version_min, '>='))
+                && ($this->version_max === null
+                || version_compare($version, $this->version_max, $this->compare_max));
     }
 
     /**
-     * Create dependency of module in any version
+     * Create dependency of module starting from version specified to last major
+     * revision of this version.
+     * 
+     * Dependency::requires('Base', '1.2.1') - means all starting from 1.2.1
+     * to 1.x.y are ok. 1.99.99 is ok but 2.0 is not.
      * @param string $module module name
      * @return Dependency
      */
-    static function requires($module) {
-        return new self($module, null, null);
+    static function requires($module, $version) {
+        $last_version = null;
+        if (preg_match('(\d+)', $version, $matches)) {
+            $last_version = (intval($matches[0])+1) . '.smallest';
+        }
+        return new self($module, $version, $last_version, false);
     }
 
     /**
      * Create dependency of module in exact version
      * @param string $module module name
-     * @param string $version alphabetical comparable version string
+     * @param string $version version string comparable with version_compare
      * @return Dependency
      */
     static function requires_exact($module, $version) {
@@ -81,7 +94,7 @@ class Dependency {
     /**
      * Create dependency of module in at least version supplied
      * @param string $module module name
-     * @param string $version alphabetical comparable version string
+     * @param string $version version string comparable with version_compare
      * @return Dependency
      */
     static function requires_at_least($module, $version) {
@@ -91,12 +104,15 @@ class Dependency {
     /**
      * Create dependency of module in range of versions inclusive
      * @param string $module module name
-     * @param string $version_min alphabetical comparable version string
-     * @param string $version_max alphabetical comparable version string
+     * @param string $version_min version string comparable with version_compare
+     * @param string $version_max version string comparable with version_compare
+     * @param bool $version_max_is_ok when true version specified in previous
+     * argument is last working. When false version_max is first which does not
+     * work.
      * @return Dependency
      */
-    static function requires_range($module, $version_min, $version_max) {
-        return new self($module, $version_min, $version_max);
+    static function requires_range($module, $version_min, $version_max, $version_max_is_ok = true) {
+        return new self($module, $version_min, $version_max, $version_max_is_ok);
     }
 
 }
