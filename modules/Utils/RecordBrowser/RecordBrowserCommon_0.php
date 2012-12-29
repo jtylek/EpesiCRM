f2e76471e0f6474dc0718f1f0acaaaae4e8a73fa
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-12-29 14:27:49 +0000

    RecordBrowser - checkbox updating fixed

--- modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
+++ modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
@@ -838,9 +838,11 @@ class Utils_RecordBrowserCommon extends ModuleCommon {
                 if ($all_fields) $values[$args['id']] = '';
                 else continue;
             }
-			if (is_bool($values[$args['id']])) {
-				if ($values[$args['id']]===true) $values[$args['id']] = 1;
-				else $values[$args['id']] = 0;
+			if ($args['type']=='checkbox') {
+				if ($values[$args['id']]) $values[$args['id']] = 1;
+				else $values[$args['id']] = '';
+				if ($record[$args['id']]) $record[$args['id']] = 1;
+				else $record[$args['id']] = '';
 			}
             if ($args['type']=='long text')
                 $values[$args['id']] = Utils_BBCodeCommon::optimize($values[$args['id']]);
