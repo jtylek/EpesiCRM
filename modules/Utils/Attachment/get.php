129b9968094985718268dac7e2b40762af1444e1
Arkadiusz Bisaga <ethnar86@gmail.com>
2013-01-19 17:52:08 +0000

    Attachments - get.php can now return thumbnails instead of full images

--- modules/Utils/Attachment/get.php
+++ modules/Utils/Attachment/get.php
@@ -47,10 +47,46 @@ $t = time();
 $remote_address = $_SERVER['REMOTE_ADDR'];
 $remote_host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 DB::Execute('INSERT INTO utils_attachment_download(attach_file_id,created_by,created_on,download_on,description,ip_address,host_name) VALUES (%d,%d,%T,%T,%s,%s,%s)',array($id,Acl::get_user(),$t,$t,$disposition,$remote_address,$remote_host));
-$f_filename = DATA_DIR.'/Utils_Attachment/'.$filename;
-if(!file_exists($f_filename))
-	die('File doesn\'t exists');
-$buffer = file_get_contents($f_filename);
+
+if (isset($_REQUEST['thumbnail'])) {
+    $o_filename = DATA_DIR.'/Utils_Attachment/'.$filename;
+    $f_filename = $o_filename.'_thumbnail';
+    if(!file_exists($f_filename)) {
+	if(!file_exists($o_filename))
+    	    die('File doesn\'t exists');
+    	$image_info = getimagesize($o_filename);
+    	$image_type = $image_info[2];
+    	$image = false;
+    	switch ($image_type) {
+	    case IMAGETYPE_JPEG: $image = imagecreatefromjpeg($o_filename); break;
+	    case IMAGETYPE_GIF: $image = imagecreatefromgif($o_filename); break;
+	    case IMAGETYPE_PNG: $image = imagecreatefrompng($o_filename); break;
+	    default: $buffer = file_get_contents($o_filename);
+    	}
+    	if ($image) {
+    	    $img_w = imagesx($image);
+    	    $img_h = imagesy($image);
+    	    $max = 300;
+    	    $w = ($img_w>=$img_h)?$max:floor($max*$img_w/$img_h);
+    	    $h = ($img_h>=$img_w)?$max:floor($max*$img_h/$img_w);
+    	    $new_image = imagecreatetruecolor($w, $h);
+    	    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $w, $h, $img_w, $img_h);
+    	    switch ($image_type) {
+	        case IMAGETYPE_JPEG: imagejpeg($new_image,$f_filename,95); break;
+		case IMAGETYPE_GIF: imagegif($new_image,$f_filename); break;
+		case IMAGETYPE_PNG: imagepng($new_image,$f_filename); break;
+    	    }
+	    $buffer = file_get_contents($f_filename);
+    	}
+    } else {
+	$buffer = file_get_contents($f_filename);
+    }
+} else {
+    $f_filename = DATA_DIR.'/Utils_Attachment/'.$filename;
+    if(!file_exists($f_filename))
+    	die('File doesn\'t exists');
+    $buffer = file_get_contents($f_filename);
+}
 
 $expires = 24*60*60;
 header('Pragma: public');
