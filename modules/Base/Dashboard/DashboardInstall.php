95c3e37367c4b1f9626be4b1eba78ad29751fb76
Arkadiusz Bisaga <ethnar86@gmail.com>
2013-01-31 15:38:42 +0000

    Dashboard - installation fix

--- modules/Base/Dashboard/DashboardInstall.php
+++ modules/Base/Dashboard/DashboardInstall.php
@@ -105,6 +105,7 @@ class Base_DashboardInstall extends ModuleInstall {
 		return array(
 			array('name'=>'Base/ActionBar','version'=>0),
 			array('name'=>'Base/Theme','version'=>0),
+			array('name'=>'Base/HomePage','version'=>0),
 			array('name'=>'Base/User','version'=>0),
 			array('name'=>'Base/User/Settings','version'=>0),
 			array('name'=>'Libs/QuickForm','version'=>0),
