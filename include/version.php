e73e7b5b66ad58b8a4aaf8523a8965a506db050b
Adam Bukowski <abukowski@telaxus.com>
2013-04-10 11:37:52 +0000

    change rev id in version.php

--- include/version.php
+++ include/version.php
@@ -1,7 +1,7 @@
 <?php
 define("EPESI_VERSION", '1.5.0'); 
 
-$svnid = '$Rev: 10136 $'; 
+$svnid = '$Rev: 10662 $';
 $scid = substr($svnid, 6); 
 define("EPESI_REVISION", intval(substr($scid, 0, -2)));
 ?>
