172d99ca8efc729ebf49bd94541c311ef51515a4
Adam Bukowski <abukowski@telaxus.com>
2013-04-16 14:16:38 +0000

    update version revision

--- include/version.php
+++ include/version.php
@@ -1,7 +1,7 @@
 <?php
 define("EPESI_VERSION", '1.5.0'); 
 
-$svnid = '$Rev: 10662 $';
+$svnid = '$Rev: 10687 $';
 $scid = substr($svnid, 6); 
 define("EPESI_REVISION", intval(substr($scid, 0, -2)));
 ?>
