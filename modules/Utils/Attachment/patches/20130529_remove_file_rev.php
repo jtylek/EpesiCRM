d556d4708ce6252e91e298dbf204fa6315e3128b
Paweł Bukowski <pbukowski@telaxus.com>
2013-06-04 20:19:52 +0000

    attachments patch fix

--- modules/Utils/Attachment/patches/20130529_remove_file_rev.php
+++ modules/Utils/Attachment/patches/20130529_remove_file_rev.php
@@ -1,9 +1,9 @@
 <?php
 
 if (array_key_exists(strtoupper('revision'),DB::MetaColumnNames('utils_attachment_file'))) {
-    DB::Execute('ALTER TABLE utils_attachment_file DROP INDEX utils_attachment_file__revision__idx');
-    DB::Execute('alter table utils_attachment_file drop foreign key utils_attachment_file_ibfk_2');
-    DB::Execute('alter table utils_attachment_file drop index attach_id');
+    @DB::Execute('ALTER TABLE utils_attachment_file DROP INDEX utils_attachment_file__revision__idx');
+    @DB::Execute('alter table utils_attachment_file drop foreign key utils_attachment_file_ibfk_2');
+    @DB::Execute('alter table utils_attachment_file drop index attach_id');
 	PatchUtil::db_drop_column('utils_attachment_file','revision');
 }
 
