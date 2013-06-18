86a3ee480c973e14b5a3a507014cf932fca88221
Paweł Bukowski <pbukowski@telaxus.com>
2013-06-18 15:18:42 +0000

    patch fix

--- modules/CRM/Roundcube/patches/20130617_add_thread_view4.php
+++ modules/CRM/Roundcube/patches/20130617_add_thread_view4.php
@@ -8,14 +8,15 @@ DB::Execute('ALTER TABLE `rc_mails_data_1` CHANGE `f_to` `f_to` varchar(4096) ')
       DB::Execute('DELETE FROM rc_mail_threads_data_1');
       DB::Execute('UPDATE rc_mails_data_1 SET f_message_id=REPLACE (f_message_id, "&lt;","")');
       DB::Execute('UPDATE rc_mails_data_1 SET f_message_id=REPLACE (f_message_id, "&gt;","")');
+
       $mails = Utils_RecordBrowserCommon::get_records('rc_mails');
       
       foreach($mails as $m) {
-        if(preg_match('/\nreferences:(.*)\n/i',$m['headers_data'],$match)) {
+        if(!$m['references'] && preg_match('/\nreferences:(.*)\n/i',$m['headers_data'],$match)) {
             $ref = trim($match[1]);
             Utils_RecordBrowserCommon::update_record('rc_mails',$m['id'],array('references'=>$ref));
         }
-        if(preg_match('/\nmessage-id:(.*)\n/i',$m['headers_data'],$match)) {
+        if(!$m['message_id'] && preg_match('/\nmessageid:(.*)\n/i',$m['headers_data'],$match)) {
             $mid = str_replace(array('<','>'),'',trim($match[1]));
             Utils_RecordBrowserCommon::update_record('rc_mails',$m['id'],array('message_id'=>$mid));
         }
