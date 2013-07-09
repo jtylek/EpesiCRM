768a4aa26c0e948624671332c420a9dcc4c6759c
Adam Bukowski <abukowski@telaxus.com>
2013-07-09 19:07:44 +0000

    1.5.3 release version.php

--- include/version.php
+++ include/version.php
@@ -1,7 +1,7 @@
 <?php
-define("EPESI_VERSION", '1.5.2');
+define("EPESI_VERSION", '1.5.3');
 
-$svnid = '$Rev: 10765 $';
+$svnid = '$Rev: 10944 $';
 $scid = substr($svnid, 6); 
 define("EPESI_REVISION", intval(substr($scid, 0, -2)));
 ?>
