7194edc5fbdff3cd230e256a42f2d5f07cf0b9fe
Paweł Bukowski <pbukowski@telaxus.com>
2013-10-15 09:49:16 +0000

    rc patch

---
+++ modules/CRM/Roundcube/patches/20131015_update_094.php
@@ -0,0 +1,11 @@
+<?php
+    if(DATABASE_DRIVER=='mysqlt' || DATABASE_DRIVER=='mysqli')
+        $f = file_get_contents('modules/CRM/Roundcube/RC/SQL/mysql.update2.sql');
+    else
+        $f = file_get_contents('modules/CRM/Roundcube/RC/SQL/postgres.update2.sql');
+    foreach(explode(';',$f) as $q) {
+        $q = trim($q);
+        if(!$q) continue;
+        @DB::Execute($q);
+    }
+?>
