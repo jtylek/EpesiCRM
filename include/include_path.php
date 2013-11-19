57c7c865779b4ac94cb48fabbb2d7ba38f023654
Adam Bukowski <abukowski@telaxus.com>
2013-11-19 09:51:26 +0000

    fix include path package

--- include/include_path.php
+++ include/include_path.php
@@ -3,10 +3,10 @@
  * @author Paul Bukowski <shacky7@gmail.com>
  * @version 1.0
  * @license SPL
- * @package e2-core
+ * @package epesi
  */
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
-ini_set('include_path','libs/minify'.PATH_SEPARATOR.'libs/MDB2'.PATH_SEPARATOR.'libs'.PATH_SEPARATOR.'.'.PATH_SEPARATOR.ini_get('include_path'));
+ini_set('include_path','libs/minify'.PATH_SEPARATOR.'libs'.PATH_SEPARATOR.'.'.PATH_SEPARATOR.ini_get('include_path'));
 
 ?>
