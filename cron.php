022a2506053dbc94345a5656cf9e4e3b53874e38
Paweł Bukowski <pbukowski@telaxus.com>
2014-01-16 13:21:55 +0100

    new cron fixes and debug turned off

--- cron.php
+++ cron.php
@@ -23,33 +23,28 @@ ini_set('memory_limit', '512M');
 ModuleManager::load_modules();
 Base_AclCommon::set_user(1);
 $ret = ModuleManager::call_common_methods('cron');
-$cron_funcs = array();
+$cron_last = DB::GetAssoc('SELECT func,last,running FROM cron ORDER BY last');
+$cron_funcs_prior = array(); //array of outdated cron callbacks
+$t = time();
 foreach($ret as $name=>$obj) {
     if(!$obj) continue;
     if(is_array($obj)) {
         foreach($obj as $func=>$every) {
            if(!strpos($func,'::')) $func = $name.'Common::'.$func;
-           $cron_funcs[$func] = $every;
+           $func_md5 = md5($func);
+           //if first cron run exists and it was executed in specified time or it keep running less then 24h - skip
+           if(isset($cron_last[$func_md5]) && ($cron_last[$func_md5]['last']>$t-$every*60 || ($cron_last[$func_md5]['running'] && $cron_last[$func_md5]['last']>$t-24*60*60))) continue;
+           if(!isset($cron_last[$func_md5])) {
+               DB::Execute('INSERT INTO cron(func,last,running) VALUES (%s,%d,%b)',array($func_md5,0,0));
+               $cron_last = array_merge(array($func_md5 => array('last'=>0,'running'=>0)),$cron_last);
+           }
+           $cron_funcs_prior[$func_md5] = $func;
         }
     }
 }
-arsort($cron_funcs);
-$cron_last = DB::GetAssoc('SELECT func,last,running FROM cron ORDER BY last');
-$cron_funcs_prior = array(); //array of outdated cron callbacks
-$t = time();
-foreach($cron_funcs as $func=>$every) {
-	$func_md5 = md5($func);
-	//if first cron run exists and it was executed in specified time or it keep running less then 24h - skip
-    if(isset($cron_last[$func_md5]) && ($cron_last[$func_md5]['last']>$t-$every*60 || ($cron_last[$func_md5]['running'] && $cron_last[$func_md5]['last']>$t-24*60*60))) continue;
-    if(!isset($cron_last[$func_md5])) {
-        DB::Execute('INSERT INTO cron(func,last,running) VALUES (%s,%d,%b)',array($func_md5,0,0));
-        $cron_last[$func_md5] = array('last'=>0,'running'=>0);
-    }
-    $cron_funcs_prior[$func_md5] = $func;
-}
 
-print_r($cron_last);
-print_r($cron_funcs_prior);
+//print_r($cron_last);
+//print_r($cron_funcs_prior);
 
 //call oldest executed callback
 foreach($cron_last as $func_md5=>$last) {
@@ -58,13 +53,13 @@ foreach($cron_last as $func_md5=>$last) {
     DB::Execute('UPDATE cron SET last=%d,running=1 WHERE func=%s',array($t,$func_md5));
     @unlink($lock);
 
-	print('call '.$cron_funcs_prior[$func_md5]."\n");
+//	print('call '.$cron_funcs_prior[$func_md5]."\n");
     $output = call_user_func(explode('::',$cron_funcs_prior[$func_md5]));
     if($output) {
         if(isset($argv))
-            print($name.":\n".strip_tags($output)."\n\n");
+            print($cron_funcs_prior[$func_md5].":\n".strip_tags($output)."\n\n");
         else
-            print($name.":<br>".$output."<hr>");
+            print($cron_funcs_prior[$func_md5].":<br>".$output."<hr>");
     }
 
     DB::Execute('UPDATE cron SET last=%d,running=0 WHERE func=%s',array(time(),$func_md5));
