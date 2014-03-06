97b3d46153201947d1194a92c7c4a54f931b9326
Janusz Tylek <jtylek@telaxus.com>
2014-03-06 22:22:25 +0100

    Patch to remove tables created by phpgacl no longer used. For old installations of EPESI only.

---
+++ modules/Base/patches/remove_phpgacl_tables.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Remove old tables created by phpgacl - no longer needed
+ * jtylek@telaxus.com
+ */
+@DB::DropTable('acl');
+@DB::DropTable('acl_sections');
+@DB::DropTable('acl_seq');
+
+@DB::DropTable('aco');
+@DB::DropTable('aco_map');
+@DB::DropTable('aco_sections');
+@DB::DropTable('aco_sections_seq');
+@DB::DropTable('aco_seq');
+
+@DB::DropTable('aro');
+@DB::DropTable('aro_groups');
+@DB::DropTable('aro_groups_id_seq');
+@DB::DropTable('aro_groups_map');
+@DB::DropTable('aro_map');
+
+@DB::DropTable('aro_sections');
+@DB::DropTable('aro_sections_seq');
+@DB::DropTable('aro_seq');
+
+@DB::DropTable('axo');
+@DB::DropTable('axo_groups');
+@DB::DropTable('axo_groups_map');
+@DB::DropTable('axo_map');
+@DB::DropTable('axo_sections');
+
+?>
