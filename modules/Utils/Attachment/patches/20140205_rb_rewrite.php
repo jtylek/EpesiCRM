299dedceef30ef3fff50709dd8fb69df1afd8c2d
Adam Bukowski <bukowski.adam@gmail.com>
2014-04-07 16:26:26 +0200

    fix Utils Attachment patch for postgresql

--- modules/Utils/Attachment/patches/20140205_rb_rewrite.php
+++ modules/Utils/Attachment/patches/20140205_rb_rewrite.php
@@ -95,9 +95,20 @@ if(DATABASE_DRIVER=='mysqli' || DATABASE_DRIVER=='mysqlt') {
         DB::Execute('alter table `utils_attachment_file` drop foreign key '.$m[1]);
 //    else trigger_error('Unable to find attach_id fkey.', E_USER_ERROR);
 } else {
-    $a = DB::GetRow('\d+ utils_attachment_file');
-    if(preg_match('/"(.+)" FOREIGN KEY .*attach_id/',$a[1],$m))
-        DB::Execute('alter table utils_attachment_file drop CONSTRAINT "'.$m[1].'"');
+    $a = DB::GetOne("SELECT
+    tc.constraint_name, tc.table_name, kcu.column_name,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name
+FROM
+    information_schema.table_constraints AS tc
+    JOIN information_schema.key_column_usage AS kcu
+      ON tc.constraint_name = kcu.constraint_name
+    JOIN information_schema.constraint_column_usage AS ccu
+      ON ccu.constraint_name = tc.constraint_name
+WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='utils_attachment_file' AND kcu.column_name='attach_id';");
+    if($a) {
+        DB::Execute('alter table utils_attachment_file drop CONSTRAINT "'.$a.'"');
+    }
 }
 @DB::DropIndex('attach_id','utils_attachment_file');
 
