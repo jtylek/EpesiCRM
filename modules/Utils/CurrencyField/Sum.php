289d132a386b82509c7ca584bb175dd49dd43651
Adam Bukowski <bukowski.adam@gmail.com>
2014-04-16 15:08:05 +0200

    Utils Currency field - classes for easier calculations

---
+++ modules/Utils/CurrencyField/Sum.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author     Adam Bukowski <abukowski@telaxus.com>
+ * @copyright  Copyright &copy; 2014, Telaxus LLC
+ * @license    MIT
+ * @version    1.0
+ * @package    epesi-utils
+ * @subpackage CurrencyField
+ */
+
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+
+class Utils_CurrencyField_Sum
+{
+    private $currencies_values = array();
+
+    public function add(Utils_CurrencyField_Value $value)
+    {
+        $sum = $this->get_currency_sum($value->get_currency_id());
+        $sum->add($value);
+    }
+
+    public function get_currency_sum($currency_id)
+    {
+        if (!array_key_exists($currency_id, $this->currencies_values)) {
+            $this->currencies_values[$currency_id] = new Utils_CurrencyField_Value(0, $currency_id);
+        }
+        return $this->currencies_values[$currency_id];
+    }
+
+    public function remove_zero_values()
+    {
+        foreach ($this->currencies_values as $currency_id => $value) {
+            if ($value->is_zero()) {
+                unset($this->currencies_values[$currency_id]);
+            }
+        }
+    }
+
+    public function get_total()
+    {
+        $this->remove_zero_values();
+        return $this->currencies_values;
+    }
+
+    public function format($separator = '<br>')
+    {
+        $ret = '';
+        foreach ($this->get_total() as $currency => $value) {
+            if ($ret) $ret .= $separator;
+            $ret .= $value->format();
+        }
+        return $ret;
+    }
+}
---
+++ modules/Utils/CurrencyField/Value.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * @author     Adam Bukowski <abukowski@telaxus.com>
+ * @copyright  Copyright &copy; 2014, Telaxus LLC
+ * @license    MIT
+ * @version    1.0
+ * @package    epesi-utils
+ * @subpackage CurrencyField
+ */
+
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+
+class Utils_CurrencyField_WrongAmountFormatException extends Exception {}
+
+class Utils_CurrencyField_CurrencyMismatchException extends Exception {}
+
+class Utils_CurrencyField_CurrencyNotFoundException extends Exception {}
+
+class Utils_CurrencyField_Value
+{
+    /**
+     * @param string $amount numeric value that represents amount of cash
+     * @param int $currency_id currency id that represents
+     */
+    public function __construct($amount, $currency_id)
+    {
+        $this->set_amount($amount);
+        $this->set_currency_id($currency_id);
+    }
+
+    public static function by_currency_code($amount, $currency_code)
+    {
+        $currency_id = self::get_currency_by_code($currency_code);
+        return new self($amount, $currency_id);
+    }
+
+    public static function from_string($string)
+    {
+        list($amount, $currency) = Utils_CurrencyFieldCommon::get_values($string);
+        return new self($amount, $currency);
+    }
+
+    public function is_zero()
+    {
+        return $this->get_amount() == 0;
+    }
+
+    public function to_string()
+    {
+        return Utils_CurrencyFieldCommon::format_default($this->get_amount(), $this->get_currency_id());
+    }
+
+    public function format()
+    {
+        return Utils_CurrencyFieldCommon::format($this->get_amount(), $this->get_currency_id());
+    }
+
+    public function add(Utils_CurrencyField_Value $other, $report_error = true)
+    {
+        if ($this->get_currency_id() == $other->get_currency_id()) {
+            $this->add_amount($other->get_amount());
+        } elseif ($report_error) {
+            $msg = "Can't add two values of different currencies.";
+            throw new Utils_CurrencyField_CurrencyMismatchException($msg);
+        }
+    }
+
+    public function add_amount($numeric_value)
+    {
+        $this->set_amount(self::add_amounts($this->get_amount(), $numeric_value));
+    }
+
+    public function subtract(Utils_CurrencyField_Value $other, $report_error = true)
+    {
+        if ($this->get_currency_id() == $other->get_currency_id()) {
+            $this->subtract_amount($other->get_amount());
+        } elseif ($report_error) {
+            $msg = "Can't subtract two values of different currencies.";
+            throw new Utils_CurrencyField_CurrencyMismatchException($msg);
+        }
+    }
+
+    public function get_amount()
+    {
+        return $this->amount;
+    }
+
+    public function get_currency_id()
+    {
+        return $this->currency_id;
+    }
+
+    public function set_amount($amount)
+    {
+        if (!is_numeric($amount)) {
+            throw new Utils_CurrencyField_WrongAmountFormatException($amount);
+        }
+        $this->amount = $amount;
+    }
+
+    public function set_currency_id($currency_id)
+    {
+        if (!Utils_CurrencyFieldCommon::get_code($currency_id)) {
+            $msg = "Currency with id: $currency_id doesn't exist in the system.";
+            throw new Utils_CurrencyField_CurrencyNotFoundException($msg);
+        }
+        $this->currency_id = $currency_id;
+    }
+
+    public function set_currency_by_code($currency_code)
+    {
+        $this->currency_id = self::get_currency_by_code($currency_code);
+    }
+
+    protected static function get_currency_by_code($currency_code)
+    {
+        $currency_id = Utils_CurrencyFieldCommon::get_id_by_code($currency_code);
+        if (!is_numeric($currency_id)) {
+            $msg = "Currency with code: $currency_code doesn't exist in the system.";
+            throw new Utils_CurrencyField_CurrencyNotFoundException($msg);
+        }
+        return $currency_id;
+    }
+
+    protected static function add_amounts($am1, $am2)
+    {
+        return $am1 + $am2;
+    }
+
+    private $amount;
+    private $currency_id;
+
+}
