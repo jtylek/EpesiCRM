ed43c673a7ad22205345667ecf064f26f777b8d7
Paweł Bukowski <pbukowski@telaxus.com>
2014-06-26 16:23:00 +0200

    cron description column

--- setup.php
+++ setup.php
@@ -575,7 +575,7 @@ function install_base() {
 	if($ret===false)
 		die('Invalid SQL query - Setup module (modules table)');
 
-	$ret = DB::CreateTable('cron',"func C(32) KEY,last I NOTNULL, running I1 NOTNULL DEFAULT 0");
+	$ret = DB::CreateTable('cron',"func C(32) KEY,last I NOTNULL, running I1 NOTNULL DEFAULT 0, description C(255)");
 	if($ret===false)
 		die('Invalid SQL query - Setup cron (cron table)');
 
