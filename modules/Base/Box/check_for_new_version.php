852478dc11aa845e5fb42ba34d8a78e3ae24767e
Adam Bukowski <bukowski.adam@gmail.com>
2014-06-26 15:42:01 +0200

    fix check for new version user messages

--- modules/Base/Box/check_for_new_version.php
+++ modules/Base/Box/check_for_new_version.php
@@ -26,14 +26,14 @@ if (!$registered) {
 $updates = Base_EpesiStoreCommon::is_update_available();
 
 if(!$updates) {
-	print(Utils_TooltipCommon::create($ver, __('You are using most up-to-date version of EPESI.'), false));
+	print($ver);
 	return;
 }
 
 if (Base_AclCommon::i_am_sa()) $tooltip = __('There are updates available for download, click to go to EPESI store.');
 else $tooltip = __('There are updates available for download. Please contact your administrator.');
 
-$message = Utils_TooltipCommon::create(__('version %s',array(EPESI_VERSION)).'<br/><b>('.__('Update Available!').')</b>', $tooltip, false);
+$message = Utils_TooltipCommon::create(__('version %s',array(EPESI_VERSION)).'<br/><b>('.__('Updates Available!').')</b>', $tooltip, false);
 
 if (Base_AclCommon::i_am_sa()) $message = '<a '.Module::create_href(array('go_to_epesi_store_for_updates'=>true)).'class="version">'.$message.'</a>';
 
