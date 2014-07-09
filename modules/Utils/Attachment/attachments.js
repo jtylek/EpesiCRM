4c4b81a0b1a2662a18e8528f7ca0cd2ba6777cd9
Adam Bukowski <bukowski.adam@gmail.com>
2014-07-09 11:08:31 +0200

    attachments - decrypt only if password provided

--- modules/Utils/Attachment/attachments.js
+++ modules/Utils/Attachment/attachments.js
@@ -122,22 +122,24 @@ document.onpaste = function(event) {
 
 utils_attachment_password = function(label,id) {
     var pass = prompt(label);
-    new Ajax.Request("modules/Utils/Attachment/check_decrypt.php", {
-        method: "post",
-        parameters:{
-            cid: Epesi.client_id,
-            id: id,
-            pass: pass
-        },
-        onSuccess:function(t) {
-            result = t.responseText.evalJSON();
-            if(typeof result.error != "undefined") return alert(result.error);
-            Event.fire(document,'e:loading');
-            if(typeof result.js != "undefined") {
-                eval(result.js);
+    if (pass != null) {
+        new Ajax.Request("modules/Utils/Attachment/check_decrypt.php", {
+            method: "post",
+            parameters:{
+                cid: Epesi.client_id,
+                id: id,
+                pass: pass
+            },
+            onSuccess:function(t) {
+                result = t.responseText.evalJSON();
+                if(typeof result.error != "undefined") return alert(result.error);
+                Event.fire(document,'e:loading');
+                if(typeof result.js != "undefined") {
+                    eval(result.js);
+                }
+                $("note_value_"+id).innerHTML = result.note;
+                Event.fire(document,'e:load');
             }
-            $("note_value_"+id).innerHTML = result.note;
-            Event.fire(document,'e:load');
-        }
-    });
+        });
+    }
 }
