5ccc971134001c56e2fd7c44e5b86534ebd3638c
Adam Bukowski <bukowski.adam@gmail.com>
2014-09-08 11:49:19 +0200

    RecordBrowser fix group permissions

--- modules/Utils/RecordBrowser/RecordBrowser_0.php
+++ modules/Utils/RecordBrowser/RecordBrowser_0.php
@@ -308,11 +308,16 @@ class Utils_RecordBrowser extends Module {
         if ($this->get_access('browse')===false) {
             return;
         }
+        $access = $this->get_access('view');
         $filters_all = array();
-        foreach ($this->table_rows as $k=>$v) {
-            if ((!isset($filters_set[$v['id']]) && $v['filter']) || (isset($filters_set[$v['id']]) && $filters_set[$v['id']])) {
-                $filters_all[] = $k;
-                if (isset($filters_set[$v['id']])) unset($filters_set[$v['id']]);
+        foreach ($this->table_rows as $k => $v) {
+            if ($access[$v['id']]) {
+                if ((!isset($filters_set[$v['id']]) && $v['filter']) || (isset($filters_set[$v['id']]) && $filters_set[$v['id']])) {
+                    $filters_all[] = $k;
+                    if (isset($filters_set[$v['id']])) {
+                        unset($filters_set[$v['id']]);
+                    }
+                }
             }
         }
         if (!$this->data_gb) $this->data_gb = $this->init_module('Utils/GenericBrowser', null, $this->tab);
