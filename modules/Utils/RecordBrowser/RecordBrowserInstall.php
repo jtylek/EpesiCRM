dce027ca19621b0f608cc644c5fc2e6503d0244d
Adam Bukowski <bukowski.adam@gmail.com>
2014-10-15 17:30:52 +0200

    RB - fix install file

--- modules/Utils/RecordBrowser/RecordBrowserInstall.php
+++ modules/Utils/RecordBrowser/RecordBrowserInstall.php
@@ -31,7 +31,7 @@ class Utils_RecordBrowserInstall extends ModuleInstall {
 						'description_callback C(128) DEFAULT \'\','.
                         'jump_to_id I1 DEFAULT 1,'.
                         'search_include I1 DEFAULT 0,'.
-                        'search_priority I1 DEFAULT 0,',
+                        'search_priority I1 DEFAULT 0,'.
                         'printer C(255) DEFAULT \'\'',
 						array('constraints'=>', UNIQUE(tab)'));
 		DB::CreateTable('recordbrowser_datatype',
