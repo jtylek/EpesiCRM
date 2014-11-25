b140d3e6ed2d72b96a9a0bebf6de36ff92aae05f
Adam Bukowski <bukowski.adam@gmail.com>
2014-11-25 12:41:35 +0100

    RB - fix patch for shorter execution

--- modules/Utils/RecordBrowser/patches/20141022_on_delete_cascade.php
+++ modules/Utils/RecordBrowser/patches/20141022_on_delete_cascade.php
@@ -3,8 +3,18 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 // Remove words map records on recordset removal
 
+$checkpoint = Patch::checkpoint('truncate');
+if (!$checkpoint->is_done()) {
+    Patch::require_time(5);
+    DB::Execute('TRUNCATE TABLE recordbrowser_words_map');
+    $checkpoint->done();
+}
+
+$alter_checkpoint = Patch::checkpoint('steps');
 if (DB::is_postgresql()) {
-    $a = DB::GetOne("SELECT
+    if ($alter_checkpoint->get('word_id', false) == false) {
+        Patch::require_time(5);
+        $a = DB::GetOne("SELECT
             tc.constraint_name, tc.table_name, kcu.column_name,
             ccu.table_name AS foreign_table_name,
             ccu.column_name AS foreign_column_name
@@ -15,14 +25,18 @@ if (DB::is_postgresql()) {
         JOIN information_schema.constraint_column_usage AS ccu
           ON ccu.constraint_name = tc.constraint_name
         WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='recordbrowser_words_map' AND kcu.column_name='word_id';");
-    if ($a) {
-        DB::StartTrans();
-        DB::Execute('ALTER TABLE recordbrowser_words_map DROP CONSTRAINT "' . $a . '"');
-        DB::Execute('ALTER TABLE recordbrowser_words_map ADD CONSTRAINT "' . $a . '" FOREIGN KEY (word_id) REFERENCES recordbrowser_words_index(id) ON DELETE CASCADE ON UPDATE CASCADE');
-        DB::CompleteTrans();
+        if ($a) {
+            DB::StartTrans();
+            DB::Execute('ALTER TABLE recordbrowser_words_map DROP CONSTRAINT "' . $a . '"');
+            DB::Execute('ALTER TABLE recordbrowser_words_map ADD CONSTRAINT "' . $a . '" FOREIGN KEY (word_id) REFERENCES recordbrowser_words_index(id) ON DELETE CASCADE ON UPDATE CASCADE');
+            DB::CompleteTrans();
+        }
+        $alter_checkpoint->set('word_id', true);
     }
 
-    $b = DB::GetOne("SELECT
+    if ($alter_checkpoint->get('tab_id', false) == false) {
+        Patch::require_time(5);
+        $b = DB::GetOne("SELECT
             tc.constraint_name, tc.table_name, kcu.column_name,
             ccu.table_name AS foreign_table_name,
             ccu.column_name AS foreign_column_name
@@ -33,30 +47,40 @@ if (DB::is_postgresql()) {
         JOIN information_schema.constraint_column_usage AS ccu
           ON ccu.constraint_name = tc.constraint_name
         WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='recordbrowser_words_map' AND kcu.column_name='tab_id';");
-    if ($b) {
-        DB::StartTrans();
-        DB::Execute('ALTER TABLE recordbrowser_words_map DROP CONSTRAINT "' . $b . '"');
-        DB::Execute('ALTER TABLE recordbrowser_words_map ADD CONSTRAINT "' . $b . '" FOREIGN KEY (tab_id) REFERENCES recordbrowser_table_properties(id) ON DELETE CASCADE ON UPDATE CASCADE');
-        DB::CompleteTrans();
+        if ($b) {
+            DB::StartTrans();
+            DB::Execute('ALTER TABLE recordbrowser_words_map DROP CONSTRAINT "' . $b . '"');
+            DB::Execute('ALTER TABLE recordbrowser_words_map ADD CONSTRAINT "' . $b . '" FOREIGN KEY (tab_id) REFERENCES recordbrowser_table_properties(id) ON DELETE CASCADE ON UPDATE CASCADE');
+            DB::CompleteTrans();
+        }
+        $alter_checkpoint->set('tab_id', true);
     }
 } elseif (DB::is_mysql()) {
     $a = DB::GetRow('SHOW CREATE TABLE recordbrowser_words_map');
 
-    preg_match('/CONSTRAINT (.+) FOREIGN KEY .*word_id/', $a[1], $m);
-    if (isset($m[1])) {
-        DB::StartTrans();
-        DB::Execute('ALTER TABLE recordbrowser_words_map DROP FOREIGN KEY ' . $m[1]);
-        DB::Execute('ALTER TABLE recordbrowser_words_map ADD FOREIGN KEY (word_id) REFERENCES recordbrowser_words_index(id) ON DELETE CASCADE ON UPDATE CASCADE');
-        DB::CompleteTrans();
+    if ($alter_checkpoint->get('word_id', false) == false) {
+        Patch::require_time(5);
+        preg_match('/CONSTRAINT (.+) FOREIGN KEY .*word_id/', $a[1], $m);
+        if (isset($m[1])) {
+            DB::StartTrans();
+            DB::Execute('ALTER TABLE recordbrowser_words_map DROP FOREIGN KEY ' . $m[1]);
+            DB::Execute('ALTER TABLE recordbrowser_words_map ADD FOREIGN KEY (word_id) REFERENCES recordbrowser_words_index(id) ON DELETE CASCADE ON UPDATE CASCADE');
+            DB::CompleteTrans();
+        }
+        unset($m);
+        $alter_checkpoint->set('word_id', true);
     }
-    unset($m);
 
-    preg_match('/CONSTRAINT (.+) FOREIGN KEY .*tab_id/', $a[1], $m);
-    if (isset($m[1])) {
-        DB::StartTrans();
-        DB::Execute('ALTER TABLE recordbrowser_words_map DROP FOREIGN KEY ' . $m[1]);
-        DB::Execute('ALTER TABLE recordbrowser_words_map ADD FOREIGN KEY (tab_id) REFERENCES recordbrowser_table_properties(id) ON DELETE CASCADE ON UPDATE CASCADE');
-        DB::CompleteTrans();
+    if ($alter_checkpoint->get('tab_id', false) == false) {
+        Patch::require_time(5);
+        preg_match('/CONSTRAINT (.+) FOREIGN KEY .*tab_id/', $a[1], $m);
+        if (isset($m[1])) {
+            DB::StartTrans();
+            DB::Execute('ALTER TABLE recordbrowser_words_map DROP FOREIGN KEY ' . $m[1]);
+            DB::Execute('ALTER TABLE recordbrowser_words_map ADD FOREIGN KEY (tab_id) REFERENCES recordbrowser_table_properties(id) ON DELETE CASCADE ON UPDATE CASCADE');
+            DB::CompleteTrans();
+        }
+        $alter_checkpoint->set('tab_id', true);
     }
 }
 
