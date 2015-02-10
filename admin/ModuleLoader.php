fd21a4637281e36a88bde7d11621bed1943b615c
Adam Bukowski <bukowski.adam@gmail.com>
2015-02-10 14:36:06 +0100

    Adjust /admin panel to changes in module manager class.

--- admin/ModuleLoader.php
+++ admin/ModuleLoader.php
@@ -19,8 +19,6 @@ class ModuleLoader {
             $this->initialized = true;
             $this->lpa = ModuleManager::get_load_priority_array();
             $this->lpa_count = count($this->lpa);
-            ModuleManager::$not_loaded_modules = $this->lpa;
-            ModuleManager::$loaded_modules = array();
             ModulesAutoloader::enable();
         }
     }
