2c4275bba6a4f7aa85606d2cbb780763e0ea6fd8
Paweł Bukowski <pbukowski@telaxus.com>
2015-03-19 21:32:01 +0100

    rc archive fix

--- modules/CRM/Roundcube/RC/plugins/epesi_archive/epesi_archive.php
+++ modules/CRM/Roundcube/RC/plugins/epesi_archive/epesi_archive.php
@@ -146,7 +146,7 @@ class epesi_archive extends rcube_plugin
         }
         $rcmail->output->command('display_message',$this->gettext('archived'), 'confirmation');
     }
-    global $E_SESSION_ID;
+    global $E_SESSION_ID,$E_SESSION;
     $tmp = $_SESSION;
     $_SESSION = $E_SESSION;
     DBSession::write($E_SESSION_ID,'');
