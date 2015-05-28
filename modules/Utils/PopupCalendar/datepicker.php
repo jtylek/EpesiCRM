6a9c4229fa3dad9819826a0cf5eaf1ceb4c0818e
Adam Bukowski <bukowski.adam@gmail.com>
2015-05-28 12:21:09 +0200

    Popup calendar - allow custom placeholder text

--- modules/Utils/PopupCalendar/datepicker.php
+++ modules/Utils/PopupCalendar/datepicker.php
@@ -35,7 +35,8 @@ class HTML_QuickForm_datepicker extends HTML_QuickForm_input {
 			$ex_date = Base_RegionalSettingsCommon::time2reg(null,false,true,false);
 			$date_format = Base_RegionalSettingsCommon::date_format();
 			$this->setType('text');
-            $this->setAttribute('placeholder', __('Click to select date'));
+            if (!$this->getAttribute('placeholder'))
+                $this->setAttribute('placeholder', __('Click to select date'));
             $js = Utils_PopupCalendarCommon::create_href(md5($id),
                     'new Ajax.Request(\'modules/Utils/PopupCalendar/up.php\','.
                     '{method:\'post\', parameters:{date: __YEAR__+\'-\'+__MONTH__+\'-\'+__DAY__},'.
