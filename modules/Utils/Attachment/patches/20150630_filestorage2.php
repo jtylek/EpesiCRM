ff1ceb9050a35b8176736435f959ca29c87a8601
Paweł Bukowski <pbukowski@telaxus.com>
2015-09-09 18:45:46 +0200

    [FIX] Utis/Attachment - fix patch

--- modules/Utils/Attachment/patches/20150630_filestorage2.php
+++ modules/Utils/Attachment/patches/20150630_filestorage2.php
@@ -23,23 +23,21 @@ if(!$files_checkpoint->is_done()) {
         $row = $ret->FetchRow();
         if(!$row) break;
         
-        if(!file_exists(DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id'])) {
-            DB::Execute('DELETE FROM utils_attachment_download WHERE attach_file_id=%d',$row['id']);
-            DB::Execute('DELETE FROM utils_attachment_file WHERE id=%d',$row['id']);
-            continue;
+        if(file_exists(DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id'])) {
+            Patch::set_message('Processing file: '.$files.'/'.$files_qty);
+            $files_checkpoint->require_time(2);
+
+            $fsid = Utils_FileStorageCommon::write_file($row['original'],DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id'],'attachment_file/'.$row['id']);
+            unlink(DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id']);
+            DB::Execute('UPDATE utils_attachment_file SET filestorage_id=%d WHERE id=%d',array($fsid,$row['id']));
         }
-
-        Patch::set_message('Processing file: '.$files.'/'.$files_qty);
-        $files_checkpoint->require_time(2);
-
-        $fsid = Utils_FileStorageCommon::write_file($row['original'],DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id'],'attachment_file/'.$row['id']);
-        unlink(DATA_DIR.'/Utils_Attachment/'.$row['aid'].'/'.$row['id']);
-        DB::Execute('UPDATE utils_attachment_file SET filestorage_id=%d WHERE id=%d',array($fsid,$row['id']));
-
+        
         $files_checkpoint->set('files',$files);
     }
     
     $files_checkpoint->done();
 }
 
+DB::Execute('DELETE FROM utils_attachment_download WHERE attach_file_id in (SELECT id FROM utils_attachment_file WHERE filestorage_id=0)');
+DB::Execute('DELETE FROM utils_attachment_file WHERE filestorage_id=0');
 DB::Execute('ALTER TABLE utils_attachment_file ADD FOREIGN KEY (filestorage_id) REFERENCES utils_filestorage_files(id)');
