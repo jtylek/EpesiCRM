2069a506eb529222715a437f117f08a930e07469
Adam Bukowski <bukowski.adam@gmail.com>
2015-09-17 12:43:52 +0200

    [UPDATE] handle more type of fields for query builder
    nested select crits

--- modules/Utils/RecordBrowser/QueryBuilderIntegration.php
+++ modules/Utils/RecordBrowser/QueryBuilderIntegration.php
@@ -27,9 +27,9 @@ class Utils_RecordBrowser_QueryBuilderIntegration
     {
         $ret = array();
         foreach ($this->fields as $f) {
-            $def = self::map_rb_field_to_query_builder_filter($f);
+            $def = self::map_rb_field_to_query_builder_filters($this->tab, $f);
             if ($def) {
-                $ret[] = $def;
+                $ret = array_merge($ret, $def);
             }
         }
         return $ret;
@@ -45,18 +45,21 @@ class Utils_RecordBrowser_QueryBuilderIntegration
         return $ret;
     }
 
-    public static function map_rb_field_to_query_builder_filter($f)
+    public static function map_rb_field_to_query_builder_filters($tab, $f, $in_depth = true, $prefix = '', $sufix = '', $label_prefix = '')
     {
+        $filters = array();
         $type = null;
         $values = null;
         $input = null;
         $opts = array();
+        $filters[] = &$opts;
         switch ($f['type']) {
             case 'text':
                 $type = 'string';
                 break;
             case 'multiselect':
                 $opts['multiple'] = true;
+                $opts['rows'] = '10';
             case 'select':
                 $param = explode(';', $f['param']);
                 $ref = explode('::', $param[0]);
@@ -69,6 +72,26 @@ class Utils_RecordBrowser_QueryBuilderIntegration
                     $single_tab = count($tabs) == 1;
                 }
                 $type = $single_tab ? 'integer' : 'string';
+                $input = 'select';
+                $values = self::permissions_get_field_values($tab, $f);
+                if ($in_depth && $single_tab) {
+                    $one_tab = reset($tabs);
+                    if (Utils_RecordBrowserCommon::check_table_name($one_tab, false, false)) {
+                        $fields = Utils_RecordBrowserCommon::init($one_tab);
+                        foreach ($fields as $k => $v) {
+                            if ($v['type'] == 'calculated' || $v['type'] == 'hidden') {
+                            } else {
+                                $new_label_prefix = _V($f['name']) . ' ' .  __('is set to record where') . ' ';
+                                $sub_filter = self::map_rb_field_to_query_builder_filters($tab, $v, false, $f['id'] . '[', ']', $new_label_prefix);
+                                if ($sub_filter) {
+                                    $sub_filter = reset($sub_filter);
+                                    $sub_filter['optgroup'] = $new_label_prefix;
+                                    $filters[] = $sub_filter;
+                                }
+                            }
+                        }
+                    }
+                }
                 break;
             case 'commondata':
                 $type = 'string';
@@ -79,22 +102,25 @@ class Utils_RecordBrowser_QueryBuilderIntegration
                     $values = $values + Utils_CommonDataCommon::get_translated_array($array_id, is_array($f['param']) ? $f['param']['order_by_key'] : false);
                 }
                 break;
-            case 'integer': $f = DB::dict()->ActualType('I4'); break;
-            case 'float': $f = DB::dict()->ActualType('F'); break;
-            case 'date': $f = DB::dict()->ActualType('D'); break;
-            case 'timestamp': $f = DB::dict()->ActualType('T'); break;
+            case 'integer':     $type = 'integer'; break;
+            case 'float':       $type = 'double'; break;
+            case 'timestamp':
+            case 'date':
+                $type = 'string';
+                $input = 'select';
+                $values = self::permissions_get_field_values($tab, $f);
+                break;
             case 'time': $f = DB::dict()->ActualType('T'); break;
-            case 'long text': $f = DB::dict()->ActualType('X'); break;
-            case 'hidden': $f = (isset($param)?$param:''); break;
-            case 'calculated': $f = (isset($param)?$param:''); break;
-            case 'checkbox': $f = DB::dict()->ActualType('I1'); break;
+            case 'long text':   $type = 'string'; $input = 'textarea'; break;
+            case 'hidden': break;
+            case 'calculated': break;
+            case 'checkbox':    $type = 'boolean'; break;
             case 'currency': $f = DB::dict()->ActualType('C').'(128)'; break;
-            case 'autonumber': $len = strlen(self::format_autonumber_str($param, null));
-                $f = DB::dict()->ActualType('C') . "($len)"; break;
+            case 'autonumber': break;
         }
         if ($type) {
-            $opts['id'] = $f['id'];
-            $opts['label'] = _V($f['name']);
+            $opts['id'] = $prefix . $f['id'] . $sufix;
+            $opts['label'] = $label_prefix . _V($f['name']);
             $opts['type'] = $type;
             if ($values) {
                 $opts['values'] = $values;
@@ -102,21 +128,17 @@ class Utils_RecordBrowser_QueryBuilderIntegration
             if ($input) {
                 $opts['input'] = $input;
             }
-            return $opts;
+            return $filters;
         }
         return null;
     }
 
-    private function permissions_get_field_values($field, $in_depth=true) {
-        static $all_fields = array();
-        if (!isset($all_fields[$this->tab]))
-            foreach ($this->table_rows as $k=>$v)
-                $all_fields[$this->tab][$v['id']] = $k;
-        $args = $this->table_rows[$all_fields[$this->tab][$field]];
+    private static function permissions_get_field_values($tab, $args) {
         $arr = array(''=>'['.__('Empty').']');
+        $field = $args['id'];
         switch (true) {
             case $args['type']=='text' && $args['filter']:
-                $arr_add = @DB::GetAssoc('SELECT f_'.$args['id'].', f_'.$args['id'].' FROM '.$this->tab.'_data_1 GROUP BY f_'.$args['id'].' ORDER BY count(*) DESC LIMIT 20');
+                $arr_add = @DB::GetAssoc('SELECT f_'.$args['id'].', f_'.$args['id'].' FROM '.$tab.'_data_1 GROUP BY f_'.$args['id'].' ORDER BY count(*) DESC LIMIT 20');
                 if($arr_add) $arr += $arr_add;
                 break;
             case $args['commondata']:
@@ -124,8 +146,8 @@ class Utils_RecordBrowser_QueryBuilderIntegration
                 if (strpos($array_id, '::')===false)
                     $arr = $arr + Utils_CommonDataCommon::get_translated_array($array_id, is_array($args['param'])?$args['param']['order_by_key']:false);
                 break;
-            case $this->tab=='contact' && $field=='login' ||
-                 $this->tab=='rc_accounts' && $field=='epesi_user': // just a quickfix, better solution will be needed
+            case $tab=='contact' && $field=='login' ||
+                 $tab=='rc_accounts' && $field=='epesi_user': // just a quickfix, better solution will be needed
                 $arr = $arr + array('USER_ID'=>__('User Login'));
                 break;
             case $args['type']=='date' || $args['type']=='timestamp':
@@ -141,37 +163,6 @@ class Utils_RecordBrowser_QueryBuilderIntegration
             case ($args['type']=='select' || $args['type']=='multiselect') && isset($args['ref_table']):
                 if ($args['ref_table']=='contact') $arr = $arr + array('USER'=>__('User Contact'));
                 if ($args['ref_table']=='company') $arr = $arr + array('USER_COMPANY'=>__('User Company'));
-                if (!$in_depth) continue;
-
-                $last_tab = $this->tab;
-                $tabs = explode(',', $args['ref_table']);
-                if (count($tabs) != 1) break;
-                $one_tab = reset($tabs);
-                if ($one_tab != '__RECORDSETS__'
-                    && Utils_RecordBrowserCommon::check_table_name($one_tab, false, false)) {
-                    $this->tab = $one_tab;
-                    $this->init();
-                    if (!isset($all_fields[$this->tab]))
-                        foreach ($this->table_rows as $k=>$v)
-                            $all_fields[$this->tab][$v['id']] = $k;
-
-
-                    foreach ($all_fields[$this->tab] as $k=>$v) {
-                        if ($this->table_rows[$v]['type']=='calculated' || $this->table_rows[$v]['type']=='hidden') unset($all_fields[$this->tab][$k]);
-                        else {
-                            $arr2 = $this->permissions_get_field_values($k, false, $this->tab);
-                            foreach ($arr2 as $k2=>$v2)
-                                $arr2[$k2] = '"'.$k2.'":"'.$v2.'"';
-                            eval_js('utils_recordbrowser__field_sub_values["'.$field.'__'.$k.'"] = {'.implode(',',$arr2).'};');
-                        }
-                    }
-                    foreach ($all_fields[$this->tab] as $k=>$v) {
-                        $arr[$k] = __(' records with %s set to ', array(_V($v)));
-                    }
-                }
-
-                $this->tab = $last_tab;
-                $this->init();
                 break;
         }
         return $arr;
@@ -231,6 +222,8 @@ class Utils_RecordBrowser_QueryBuilderIntegration
                 $ret = $arr['condition'] == 'AND' ? rb_and($rules) : rb_or($rules);
             }
         } elseif (isset($arr['field']) && isset($arr['operator']) && isset($arr['value'])) {
+//            $fields = explode('__sub__', $arr['field']);
+//            $field = isset($fields[1]) ? "$fields[0][$fields[1]]" : $fields[0];
             $field = $arr['field'];
             $operator = self::map_query_builder_operator_to_crits($arr['operator']);
             $value = $arr['value'];
