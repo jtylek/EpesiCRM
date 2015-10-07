e4c0d42747660eef5dd65bc7ddb54a8c92ae8a4e
Adam Bukowski <bukowski.adam@gmail.com>
2015-10-07 12:14:06 +0200

    [FIX] Crits issue with USER_COMPANY special crit

--- modules/CRM/Contacts/ContactsCommon_0.php
+++ modules/CRM/Contacts/ContactsCommon_0.php
@@ -1353,7 +1353,7 @@ class CRM_ContactsCommon extends ModuleCommon {
         $ret = array();
         $me = self::get_my_record();
         $my_contact_id = $me['id'] ? $me['id'] : -1;
-        $my_company_id = isset($me['company_name']) ? $me['company_name'] : -1;
+        $my_company_id = (isset($me['company_name']) && $me['company_name']) ? $me['company_name'] : -1;
         $ret[] = new Utils_RecordBrowser_ReplaceValue('USER', __('User Contact'), $my_contact_id);
         $ret[] = new Utils_RecordBrowser_ReplaceValue('USER_COMPANY', __('User Company'), $my_company_id);
         return $ret;
