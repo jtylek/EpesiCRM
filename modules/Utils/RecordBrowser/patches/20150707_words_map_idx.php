3f2096ce8142236d765a272245750a015382692b
Paweł Bukowski <pbukowski@telaxus.com>
2015-12-14 16:05:59 +0100

    [fix] Utils/RecordBrowser - patch

--- modules/Utils/RecordBrowser/patches/20150707_words_map_idx.php
+++ modules/Utils/RecordBrowser/patches/20150707_words_map_idx.php
@@ -1,6 +1,19 @@
 <?php
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
+$checkpoint = Patch::checkpoint('truncate');
+if (!$checkpoint->is_done()) {
+    $size = DB::GetOne('SELECT count(*) FROM recordbrowser_words_map');
+    if($size>100000) {
+        @DB::Execute('TRUNCATE recordbrowser_words_map');
+        $tabs = DB::GetCol('SELECT tab FROM recordbrowser_table_properties');
+        foreach($tabs as $tab) DB::Execute('UPDATE ' . $tab . '_data_1 SET indexed=0');
+        return false;
+
+    }
+    $checkpoint->done();
+}
+
 $checkpoint = Patch::checkpoint('word_index');
 if (!$checkpoint->is_done()) {
     DB::CreateIndex('rb_words_map__word_idx', 'recordbrowser_words_map', 'word_id');
