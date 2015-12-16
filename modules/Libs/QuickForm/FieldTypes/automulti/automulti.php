0a203b7c7f39fca1f1c00bae8acc7515b1358658
Adam Bukowski <bukowski.adam@gmail.com>
2015-12-16 13:46:05 +0100

    [FIX] automulti default value for frozen form

--- modules/Libs/QuickForm/FieldTypes/automulti/automulti.php
+++ modules/Libs/QuickForm/FieldTypes/automulti/automulti.php
@@ -338,7 +338,7 @@ class HTML_QuickForm_automulti extends HTML_QuickForm_element {
                 $value = $this->_findValue($caller->_submitValues);
                 // Fix for bug #4465 & #5269
                 // XXX: should we push this to element::onQuickFormEvent()?
-                if (null === $value) {
+                if (null === $value && ((is_callable(array($caller,'isSubmitted')) && !$caller->isSubmitted()) || $this->isFrozen())) {
                     $value = $this->_findValue($caller->_defaultValues);
                 }
             }
