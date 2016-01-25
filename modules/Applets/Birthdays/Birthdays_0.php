21a17dd47533442ab5f7435735f226657d923f08
Adam Bukowski <bukowski.adam@gmail.com>
2016-01-25 14:46:23 +0100

    [FIX] Birthday applet with new crits. Fix install file. Allow past birthdays
    Do not use raw sql crits as they are malformed for postgresql - no cast
    on field value causes types mismatch.

--- modules/Applets/Birthdays/Birthdays_0.php
+++ modules/Applets/Birthdays/Birthdays_0.php
@@ -16,14 +16,16 @@ class Applets_Birthdays extends Module {
 	public function body() {
 	}
 
-public function applet($conf, & $opts) {
+	public function applet($conf, & $opts) {
 		//available applet options: toggle,href,title,go,go_function,go_arguments,go_contruct_arguments
 		$opts['go'] = false; // enable/disable full screen
 		$opts['title'] = $conf['title'];
 
+		$title = __('Birthdays upcoming in the next: %d days.', array($conf['no_of_days']));
+		$sort_order = 'ASC';
+
 		// initialize the recordset
 		$rb = $this->init_module(Utils_RecordBrowser::module_name(),'contact','contact');
-		$me = CRM_ContactsCommon::get_my_record();
 
 		// $conds - parameters for the applet
 		// 1st - table field names, width, truncate
@@ -41,20 +43,26 @@ public function applet($conf, & $opts) {
 						);
 		// 2nd - criteria (filter)
 		// TO DO - filter date - today through today+2 weeks
-		$dates = array();
-		for ($i=0;$i<$conf['no_of_days'];$i++)
-			$dates[] = DB::Concat(DB::qstr('%'),DB::qstr(date('-m-d',strtotime(Base_RegionalSettingsCommon::time2reg(strtotime('+'.$i.' days'), false, true, true, false)))));
+		$op1 = '>=';
+		$op2 = '<';
+		if ($conf['no_of_days'] < 0) {
+			$op1 = '<';
+			$op2 = '>=';
+			$title = __('Birthdays from the last %d days.', array(-$conf['no_of_days']));
+			$sort_order = 'DESC';
+		}
+		$today = Base_RegionalSettingsCommon::time2reg(null, false, true, true, false);
+		$to_date = Base_RegionalSettingsCommon::time2reg(strtotime("+$conf[no_of_days] days"), false, true, true, false);
+		$crits = array("{$op1}birth_date" => $today, "{$op2}birth_date" => $to_date);
 		if ( (isset($conf['cont_type'])) && ($conf['cont_type']=='f') ) {
-				$crits=array(':Fav'=>true,'"~birth_date'=>$dates);
-			} else {
-				$crits=array('"~birth_date'=>$dates);
+			$crits[':Fav'] = true;
 		}
 
 		// 3rd - sorting
-		$sorting = array('last_name'=>'ASC');
+		$sorting = array('birth_date'=> $sort_order);
 
 		// 4th - function to return tooltip
-		$tooltip = 'test';
+		$tooltip = null;
 
 		// 5th - limit how many records are returned, null = no limit
 		$limit = null;
@@ -71,7 +79,7 @@ public function applet($conf, & $opts) {
 									& $opts
 				);
 		// initialize miniview
-		print(__('Birthdays upcoming in the next: %d days.', array($conf['no_of_days'])));
+		print($title);
 		$this->display_module($rb, $conds, 'mini_view');
 	}
 }
