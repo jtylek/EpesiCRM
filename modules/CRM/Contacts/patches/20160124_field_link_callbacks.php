a2ddcdd0a854d717215944a359e2d8a47c2a825c
Adam Bukowski <bukowski.adam@gmail.com>
2016-01-25 13:14:18 +0100

    [FIX] Contacts display callback patch to use set_display_callback method

--- modules/CRM/Contacts/patches/20160124_field_link_callbacks.php
+++ modules/CRM/Contacts/patches/20160124_field_link_callbacks.php
@@ -1,5 +1,6 @@
 <?php
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
-DB::Execute('UPDATE contact_callback SET callback="Utils_RecordBrowserCommon::display_linked_field_label" WHERE freezed=1 AND (field="First Name" OR field="Last Name")');
-DB::Execute('UPDATE company_callback SET callback="Utils_RecordBrowserCommon::display_linked_field_label" WHERE freezed=1 AND field="Company Name"');
+Utils_RecordBrowserCommon::set_display_callback('contact', 'First Name', 'Utils_RecordBrowserCommon::display_linked_field_label');
+Utils_RecordBrowserCommon::set_display_callback('contact', 'Last Name', 'Utils_RecordBrowserCommon::display_linked_field_label');
+Utils_RecordBrowserCommon::set_display_callback('company', 'Company Name', 'Utils_RecordBrowserCommon::display_linked_field_label');
