a73db9410addbdda029337e14e608d58e188f479
Adam Bukowski <bukowski.adam@gmail.com>
2016-01-26 10:44:48 +0100

    [FIX] Contacts display callback patch to check current callback
    Change to new only if it's set to default

--- modules/CRM/Contacts/patches/20160124_field_link_callbacks.php
+++ modules/CRM/Contacts/patches/20160124_field_link_callbacks.php
@@ -4,7 +4,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 $callback = 'Utils_RecordBrowserCommon::display_linked_field_label';
 
 Utils_RecordBrowserCommon::display_callback_cache('contact');
-$cache = & Utils_RecordBrowserCommon::$display_callback_table;
+$cache = & Utils_RecordBrowserCommon::$display_callback_table['contact'];
 
 $field = 'First Name';
 if ($cache[$field] == 'CRM_ContactsCommon::display_fname') {
@@ -18,7 +18,7 @@ if ($cache[$field] == 'CRM_ContactsCommon::display_lname') {
 }
 
 Utils_RecordBrowserCommon::display_callback_cache('company');
-$cache = & Utils_RecordBrowserCommon::$display_callback_table;
+$cache = & Utils_RecordBrowserCommon::$display_callback_table['company'];
 $field = 'Company Name';
 if ($cache[$field] == 'CRM_ContactsCommon::display_cname') {
     Utils_RecordBrowserCommon::set_display_callback('contact', $field, $callback);
