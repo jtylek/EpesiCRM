df6e91cda12a5b1b772abddd40d8432cf23d3656
Adam Bukowski <bukowski.adam@gmail.com>
2016-03-15 12:43:12 +0100

    [FIX] Attachment download and printouts

--- modules/Utils/Attachment/AttachmentCommon_0.php
+++ modules/Utils/Attachment/AttachmentCommon_0.php
@@ -302,6 +302,10 @@ class Utils_AttachmentCommon extends ModuleCommon {
         if(!$crypted || isset($_SESSION['client']['cp'.$row['id']])) {
             $files = DB::GetAll('SELECT id, created_by, created_on, original, filestorage_id, (SELECT count(*) FROM utils_attachment_download uad WHERE uaf.id=uad.attach_file_id) as downloads FROM utils_attachment_file uaf WHERE uaf.attach_id=%d AND uaf.deleted=0', array($row['id']));
             foreach ($files as $f) {
+                if ($nolink) {
+                    $icon .= '<br>' . $f['original'];
+                    continue;
+                }
                 try {
                     $meta = Utils_FileStorageCommon::meta($f['filestorage_id']);
                     $f_filename = $meta['file'];
@@ -358,6 +362,10 @@ class Utils_AttachmentCommon extends ModuleCommon {
             self::$mark_as_read = array();
         }
 
+        if ($nolink) {
+            $files = $icon ? __('Files') . ':' . $icon : '';
+            return $text.$files;
+        }
         $text = (!$view?'<b style="float:left;margin-right:30px;">'.$row['title'].'</b> ':'').$text.$icon.$inline_img;
         if($row['sticky']) $text = '<img src="'.Base_ThemeCommon::get_template_file('Utils_Attachment','sticky.png').'" hspace=3 align="left"> '.$text;
 
