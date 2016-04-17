cb8eb7b1bea5417626db12b61bc33d7bde896bea
Georgi Hristov <ghristov@gmx.de>
2016-04-17 14:59:09 +0200

    [UPDATE] jquery rewrite of edit_history.js

--- modules/Utils/RecordBrowser/edit_history.js
+++ modules/Utils/RecordBrowser/edit_history.js
@@ -1,20 +1,21 @@
 recordbrowser_edit_history_jump = function (selected_date, tab, id, form_name) {
-	$('historical_view_pick_date').value = selected_date;
+	jq('#historical_view_pick_date').val(selected_date);
 	recordbrowser_edit_history(tab, id, form_name);
 }
 
 recordbrowser_edit_history = function (tab, id, form_name) {
 	var field = "historical_view_pick_date";
-	new Ajax.Request('modules/Utils/RecordBrowser/edit_history.php', {
-		method: 'post',
-		parameters:{
+	jq.ajax({
+		type: 'post',
+		url: 'modules/Utils/RecordBrowser/edit_history.php', 
+		data:{
 			tab: tab,
 			id: id,
 			date: document.forms[form_name].elements[field].value,
 			cid: Epesi.client_id
 		},
-		onSuccess:function(t) {
-			eval(t.responseText);
+		success:function(response) {
+			eval(response);
 		}
 	});
 }
