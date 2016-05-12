umnierazniej/EPESI

Add basic table

modules/Utils/GenericBrowser/GenericBrowser_0.php
@@ -895,6 +895,8 @@ class Utils_GenericBrowser extends Module {
 			eval_js_once('gb_collapse_icon_off = "' . Base_ThemeCommon::get_template_file(Utils_GenericBrowser::module_name(), 'collapse_gray.gif') . '";');
 		}
 
+		$rows_data = array();
+
 		foreach ($this->rows as $i => $r) {
 			$col = array();
 
@@ -987,11 +989,14 @@ class Utils_GenericBrowser extends Module {
 
 			ksort($col);
 			$expanded = $this->expandable ? ' expanded' : '';
+			$row_data = array();
 			foreach ($col as $v)
-				$out_data[] = array('label' => '<div class="expandable' . $expanded . '">' . $v['label'] . '</div>', 'attrs' => $v['attrs']);
+				$row_data[] = array('label' => '<div class="expandable' . $expanded . '">' . $v['label'] . '</div>', 'attrs' => $v['attrs']);
 			if (isset($this->rows_jses[$i]))
 				eval_js($this->rows_jses[$i]);
+			$rows_data[] = $row_data;
 		}
+
 		if (isset($quickjump)) {
 			$quickjump_to = $this->get_module_variable('quickjump_to');
 			$all = '<span class="all">' . __('All') . '</span>';
@@ -1012,6 +1017,10 @@ class Utils_GenericBrowser extends Module {
 			$options['letter_links'] = $letter_links;
 			$options['quickjump_to'] = $quickjump_to;
 		}
+
+		foreach($rows_data as $row_data)
+			foreach($row_data as $col_data)
+				$out_data[] = $col_data;
 
 		$options['data'] = $out_data;
 		$options['cols'] = $out_headers;
@@ -1071,6 +1080,11 @@ class Utils_GenericBrowser extends Module {
 		else
 			$theme->display();
 		$this->set_module_variable('show_all_triggered', false);
+		$this->display('table.twig', array(
+			'columns' => $out_headers,
+			'rows' => $rows_data,
+			'summary' => $this->summary()
+		));
 	}
 	
 	public function show_all() {

jtylek/EpesiCRM

modules/Utils/GenericBrowser/GenericBrowser_0.php
@@ -901,6 +901,8 @@ class Utils_GenericBrowser extends Module {
 			eval_js_once('gb_collapse_icon_off = "' . Base_ThemeCommon::get_template_file(Utils_GenericBrowser::module_name(), 'collapse_gray.gif') . '";');
 		}
 
+		$rows_data = array();
+
 		foreach ($this->rows as $i => $r) {
 			$col = array();
 
@@ -993,11 +995,14 @@ class Utils_GenericBrowser extends Module {
 
 			ksort($col);
 			$expanded = $this->expandable ? ' expanded' : '';
+			$row_data = array();
 			foreach ($col as $v)
-				$out_data[] = array('label' => '<div class="expandable' . $expanded . '">' . $v['label'] . '</div>', 'attrs' => $v['attrs']);
+				$row_data[] = array('label' => '<div class="expandable' . $expanded . '">' . $v['label'] . '</div>', 'attrs' => $v['attrs']);
 			if (isset($this->rows_jses[$i]))
 				eval_js($this->rows_jses[$i]);
+			$rows_data[] = $row_data;
 		}
+
 		if (isset($quickjump)) {
 			$quickjump_to = $this->get_module_variable('quickjump_to');
 			$all = '<span class="all">' . __('All') . '</span>';
@@ -1018,6 +1023,10 @@ class Utils_GenericBrowser extends Module {
 			$options['letter_links'] = $letter_links;
 			$options['quickjump_to'] = $quickjump_to;
 		}
+
+		foreach($rows_data as $row_data)
+			foreach($row_data as $col_data)
+				$out_data[] = $col_data;
 
 		$options['data'] = $out_data;
 		$options['cols'] = $out_headers;
@@ -1079,6 +1088,11 @@ class Utils_GenericBrowser extends Module {
 		else
 			$theme->display();
 		$this->set_module_variable('show_all_triggered', false);
+		$this->twig_display('table.twig', array(
+			'columns' => $out_headers,
+			'rows' => $rows_data,
+			'summary' => $this->summary()
+		));
 	}
 	
 	public function show_all() {