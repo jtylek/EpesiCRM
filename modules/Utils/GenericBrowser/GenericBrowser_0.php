ec0d84882668072c7e0bc4c7326b373a32cc0652
Paweł Jedwabny <pjedwabny@gmail.com>
2016-06-27 16:51:21 +0200

    Revert "Hide old table in generic browser"
    This reverts commit 849e58de08fb1012785639e11e38a04cefae21ce.

--- modules/Utils/GenericBrowser/GenericBrowser_0.php
+++ modules/Utils/GenericBrowser/GenericBrowser_0.php
@@ -1096,10 +1096,10 @@ class Utils_GenericBrowser extends Module {
 		foreach($options as $key => $value)
 			$theme->assign($key, $value);
 
-//		if (isset($template))
-//			$theme->display($template, true);
-//		else
-//			$theme->display();
+		if (isset($template))
+			$theme->display($template, true);
+		else
+			$theme->display();
 		$this->set_module_variable('show_all_triggered', false);
 	}
 	
