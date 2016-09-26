bef9a3f6730fdfe0ef60bd5e14c38e4157423174
Adam Bukowski <bukowski.adam@gmail.com>
2016-09-26 12:51:49 +0200

    Load all console commands. Remove use statements.
    Use fully qualified class names as there is only one usage for each class

--- console.php
+++ console.php
@@ -5,25 +5,21 @@
 define('SET_SESSION', false);
 require 'include.php';
 
-use Epesi\Console\CacheRebuildCommand;
-use Epesi\Console\Maintenance\MaintenanceStatusCommand;
-use Epesi\Console\Modules\InstallModuleCommand;
-use Epesi\Console\Modules\UninstallModuleCommand;
-use Epesi\Console\ThemeRebuildCommand;
 use Symfony\Component\Console\Application;
-use Epesi\Console\Modules\ListModulesCommand;
-use Epesi\Console\Modules\DisableModuleCommand;
-use Epesi\Console\Modules\EnableModuleCommand;
 
 ModuleManager::load_modules();
 
 $application = new Application();
-$application->add(new ListModulesCommand());
-$application->add(new DisableModuleCommand());
-$application->add(new EnableModuleCommand());
-$application->add(new CacheRebuildCommand());
-$application->add(new ThemeRebuildCommand());
-$application->add(new MaintenanceStatusCommand());
-$application->add(new InstallModuleCommand());
-$application->add(new UninstallModuleCommand());
+$application->add(new \Epesi\Console\Modules\ListModulesCommand());
+$application->add(new \Epesi\Console\Modules\DisableModuleCommand());
+$application->add(new \Epesi\Console\Modules\EnableModuleCommand());
+$application->add(new \Epesi\Console\Modules\InstallModuleCommand());
+$application->add(new \Epesi\Console\Modules\UninstallModuleCommand());
+$application->add(new \Epesi\Console\CacheRebuildCommand());
+$application->add(new \Epesi\Console\ThemeRebuildCommand());
+$application->add(new \Epesi\Console\Maintenance\MaintenanceStatusCommand());
+$application->add(new \Epesi\Console\Maintenance\MaintenanceOnCommand());
+$application->add(new \Epesi\Console\Maintenance\MaintenanceOffCommand());
+$application->add(new \Epesi\Console\SearchClearCommand());
+$application->add(new \Epesi\Console\SearchIndexCommand());
 $application->run();
