fecb80ce2d5763584d34ed920f6ec74796de821c
Paweł Jedwabny <pjedwabny@gmail.com>
2016-10-24 16:06:42 +0200

    More than 5 arguments allowed in onQuickFormEvent createElement event

--- libs/quickform/QuickForm/element.php
+++ libs/quickform/QuickForm/element.php
@@ -396,7 +396,7 @@ class HTML_QuickForm_element extends HTML_Common
         switch ($event) {
             case 'createElement':
                 $className = get_class($this);
-                $this->$className($arg[0], $arg[1], $arg[2], $arg[3], $arg[4]);
+                call_user_func_array(array($this, $className), $arg);
                 break;
             case 'addElement':
                 $this->onQuickFormEvent('createElement', $arg, $caller);
