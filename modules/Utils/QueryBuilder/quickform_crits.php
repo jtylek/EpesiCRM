e94d2c7426243f2d0eaf287c490ea73e6e9ba35b
Paweł Jedwabny <pjedwabny@gmail.com>
2016-11-06 10:47:29 +0100

    Use PHP5 constructor in crits

--- modules/Utils/QueryBuilder/quickform_crits.php
+++ modules/Utils/QueryBuilder/quickform_crits.php
@@ -10,8 +10,8 @@
 
 class HTML_QuickForm_crits extends HTML_QuickForm_input {
 
-    function HTML_QuickForm_crits($elementName = null, $elementLabel = null, $attributes=null) {
-        HTML_QuickForm_input::HTML_QuickForm_input($elementName, $elementLabel, $attributes);
+    function __construct($elementName = null, $elementLabel = null, $attributes=null) {
+        parent::__construct($elementName, $elementLabel, $attributes);
         if ($this->_caller instanceof HTML_QuickForm) {
             $this->_caller->addFormRule(array($this, 'check_for_error'));
         }
