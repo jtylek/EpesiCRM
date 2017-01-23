e1137d9c3085581e059094b15696755139fe4213
Adam Bukowski <bukowski.adam@gmail.com>
2017-01-23 14:34:10 +0100

    [UPDATE] Use bootstrap datetimepicker

--- modules/Utils/PopupCalendar/datepicker.php
+++ modules/Utils/PopupCalendar/datepicker.php
@@ -1,80 +1,100 @@
 <?php
+
 /**
- * @author Paul Bukowski <pbukowski@telaxus.com> and Arkadiusz Bisaga <abisaga@telaxus.com>
- * @copyright Copyright &copy; 2008, Telaxus LLC
+ * @author  Paul Bukowski <pbukowski@telaxus.com>
+ *          Arkadiusz Bisaga <abisaga@telaxus.com>
+ *          Adam Bukowski <abukowski@telaxus.com>
+ * @copyright Copyright &copy; 2017, Telaxus LLC
  * @license MIT
  * @version 1.0
  * @package epesi-Utils
  * @subpackage PopupCalendar
  */
-class HTML_QuickForm_datepicker extends HTML_QuickForm_input {
+class HTML_QuickForm_datepicker extends HTML_QuickForm_text
+{
 
-	function __construct($elementName=null, $elementLabel=null, $attributes=null) {
-		parent::__construct($elementName, $elementLabel, $attributes);
-		$this->_persistantFreeze = true;
-		$this->setType('datepicker');
-//		$this->updateAttributes(array('readonly'=>1));
-	} //end constructor
+    function getMomentJsDateFormat()
+    {
+        $trans = array(
+            '%Y-%m-%d' => 'YYYY-MM-DD',
+            '%m/%d/%Y' => 'MM/DD/YYYY',
+            '%d %B %Y' => 'DD[ ]MMMM YYYY',
+            '%d %b %Y' => 'DD MMM YYYY',
+            '%b %d, %Y' => 'MMM DD, YYYY');
+        $date_format = Base_RegionalSettingsCommon::date_format();
+        return isset($trans[$date_format]) ? $trans[$date_format] : 'YYYY-MM-DD';
+    }
 
-	function toHtml() {
-		$str = "";
-		if ($this->_flagFrozen) {
-			$str .= $this->getFrozenHtml();
-		} else {
-			$value = $this->getAttribute('value');
-			if (is_numeric($value)) $value = date('Y-m-d', $value);
-			$id = $this->getAttribute('id');
-			$name = $this->getAttribute('name');
-			if ($value) $this->setAttribute('value',Base_RegionalSettingsCommon::time2reg($value,false,true,false));
-			if(!isset($id)) {
-				$id = 'datepicker_field_'.$name;
-				$this->updateAttributes(array('id'=>$id));
-			}
-			$ex_date = Base_RegionalSettingsCommon::time2reg(null,false,true,false);
-			$date_format = Base_RegionalSettingsCommon::date_format();
-			$this->setType('text');
-            if (!$this->getAttribute('placeholder'))
-                $this->setAttribute('placeholder', __('Click to select date'));
-            $js = Utils_PopupCalendarCommon::create_href(md5($id),
-                    'new jq.ajax(\'modules/Utils/PopupCalendar/up.php\','.
-                    '{method:\'post\', data:{date: __YEAR__+\'-\'+__MONTH__+\'-\'+__DAY__},'.
-                    'success:function(t){e=jq(\'#'.Epesi::escapeJS($id,false).'\');if(e.length) {e.val(t);e.change();}}})',
-                    null,null,
-                    'jq(popup).clonePosition(\'#'.$id.'\',{cloneWidth:false,cloneHeight:false,offsetTop:jq(\'#'.$id.'\').height()})',$value, $id);
-            $str .= '<input ' . $js . ' ' . $this->_getAttrString($this->_attributes) . ' '.Utils_TooltipCommon::open_tag_attrs(__('Example date: %s',array($ex_date)), false ).' />';
-			eval_js('jq(\'#'.$id.'\').keypress(function(ev){Utils_PopupCalendarDatePicker.validate(ev,\''.Epesi::escapeJS($date_format,false).'\')})');
-			eval_js('jq(\'#'.$id.'\').blur(function(ev){Utils_PopupCalendarDatePicker.validate_blur(ev,\''.Epesi::escapeJS($date_format,false).'\')})');
-		}
-		return $str;
-	} //end func toHtml
+    function getHtml()
+    {
+        $value = $this->getAttribute('value');
+        if (is_numeric($value)) $value = date('Y-m-d', $value);
+        $id = $this->getAttribute('id');
+        $name = $this->getAttribute('name');
+        if (!isset($id)) {
+            $id = 'datepicker_field_' . $name;
+            $id = str_replace(['[', ']'], '_', $id);
+            $this->updateAttributes(array('id' => $id));
+        }
+        if (!$this->getAttribute('placeholder'))
+            $this->setAttribute('placeholder', __('Click to select date'));
+        load_js('libs/moment-with-locales.min.js');
+        load_js('libs/bootstrap-datetimepicker.min.js');
+        load_css('libs/bootstrap-datetimepicker.min.css');
 
-	function getValue() {
-		$value = $this->getAttribute('value');
-		if (!$value) return $value;
+        $options = $this->getDatetimepickerOptions();
+        eval_js('jq(\'#' . $id . '\').datetimepicker(' . json_encode($options) . ');');
+        if ($value) {
+            eval_js('jq(\'#' . $id . '\').data("DateTimePicker").date(moment(\'' . $value . '\'));');
+        }
+        return parent::getHtml();
+    }
+
+    public function getDatetimepickerOptions()
+    {
+        $date_format = $this->getMomentJsDateFormat();
+        $lang = Base_LangCommon::get_lang_code();
+        $options = array(
+            'format' => $date_format,
+            'locale' => $lang,
+            'showTodayButton' => true,
+            'showClear' => true,
+            'useCurrent' => false,
+        );
+        return $options;
+    }
+
+    function getValue()
+    {
+        $value = $this->getAttribute('value');
+        if (!$value) return $value;
 //		print('get_value('.$this->getName().')='.$value.' '.Base_RegionalSettingsCommon::time2reg($value,false,true,false).'<hr>');
-                if(!is_numeric($value) && is_string($value) && !strtotime($value)) return $value;
-		return Base_RegionalSettingsCommon::time2reg($value,false,true,false);
-	} // end func setValue
+        if (!is_numeric($value) && is_string($value) && !strtotime($value)) return $value;
+        $time2reg = Base_RegionalSettingsCommon::time2reg($value, false, true, false);
+        return $time2reg;
+    } // end func setValue
 
-	function exportValue(&$submitValues, $assoc = false) {
+    function exportValue(&$submitValues, $assoc = false)
+    {
         $value = $this->_findValue($submitValues);
         if (is_null($value)) {
             $value = $this->getValue();
-        } 
-		if ($value=='') return $this->_prepareValue('', $assoc);
-		$cleanValue = date('Y-m-d',Base_RegionalSettingsCommon::reg2time($value,false));
-		return $this->_prepareValue($cleanValue, $assoc);
-	}
+        }
+        if ($value == '') return $this->_prepareValue('', $assoc);
+        $cleanValue = date('Y-m-d', Base_RegionalSettingsCommon::reg2time($value, false));
+        return $this->_prepareValue($cleanValue, $assoc);
+    }
 
-    function onQuickFormEvent($event, $arg, &$caller) {
-		if($event=='updateValue' && is_callable(array($caller,'applyFilter')))
-			$caller->applyFilter($this->getName(),array($this,'reg2time'));
-		return parent::onQuickFormEvent($event,$arg,$caller);
-	}
-	
-	function reg2time($value) {
-		if (!$value) return '';
-		return strftime('%Y-%m-%d',Base_RegionalSettingsCommon::reg2time($value,false));
-	}
-} //end class HTML_QuickForm_datepicker
-?>
+    function onQuickFormEvent($event, $arg, &$caller)
+    {
+        if ($event == 'updateValue' && is_callable(array($caller, 'applyFilter')))
+            $caller->applyFilter($this->getName(), array($this, 'reg2time'));
+        return parent::onQuickFormEvent($event, $arg, $caller);
+    }
+
+    function reg2time($value)
+    {
+        if (!$value) return '';
+        return strftime('%Y-%m-%d', Base_RegionalSettingsCommon::reg2time($value, false));
+    }
+}
--- modules/Utils/PopupCalendar/timestamp.php
+++ modules/Utils/PopupCalendar/timestamp.php
@@ -1,166 +1,49 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-/**
- * Class for a group of elements used to input dates (and times).
- * 
- * PHP versions 4 and 5
- *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category	HTML
- * @package	 HTML_QuickForm
- * @author	  Alexey Borzov <avb@php.net>
- * @copyright   2001-2007 The PHP Group
- * @license	 http://www.php.net/license/3_01.txt PHP License 3.01
- * @version	 CVS: $Id: date.php,v 1.60 2007/06/04 19:22:23 avb Exp $
- * @link		http://pear.php.net/package/HTML_QuickForm
- */
-
-/**
- * Class for <select></select> elements
- */
 require_once 'datepicker.php';
 
 /**
- * Class for a group of elements used to input dates (and times).
- * 
- * Inspired by original 'date' element but reimplemented as a subclass
- * of HTML_QuickForm_group
- * 
- * @category	HTML
- * @package	 HTML_QuickForm
- * @author	  Alexey Borzov <avb@php.net>
- * @version	 Release: 3.2.9
- * @since	   3.1
+ * @author Adam Bukowski <abukowski@telaxus.com>
+ * @copyright Copyright &copy; 2017, Telaxus LLC
+ * @license MIT
+ * @version 1.0
+ * @package epesi-Utils
+ * @subpackage PopupCalendar
  */
-class HTML_QuickForm_timestamp extends HTML_QuickForm_group
+class HTML_QuickForm_timestamp extends HTML_QuickForm_datepicker
 {
-	private $_elementName;
-	
-	// }}}
-	// {{{ constructor
+    protected $showDate = true;
 
-	function __construct($elementName = null, $elementLabel = null, $options = array(), $attributes = null) {
-	    parent::__construct($elementName, $elementLabel);
-	    $this->setAttributes($attributes);
-		$this->_elementName = $elementName;
-		$this->_persistantFreeze = true;
-		$this->_appendName = true;
-		$this->_type = 'group';
-		$this->_options = $options;
-	}
+    /**
+     * Class constructor
+     *
+     * @param     string $elementName       (optional) Input field name attribute
+     * @param     string $elementLabel      (optional) Input field label
+     * @param     array $options            (optional) Input field label
+     * @param     mixed  $attributes        (optional) Either a typical HTML attribute string
+     *                                      or an associative array
+     */
+    public function __construct($elementName = null, $elementLabel = null, $options = array(), $attributes = null)
+    {
+        parent::__construct($elementName, $elementLabel, $attributes);
+        if (isset($options['date'])) {
+            $this->showDate = $options['date'] == true;
+        }
+    }
 
-	// }}}
-	// {{{ _createElements()
+    function getMomentJsDateFormat()
+    {
+        $date_format = $this->showDate ? parent::getMomentJsDateFormat() . ' ' : '';
+        $time_format = Base_RegionalSettingsCommon::time_12h() ? 'hh:mm a' : 'HH:mm';
+        return $date_format . $time_format;
+    }
 
-	function _createElements() {
-		$time_format = Base_RegionalSettingsCommon::time_12h()?'h:i a':'H:i';
-		$lang_code = Base_LangCommon::get_lang_code();
-		$this->_options['format'] = $time_format;
-		if (!isset($this->_options['optionIncrement'])) $this->_options['optionIncrement'] = array('i' => 5);
-		$this->_options['language'] = $lang_code;
-		if (!isset($this->_options['date'])) $this->_options['date'] = true;
+    public function getDatetimepickerOptions()
+    {
+        $options = parent::getDatetimepickerOptions();
+        $options['sideBySide'] = true;
+        $options['stepping'] = 5;
+        return $options;
+    }
 
-        $attributes = $this->getAttributes();
-        $date_attr = is_array($attributes) ? array_merge($attributes, array('style' => 'width:auto;')) : array('style' => 'width:auto;');
-        $this->_elements['__date'] = new HTML_QuickForm_date('__date', null, $this->_options, $date_attr);
-        if ($this->_options['date'])
-            $this->_elements['__datepicker'] = new HTML_QuickForm_datepicker('__datepicker', null, $attributes);
-	}
-
-	// }}}
-	// {{{ _createOptionList()
-
-   /**
-	* Creates an option list containing the numbers from the start number to the end, inclusive
-	*
-	* @param	int	 The start number
-	* @param	int	 The end number
-	* @param	int	 Increment by this value
-	* @access   private
-	* @return   array   An array of numeric options.
-	*/
-	function _createOptionList($start, $end, $step = 1) {
-		for ($i = $start, $options = array(); $start > $end? $i >= $end: $i <= $end; $i += $step) {
-			$options[$i] = sprintf('%02d', $i);
-		}
-		return $options;
-	}
-
-	// }}}
-	// {{{ setValue()
-
-	// }}}
-	// {{{ toHtml()
-
-	function toHtml() {
-		$renderer = new HTML_QuickForm_Renderer_Default();
-		$renderer->setElementTemplate('{element}');
-		$renderer->setGroupElementTemplate('<div>{element}</div>', $this->_elementName);
-		parent::accept($renderer);
-		return str_replace('&nbsp;','',$renderer->toHtml());
-	}
-
-	function onQuickFormEvent($event, $arg, &$caller) {
-		if ('updateValue' == $event) {
-				// we need to call setValue(), 'cause the default/constant value
-				// may be in fact a timestamp, not an array
-			$this->_createElementsIfNotExist();
-			return HTML_QuickForm_element::onQuickFormEvent($event, $arg, $caller);
-		} else {
-			return parent::onQuickFormEvent($event, $arg, $caller);
-		}
-	}
-
-	function exportValue(&$submitValues, $assoc = false) {
-		if ($this->_options['date']) {
-			$dpv = $this->_elements['__datepicker']->exportValue($submitValues);
-			if ($dpv=='') return $this->_prepareValue('', $assoc);
-			$dv = $this->_elements['__date']->exportValue($submitValues);
-			$result = recalculate_time(date('Y-m-d'),$dv);
-			$cleanValue = date('Y-m-d H:i:s',Base_RegionalSettingsCommon::reg2time($dpv.' '.date('H:i:s', $result),!isset($this->_options['regional_settings_tz']) || $this->_options['regional_settings_tz']==true)); //tz trans - last arg changed from false...
-		} else {
-			$dv = $this->_elements['__date']->exportValue($submitValues);
-			$result = recalculate_time(date('Y-m-d'),$dv);
-			$cleanValue = date('1970-01-01 H:i:s',Base_RegionalSettingsCommon::reg2time('1970-01-01 '.date('H:i:s', $result),!isset($this->_options['regional_settings_tz']) || $this->_options['regional_settings_tz']==true)); //tz trans - last arg changed from false...
-		}
-		return $this->_prepareValue($cleanValue, $assoc);
-	}
-
-	// }}}
-	// {{{ accept()
-
-	function accept(HTML_QuickForm_Renderer &$renderer, $required = false, $error = null) {
-		$renderer->renderElement($this, $required, $error);
-	}
-
-	function setValue($value)
-	{
-		$this->_createElementsIfNotExist();
-		if(is_array($value)) {
-			if ($this->_options['date']) {
-				if($value['__datepicker']!=='')
-					$value['__datepicker'] = strftime('%Y-%m-%d',Base_RegionalSettingsCommon::reg2time($value['__datepicker'],false));
-				$this->_elements['__datepicker']->setValue($value['__datepicker']);
-			}
-			$this->_elements['__date']->setValue(isset($value['__date'])?$value['__date']:'');
-		} else {
-			if (!$value) return;
-			if (!is_numeric($value)) $value = Base_RegionalSettingsCommon::reg2time($value,false);
-			$value -= (date('i',$value) % $this->_options['optionIncrement']['i'])*60;
-			//tz trans begin
-			if(!isset($this->_options['regional_settings_tz']) || $this->_options['regional_settings_tz']==true)
-				$value = Base_RegionalSettingsCommon::time2reg($value,true,true,true,false);
-			//tz trans end
-			foreach ($this->_elements as & $v)
-				$v->setValue($value);
-		}
-	} //end func setValue
-	
 }
-?>
