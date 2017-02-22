359c59174f667645676cd1736cbf2800512438ba
Adam Bukowski <bukowski.adam@gmail.com>
2017-02-22 15:33:33 +0100

    [ADD] AspectMock to tests

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -4,6 +4,12 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\TestInterface;
+
 class Unit extends \Codeception\Module
 {
+    function _after(TestInterface $test)
+    {
+        \AspectMock\Test::clean();
+    }
 }
