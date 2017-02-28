00ec15130ce6c1820be35ec23b3f15da2d63bbe7
Adam Bukowski <bukowski.adam@gmail.com>
2017-02-28 14:52:36 +0100

    Fix max files set

--- modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
+++ modules/Utils/RecordBrowser/RecordBrowserCommon_0.php
@@ -3705,7 +3705,7 @@ class Utils_RecordBrowserCommon extends ModuleCommon {
             }
 			$dropzoneField->set_defaults($files);
 		}
-        if (isset($desc['param']['max_files'])) {
+        if (isset($desc['param']['max_files']) && $desc['param']['max_files'] !== false) {
             $dropzoneField->set_max_files($desc['param']['max_files']);
         }
 		$dropzoneField->add_to_form($form, $field, $label);
