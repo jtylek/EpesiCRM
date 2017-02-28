4c27f0813b66200c2c511bc39ad78f5c0828a23a
Adam Bukowski <bukowski.adam@gmail.com>
2017-02-28 18:48:41 +0100

    Remove unnecessary patch that was really slow

--- modules/Utils/Attachment/patches/20170217_update_filestorage_ids.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-defined("_VALID_ACCESS") || die('Direct access forbidden');
-
-DB::Execute('UPDATE utils_attachment_file af SET filestorage_id=(SELECT id FROM utils_filestorage WHERE link=(' . DB::Concat('\'attachment_file/\'', 'af.id') . '))');
