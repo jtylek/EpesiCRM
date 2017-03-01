c5d9087965f495e211e2b3c2180c59c02da220d6
NorbertNader <norbert.nader@gmail.com>
2017-03-01 11:18:34 +0100

    [FIX] PHP 7.1 Notice: A non well formed numeric value encountered
    Trying to calculate on string, casted to int and removed 'px' sufix before calculation

--- modules/Utils/GenericBrowser/GenericBrowser_0.php
+++ modules/Utils/GenericBrowser/GenericBrowser_0.php
@@ -988,7 +988,11 @@ class Utils_GenericBrowser extends Module {
 				if ((!isset($this->columns[$k]['wrapmode']) || $this->columns[$k]['wrapmode']!='cut') && isset($v['hint'])) $col[$k]['attrs'] .= ' title="'.$v['hint'].'"';
 				$col[$k]['attrs'] .= (isset($this->columns[$k]['wrapmode']) && $this->columns[$k]['wrapmode']=='nowrap')?' nowrap':'';
 				if ($all_width!=0)
-        				$max_width = 130*$this->columns[$k]['width']/$all_width*(7+(isset($this->columns[$k]['fontsize'])?$this->columns[$k]['fontsize']:0));
+					$max_width = 130*(substr($this->columns[$k]['width'],-2)=="px"
+							? (int)substr($this->columns[$k]['width'],0,-2)
+							: (int)$this->columns[$k]['width'])/$all_width*(7+(isset($this->columns[$k]['fontsize'])
+								? $this->columns[$k]['fontsize']
+								: 0));
         			else
         			        $max_width = 0;
 				if (isset($this->columns[$k]['wrapmode']) && $this->columns[$k]['wrapmode']=='cut'){
