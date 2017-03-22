cbd1e931dcf9ff5f700101935137037101ec4731
Paweł Bukowski <pbukowski@telaxus.com>
2017-03-22 12:26:55 +0100

    [change] use fontawesome from gantalella

--- include.php
+++ include.php
@@ -37,7 +37,7 @@ try {
     require_once('include/history.php');
     require_once('include/patches.php');
     require_once('include/simple_login.php');
-    require_once('libs/php-fontawesome.php');
+    require_once('include/fontawesome.php');
 } catch (Exception $e) {
     die($e->getMessage());
 }
---
+++ include/fontawesome.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Paul Bukowski <pbukowski@telaxus.com>
+ * @version 1.0
+ * @copyright Copyright &copy; 2017, Telaxus LLC
+ * @license SPL
+ * @package epesi-base
+ */
+defined("_VALID_ACCESS") || die('Direct access forbidden');
+require_once(EPESI_LOCAL_DIR.'/libs/php-fontawesome.php');
+class FontAwesome extends Smk_FontAwesome {
+    public static function get($class_prefix = 'fa-'){
+        return parent::getArray(EPESI_LOCAL_DIR.'/vendor/bower-asset/gentelella/vendors/font-awesome/css/font-awesome.min.css',$class_prefix);
+    }
+}
