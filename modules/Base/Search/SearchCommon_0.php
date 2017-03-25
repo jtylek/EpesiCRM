8daa5543b5a08fd4a932bba915846fb0d07e6a2c
Paweł Bukowski <pbukowski@telaxus.com>
2017-03-25 21:12:12 +0100

    search menu entry icon

--- modules/Base/Search/SearchCommon_0.php
+++ modules/Base/Search/SearchCommon_0.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Search class.
- * 
- * Provides for search functionality in a module. 
- * 
+ *
+ * Provides for search functionality in a module.
+ *
  * @author Arkadiusz Bisaga <abisaga@telaxus.com>
  * @copyright Copyright &copy; 2008, Telaxus LLC
  * @license MIT
@@ -16,7 +16,7 @@ defined("_VALID_ACCESS") || die('Direct access forbidden');
 class Base_SearchCommon extends ModuleCommon {
 	public static function menu() {
 		if (Base_AclCommon::check_permission('Search'))
-			return array(_M('Search')=>array());
+			return array(_M('Search')=>array('__icon__'=>'search'));
 		return array();
 	}
 
