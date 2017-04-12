2120054cb2cdd3d638d9227fc493b2ee2bb24053
NorbertNader <norbert.nader@gmail.com>
2017-04-12 16:24:31 +0200

    [fix] check if expired

--- modules/Utils/FileStorage/RemoteActionHandler.php
+++ modules/Utils/FileStorage/RemoteActionHandler.php
@@ -20,8 +20,7 @@ class Utils_FileStorage_RemoteActionHandler
         $id = $request->query->get('id');
         $token = $request->query->get('token');
         $remote = DB::GetRow('SELECT expires_on, uf.file_id FROM utils_filestorage_remote as ufr INNER JOIN utils_filestorage as uf ON ufr.file_id = uf.id  WHERE ufr.id=%d AND ufr.token=%s',[$id,$token]);
-        $expires_on = $remote['expires_on'];
-        if($expires_on < DB::DBTimeStamp(time())) {
+        if(strtotime($remote['expires_on']) < time()) {
             return new Response('File has expired');
         }
         $request->query->replace(['id'=>$remote['file_id']]);
