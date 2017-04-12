afdb8944d571866594bd136ceba0f78773ec98b1
NorbertNader <norbert.nader@gmail.com>
2017-04-12 16:01:11 +0200

    [feature] remote file download

--- modules/Utils/FileStorage/RemoteActionHandler.php
+++ modules/Utils/FileStorage/RemoteActionHandler.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/ActionHandler.php';
 
 class Utils_FileStorage_RemoteActionHandler
@@ -9,7 +12,20 @@ class Utils_FileStorage_RemoteActionHandler
 
     protected function hasAccess($action, $request)
     {
-        return false;
+        return !$this->forUsersOnly;
+    }
+
+    protected function getFile(Request $request, $disposition)
+    {
+        $id = $request->query->get('id');
+        $token = $request->query->get('token');
+        $remote = DB::GetRow('SELECT expires_on, uf.file_id FROM utils_filestorage_remote as ufr INNER JOIN utils_filestorage as uf ON ufr.file_id = uf.id  WHERE ufr.id=%d AND ufr.token=%s',[$id,$token]);
+        $expires_on = $remote['expires_on'];
+        if($expires_on < DB::DBTimeStamp(time())) {
+            return new Response('File has expired');
+        }
+        $request->query->replace(['id'=>$remote['file_id']]);
+        return parent::getFile($request, $disposition);
     }
 
 }
