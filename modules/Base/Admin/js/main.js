2ae025f95f2de32300c077a56cb31e0c831ab969
NorbertNader <norbert.nader@gmail.com>
2017-04-14 00:49:50 +0200

    [fix] replace deprecated direction option with slide
    was throwing this.easing not defined

--- modules/Base/Admin/js/main.js
+++ modules/Base/Admin/js/main.js
@@ -5,12 +5,12 @@ admin_switch_button = function(button_id,field_checked,section_id,init) {
 		var s_opacity=1;
 		if (admin_switch_button.status[button_id] == 1 && !init) return;
 		admin_switch_button.status[button_id] = 1;
-        section.show('blind',{direction:'vertical'},dur);
+        section.slideDown(dur);
 	} else {
 		var s_opacity=0.4;
 		if (admin_switch_button.status[button_id] == 0 && !init) return;
 		admin_switch_button.status[button_id] = 0;
-        section.hide('blind',{direction:'vertical'},dur);
+        section.slideUp(dur);
 	}
 	jq('#'+button_id).fadeTo(dur,s_opacity);
 }
