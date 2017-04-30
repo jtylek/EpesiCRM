a911c77c4359efe72bcc3cfc77cd660b45fb946b
NorbertNader <norbert.nader@gmail.com>
2017-04-30 19:49:34 +0200

    Change version number

--- include/version.php
+++ include/version.php
@@ -1,6 +1,6 @@
 <?php
-define("EPESI_VERSION", '1.8.1');
-define("EPESI_REVISION", 20161121);
+define("EPESI_VERSION", '1.8.2');
+define("EPESI_REVISION", 20170430);
 
 function epesi_requires_update()
 {
