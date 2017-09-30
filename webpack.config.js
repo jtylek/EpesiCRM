6362c7fb2f70264620357077f321655b4dfb4f01
Paweł Jedwabny <pjedwabny@gmail.com>
2017-09-30 21:00:37 +0200

    use ExtractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     context: __dirname + "/src",
     entry: "./index",
@@ -10,7 +12,11 @@ module.exports = {
         loaders: [
             {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
             {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })},
             {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
@@ -28,5 +34,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
 };
