70951a476143cbc2e3535a069137ca4e1ba0552f
Paweł Jedwabny <pjedwabny@gmail.com>
2017-09-30 20:24:57 +0200

    enable source-map in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     context: __dirname + "/src",
     entry: "./index",
+    devtool: 'source-map',
     output: {
         path: __dirname + "/dist",
         filename: "index.js"
