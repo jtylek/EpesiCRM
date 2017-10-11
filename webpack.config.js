25d93354c3169376d54adcddb85b16d54e7e34a7
Paweł Jedwabny <pjedwabny@gmail.com>
2017-10-11 21:12:41 +0200

    remove deprecated loader for local bootstrap from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /bootstrap\/js\//, loader: 'imports-loader?jQuery=jquery' },
             {test: /\.less$/,
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
