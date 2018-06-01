4ffb340f629294707bbadfea4871ec27540f08b9
Paweł Jedwabny <pjedwabny@gmail.com>
2018-06-01 21:45:02 +0200

    Revert "[fix] remove hard coded indicator from base/box"
    This reverts commit 2ced73b

--- modules/Base/Box/Box_0.php
+++ modules/Base/Box/Box_0.php
@@ -156,6 +156,8 @@ class Base_Box extends Module {
 		$theme->assign('home', array('href'=>Base_HomePageCommon::get_href(), 'label'=>__('Home')));
 
         $theme->assign('version_no',$version_no);
+        $theme->assign('indicator', $this->init_module(Base_User_Login::module_name())->indicator());
+
         $theme->assign('help',Base_MainModuleIndicatorCommon::get_href());
         $theme->display();
 
