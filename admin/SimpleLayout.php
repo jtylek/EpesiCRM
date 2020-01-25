643fb52f96dbeee456849dfaa70600b0e79b456b
Janusz Tylek <j@epe.si>
2020-01-25 09:28:24 +0100

    Fixed footer and added help link

--- admin/SimpleLayout.php
+++ admin/SimpleLayout.php
@@ -97,10 +97,11 @@ class SimpleLayout {
 
     function pagefooter() {
         ?>
-				<div>
-				<br>
-                    <span class="footer"><a href="https://epe.si"><img src="images/epesi-powered.png" border="0"></a> &bull; copyright &copy; 2006 - <?php echo date('Y'); ?> by <a href="https://epe.si">Janusz Tylek</a>  &bull; MIT License</span>
-				</div>
+		<div id="footer"
+                    <div class="footer"><a href="https://epe.si"><img src="images/epesi-powered.png" border="0"></a></div>
+			<div>Copyright &copy; 2006-<?php echo date('Y'); ?> by Janusz Tylek</div>
+		    <div class="support">Support: <a href="https://epesi.org">https://epesi.org</a></div>
+		</div>
             </body>
         </html>
     <?php }
