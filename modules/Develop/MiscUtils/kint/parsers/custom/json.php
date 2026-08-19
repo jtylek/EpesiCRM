02d46a0243c28e4ec0b1d80c21b7e4e1c63bb505
Janusz Tylek <j@epe.si>
2026-08-20 00:55:12 +0200

    Fix removed curly-brace string offset syntax in bundled Kint parsers (PHP 8 compat)

--- modules/Develop/MiscUtils/kint/parsers/custom/json.php
+++ modules/Develop/MiscUtils/kint/parsers/custom/json.php
@@ -4,7 +4,7 @@ class Kint_Parsers_Json extends kintParser
 	protected function _parse( & $variable )
 	{
 		if ( !is_string( $variable )
-			|| !isset( $variable{0} ) || ( $variable{0} !== '{' && $variable{0} !== '[' )
+			|| !isset( $variable[0] ) || ( $variable[0] !== '{' && $variable[0] !== '[' )
 			|| ( $json = json_decode( $variable ) ) === null
 		) return false;
 
